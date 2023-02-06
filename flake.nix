9e51921eebe6cb5d4a483c923817ac7ea3a2eb36
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2023-02-06 15:10:14 +0000

    add flake (#32)

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+    utils.url = "github:numtide/flake-utils";
+    foundry.url = "github:shazow/foundry.nix/monthly";
+  };
+  outputs = inputs@{ self, nixpkgs, foundry, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ foundry.overlay ];
+        };
+      in {
+        devShell = with pkgs;
+          mkShell {
+            SOLC_VERSION = "0.8.15";
+            buildInputs = [ foundry.defaultPackage.${system} solc-select yarn ];
+            shellHook = ''
+              solc-select install $SOLC_VERSION
+            '';
+          };
+      });
+}
