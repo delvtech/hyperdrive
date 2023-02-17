d1b1df18a1dfbaa46883116cd19627f09e216f84
jonny rhea <jonathan.rhea@gmail.com>
2023-02-17 14:47:27 -0600

    added some mock classes to get code coverage working with libraries in floundry

--- test/YieldSpaceMath.t.sol
+++ test/YieldSpaceMath.t.sol
@@ -2,11 +2,14 @@
 pragma solidity ^0.8.18;
 
 import { Test } from "forge-std/Test.sol";
-import { YieldSpaceMath } from "contracts/libraries/YieldSpaceMath.sol";
+import "test/mocks/MockYieldSpaceMath.sol";
 
 contract YieldSpaceMathTest is Test {
+
     function test__calculateOutGivenIn() public {
-        uint256 result1 = YieldSpaceMath.calculateOutGivenIn(
+        // NOTE: Coverage only works if I initialize the fixture in the test function
+        MockYieldSpaceMath yieldSpaceMath = new MockYieldSpaceMath();
+        uint256 result1 = yieldSpaceMath.calculateOutGivenIn(
             61.824903300361854e18, // shareReserves
             56.92761678068477e18, // bondReserves
             119.1741606776616e18, // bondReserveAdjustment
@@ -18,7 +21,7 @@ contract YieldSpaceMathTest is Test {
         );
         assertEq(result1, 5.955718322566968926e18);
 
-        uint256 result2 = YieldSpaceMath.calculateOutGivenIn(
+        uint256 result2 = yieldSpaceMath.calculateOutGivenIn(
             61.824903300361854e18, // shareReserves
             56.92761678068477e18, // bondReserves
             119.1741606776616e18, // bondReserveAdjustment
@@ -29,5 +32,33 @@ contract YieldSpaceMathTest is Test {
             false // isBondIn
         );
         assertEq(result2, 5.031654806080804961e18);
+    }
+
+    function test__calculateInGivenOut() public {
+        // NOTE: Coverage only works if I initialize the fixture in the test function
+        MockYieldSpaceMath yieldSpaceMath = new MockYieldSpaceMath();
+        uint256 result1 = yieldSpaceMath.calculateInGivenOut(
+            61.824903300361854e18, // shareReserves
+            56.92761678068477e18, // bondReserves
+            119.1741606776616e18, // bondReserveAdjustment
+            5.500250311701939e18, // amountOut
+            1e18 - 0.08065076081220067e18, // stretchedTimeElapsed
+            1e18, // c
+            1e18, // mu
+            true // isBaseOut
+        );
+        assertEq(result1, 6.015131552181907864e18);
+
+        uint256 result2 = yieldSpaceMath.calculateInGivenOut(
+            61.824903300361854e18, // shareReserves
+            56.92761678068477e18, // bondReserves
+            119.1741606776616e18, // bondReserveAdjustment
+            5.500250311701939e18, // amountOut
+            1e18 - 0.08065076081220067e18, // stretchedTimeElapsed
+            1e18, // c
+            1e18, // mu
+            false // isBaseOut
+        );
+        assertEq(result2, 5.077749727267331547e18);
     }
 }
---
+++ test/mocks/MockYieldSpaceMath.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.18;
+
+import { YieldSpaceMath } from "contracts/libraries/YieldSpaceMath.sol";
+
+contract MockYieldSpaceMath {
+    function calculateOutGivenIn (
+        uint256 shareReserves,
+        uint256 bondReserves,
+        uint256 bondReserveAdjustment,
+        uint256 amountIn,
+        uint256 stretchedTimeElapsed,
+        uint256 c,
+        uint256 mu,
+        bool isBondIn
+    ) external returns (uint256) {
+        uint256 result = YieldSpaceMath.calculateOutGivenIn(
+            shareReserves,
+            bondReserves,
+            bondReserveAdjustment,
+            amountIn,
+            stretchedTimeElapsed,
+            c,
+            mu,
+            isBondIn
+        );
+        return result;
+    }
+
+    function calculateInGivenOut (
+        uint256 shareReserves,
+        uint256 bondReserves,
+        uint256 bondReserveAdjustment,
+        uint256 amountOut,
+        uint256 stretchedTimeElapsed,
+        uint256 c,
+        uint256 mu,
+        bool isBaseOut
+    ) external returns (uint256) {
+        uint256 result = YieldSpaceMath.calculateInGivenOut(
+            shareReserves,
+            bondReserves,
+            bondReserveAdjustment,
+            amountOut,
+            stretchedTimeElapsed,
+            c,
+            mu,
+            isBaseOut
+        );
+        return result;
+    }
+}
