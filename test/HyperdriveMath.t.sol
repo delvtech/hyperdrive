8fa59de672cf7475cd8a874346a56fb3801285d1
Jonny Rhea <5555162+jrhea@users.noreply.github.com>
2023-02-17 15:06:49 -0600

    library coverage fix (#83)
    * added some mock classes to get code coverage working with libraries in floundry
    
    * prettier
    
    * code coverage no longer commenting
    
    * prettier

--- test/HyperdriveMath.t.sol
+++ test/HyperdriveMath.t.sol
@@ -4,15 +4,17 @@ pragma solidity ^0.8.15;
 import { ERC20PresetFixedSupply } from "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol";
 import { Test } from "forge-std/Test.sol";
 import { ForwarderFactory } from "contracts/ForwarderFactory.sol";
-import { HyperdriveMath } from "contracts/libraries/HyperdriveMath.sol";
+import { MockHyperdriveMath } from "test/mocks/MockHyperdriveMath.sol";
 import "contracts/libraries/FixedPointMath.sol";
 
 contract HyperdriveMathTest is Test {
     function setUp() public {}
 
     function test__calcSpotPrice() public {
+        // NOTE: Coverage only works if I initialize the fixture in the test function
+        MockHyperdriveMath hyperdriveMath = new MockHyperdriveMath();
         assertEq(
-            HyperdriveMath.calculateSpotPrice(
+            hyperdriveMath.calculateSpotPrice(
                 1 ether, // shareReserves
                 1 ether, // bondReserves
                 0 ether, // lpTotalSupply
@@ -24,7 +26,7 @@ contract HyperdriveMathTest is Test {
         );
 
         assertApproxEqAbs(
-            HyperdriveMath.calculateSpotPrice(
+            hyperdriveMath.calculateSpotPrice(
                 1.1 ether, // shareReserves
                 1 ether, // bondReserves
                 0 ether, // lpTotalSupply
@@ -38,9 +40,11 @@ contract HyperdriveMathTest is Test {
     }
 
     function test__calcAPRFromReserves() public {
+        // NOTE: Coverage only works if I initialize the fixture in the test function
+        MockHyperdriveMath hyperdriveMath = new MockHyperdriveMath();
         // equal reserves should make 0% APR
         assertEq(
-            HyperdriveMath.calculateAPRFromReserves(
+            hyperdriveMath.calculateAPRFromReserves(
                 1 ether, // shareReserves
                 1 ether, // bondReserves
                 0 ether, // lpTotalSupply
@@ -53,7 +57,7 @@ contract HyperdriveMathTest is Test {
 
         // target a 10% APR
         assertApproxEqAbs(
-            HyperdriveMath.calculateAPRFromReserves(
+            hyperdriveMath.calculateAPRFromReserves(
                 1 ether, // shareReserves
                 1.1 ether, // bondReserves
                 0 ether, // lpTotalSupply
@@ -67,7 +71,9 @@ contract HyperdriveMathTest is Test {
     }
 
     function test__calcFeesInGivenOut() public {
-        (uint256 curveFee, uint256 flatFee) = HyperdriveMath
+        // NOTE: Coverage only works if I initialize the fixture in the test function
+        MockHyperdriveMath hyperdriveMath = new MockHyperdriveMath();
+        (uint256 curveFee, uint256 flatFee) = hyperdriveMath
             .calculateFeesInGivenOut(
                 1 ether, // amountOut
                 1 ether, // timeRemaining
@@ -86,7 +92,7 @@ contract HyperdriveMathTest is Test {
             0 ether // ~ 0.011 ether or 10% of the price difference
         );
 
-        (curveFee, flatFee) = HyperdriveMath.calculateFeesInGivenOut(
+        (curveFee, flatFee) = hyperdriveMath.calculateFeesInGivenOut(
             1 ether, // amountOut
             1 ether, // timeRemaining
             0.9 ether, // spotPrice
@@ -104,7 +110,7 @@ contract HyperdriveMathTest is Test {
             0 ether // ~ 0.011 ether or 10% of the price difference
         );
 
-        (curveFee, flatFee) = HyperdriveMath.calculateFeesInGivenOut(
+        (curveFee, flatFee) = hyperdriveMath.calculateFeesInGivenOut(
             1 ether, // amountOut
             0, // timeRemaining
             0.9 ether, // spotPrice
@@ -122,7 +128,7 @@ contract HyperdriveMathTest is Test {
             0.1 ether // ~ 0.011 ether or 10% of the price difference
         );
 
-        (curveFee, flatFee) = HyperdriveMath.calculateFeesInGivenOut(
+        (curveFee, flatFee) = hyperdriveMath.calculateFeesInGivenOut(
             1 ether, // amountOut
             0, // timeRemaining
             0.9 ether, // spotPrice
@@ -142,7 +148,9 @@ contract HyperdriveMathTest is Test {
     }
 
     function test__calcFeesOutGivenIn() public {
-        (uint256 curveFee, uint256 flatFee) = HyperdriveMath
+        // NOTE: Coverage only works if I initialize the fixture in the test function
+        MockHyperdriveMath hyperdriveMath = new MockHyperdriveMath();
+        (uint256 curveFee, uint256 flatFee) = hyperdriveMath
             .calculateFeesOutGivenIn(
                 1 ether, // amountIn
                 1 ether, // timeRemaining
@@ -161,7 +169,7 @@ contract HyperdriveMathTest is Test {
             0 ether // ~ 0.011 ether or 10% of the price difference
         );
 
-        (curveFee, flatFee) = HyperdriveMath.calculateFeesOutGivenIn(
+        (curveFee, flatFee) = hyperdriveMath.calculateFeesOutGivenIn(
             1 ether, // amountIn
             1 ether, // timeRemaining
             0.9 ether, // spotPrice
@@ -179,7 +187,7 @@ contract HyperdriveMathTest is Test {
             0 ether // ~ 0.011 ether or 10% of the price difference
         );
 
-        (curveFee, flatFee) = HyperdriveMath.calculateFeesOutGivenIn(
+        (curveFee, flatFee) = hyperdriveMath.calculateFeesOutGivenIn(
             1 ether, // amountIn
             0, // timeRemaining
             0.9 ether, // spotPrice
@@ -198,7 +206,7 @@ contract HyperdriveMathTest is Test {
             "test 1"
         );
 
-        (curveFee, flatFee) = HyperdriveMath.calculateFeesOutGivenIn(
+        (curveFee, flatFee) = hyperdriveMath.calculateFeesOutGivenIn(
             1 ether, // amountIn
             0, // timeRemaining
             0.9 ether, // spotPrice
---
+++ test/mocks/MockHyperdriveMath.sol
@@ -0,0 +1,257 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.18;
+
+import { HyperdriveMath } from "contracts/libraries/HyperdriveMath.sol";
+
+contract MockHyperdriveMath {
+    function calculateAPRFromReserves(
+        uint256 _shareReserves,
+        uint256 _bondReserves,
+        uint256 _lpTotalSupply,
+        uint256 _initialSharePrice,
+        uint256 _positionDuration,
+        uint256 _timeStretch
+    ) external pure returns (uint256) {
+        uint256 result = HyperdriveMath.calculateAPRFromReserves(
+            _shareReserves,
+            _bondReserves,
+            _lpTotalSupply,
+            _initialSharePrice,
+            _positionDuration,
+            _timeStretch
+        );
+        return result;
+    }
+
+    function calculateInitialBondReserves(
+        uint256 _shareReserves,
+        uint256 _sharePrice,
+        uint256 _initialSharePrice,
+        uint256 _apr,
+        uint256 _positionDuration,
+        uint256 _timeStretch
+    ) external pure returns (uint256) {
+        uint256 result = HyperdriveMath.calculateInitialBondReserves(
+            _shareReserves,
+            _sharePrice,
+            _initialSharePrice,
+            _apr,
+            _positionDuration,
+            _timeStretch
+        );
+        return result;
+    }
+
+    function calculateBondReserves(
+        uint256 _shareReserves,
+        uint256 _lpTotalSupply,
+        uint256 _initialSharePrice,
+        uint256 _apr,
+        uint256 _positionDuration,
+        uint256 _timeStretch
+    ) external pure returns (uint256) {
+        uint256 result = HyperdriveMath.calculateBondReserves(
+            _shareReserves,
+            _lpTotalSupply,
+            _initialSharePrice,
+            _apr,
+            _positionDuration,
+            _timeStretch
+        );
+        return result;
+    }
+
+    function calculateOpenLong(
+        uint256 _shareReserves,
+        uint256 _bondReserves,
+        uint256 _bondReserveAdjustment,
+        uint256 _amountIn,
+        uint256 _normalizedTimeRemaining,
+        uint256 _timeStretch,
+        uint256 _sharePrice,
+        uint256 _initialSharePrice
+    ) external pure returns (uint256, uint256) {
+        (uint256 result1, uint256 result2) = HyperdriveMath.calculateOpenLong(
+            _shareReserves,
+            _bondReserves,
+            _bondReserveAdjustment,
+            _amountIn,
+            _normalizedTimeRemaining,
+            _timeStretch,
+            _sharePrice,
+            _initialSharePrice
+        );
+        return (result1, result2);
+    }
+
+    function calculateCloseLong(
+        uint256 _shareReserves,
+        uint256 _bondReserves,
+        uint256 _bondReserveAdjustment,
+        uint256 _amountIn,
+        uint256 _normalizedTimeRemaining,
+        uint256 _timeStretch,
+        uint256 _sharePrice,
+        uint256 _initialSharePrice
+    ) external pure returns (uint256, uint256) {
+        (uint256 result1, uint256 result2) = HyperdriveMath.calculateCloseLong(
+            _shareReserves,
+            _bondReserves,
+            _bondReserveAdjustment,
+            _amountIn,
+            _normalizedTimeRemaining,
+            _timeStretch,
+            _sharePrice,
+            _initialSharePrice
+        );
+        return (result1, result2);
+    }
+
+    function calculateOpenShort(
+        uint256 _shareReserves,
+        uint256 _bondReserves,
+        uint256 _bondReserveAdjustment,
+        uint256 _amountIn,
+        uint256 _normalizedTimeRemaining,
+        uint256 _timeStretch,
+        uint256 _sharePrice,
+        uint256 _initialSharePrice
+    ) external pure returns (uint256) {
+        uint256 result = HyperdriveMath.calculateOpenShort(
+            _shareReserves,
+            _bondReserves,
+            _bondReserveAdjustment,
+            _amountIn,
+            _normalizedTimeRemaining,
+            _timeStretch,
+            _sharePrice,
+            _initialSharePrice
+        );
+        return result;
+    }
+
+    function calculateCloseShort(
+        uint256 _shareReserves,
+        uint256 _bondReserves,
+        uint256 _bondReserveAdjustment,
+        uint256 _amountOut,
+        uint256 _normalizedTimeRemaining,
+        uint256 _timeStretch,
+        uint256 _sharePrice,
+        uint256 _initialSharePrice
+    ) external pure returns (uint256, uint256) {
+        (uint256 result1, uint256 result2) = HyperdriveMath.calculateCloseShort(
+            _shareReserves,
+            _bondReserves,
+            _bondReserveAdjustment,
+            _amountOut,
+            _normalizedTimeRemaining,
+            _timeStretch,
+            _sharePrice,
+            _initialSharePrice
+        );
+        return (result1, result2);
+    }
+
+    function calculateSpotPrice(
+        uint256 _shareReserves,
+        uint256 _bondReserves,
+        uint256 _lpTotalSupply,
+        uint256 _initialSharePrice,
+        uint256 _normalizedTimeRemaining,
+        uint256 _timeStretch
+    ) external pure returns (uint256) {
+        uint256 result = HyperdriveMath.calculateSpotPrice(
+            _shareReserves,
+            _bondReserves,
+            _lpTotalSupply,
+            _initialSharePrice,
+            _normalizedTimeRemaining,
+            _timeStretch
+        );
+        return result;
+    }
+
+    function calculateFeesOutGivenIn(
+        uint256 _amountIn,
+        uint256 _normalizedTimeRemaining,
+        uint256 _spotPrice,
+        uint256 _sharePrice,
+        uint256 _curveFeePercent,
+        uint256 _flatFeePercent,
+        bool _isBaseIn
+    ) external pure returns (uint256, uint256) {
+        (uint256 result1, uint256 result2) = HyperdriveMath
+            .calculateFeesOutGivenIn(
+                _amountIn,
+                _normalizedTimeRemaining,
+                _spotPrice,
+                _sharePrice,
+                _curveFeePercent,
+                _flatFeePercent,
+                _isBaseIn
+            );
+        return (result1, result2);
+    }
+
+    function calculateFeesInGivenOut(
+        uint256 _amountOut,
+        uint256 _normalizedTimeRemaining,
+        uint256 _spotPrice,
+        uint256 _sharePrice,
+        uint256 _curveFeePercent,
+        uint256 _flatFeePercent,
+        bool _isBaseOut
+    ) external pure returns (uint256, uint256) {
+        (uint256 result1, uint256 result2) = HyperdriveMath
+            .calculateFeesInGivenOut(
+                _amountOut,
+                _normalizedTimeRemaining,
+                _spotPrice,
+                _sharePrice,
+                _curveFeePercent,
+                _flatFeePercent,
+                _isBaseOut
+            );
+        return (result1, result2);
+    }
+
+    function calculateLpSharesOutForSharesIn(
+        uint256 _shares,
+        uint256 _shareReserves,
+        uint256 _lpTotalSupply,
+        uint256 _longsOutstanding,
+        uint256 _shortsOutstanding,
+        uint256 _sharePrice
+    ) external pure returns (uint256) {
+        uint256 result = HyperdriveMath.calculateLpSharesOutForSharesIn(
+            _shares,
+            _shareReserves,
+            _lpTotalSupply,
+            _longsOutstanding,
+            _shortsOutstanding,
+            _sharePrice
+        );
+        return result;
+    }
+
+    function calculateOutForLpSharesIn(
+        uint256 _shares,
+        uint256 _shareReserves,
+        uint256 _lpTotalSupply,
+        uint256 _longsOutstanding,
+        uint256 _shortsOutstanding,
+        uint256 _sharePrice
+    ) external pure returns (uint256, uint256, uint256) {
+        (uint256 result1, uint256 result2, uint256 result3) = HyperdriveMath
+            .calculateOutForLpSharesIn(
+                _shares,
+                _shareReserves,
+                _lpTotalSupply,
+                _longsOutstanding,
+                _shortsOutstanding,
+                _sharePrice
+            );
+        return (result1, result2, result3);
+    }
+}
