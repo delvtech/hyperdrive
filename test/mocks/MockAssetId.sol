31321d829ad794b2dbfe9f64482d129f26f3103b
jonny rhea <jonathan.rhea@gmail.com>
2023-02-23 14:59:36 -0600

    encode/decode assetId tests

--- test/mocks/MockAssetId.sol
+++ test/mocks/MockAssetId.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.8.18;
 
 import { AssetId } from "contracts/libraries/AssetId.sol";
 
-contract AssetId {
+contract MockAssetId {
     function encodeAssetId(
-        AssetIdPrefix _prefix,
+        AssetId.AssetIdPrefix _prefix,
         uint256 _timestamp
     ) external pure returns (uint256) {
         uint256 id = AssetId.encodeAssetId(_prefix, _timestamp);
@@ -14,8 +14,9 @@ contract AssetId {
 
     function decodeAssetId(
         uint256 _id
-    ) external pure returns (AssetIdPrefix, uint256) {
-        (AssetIdPrefix prefix, uint256 timestamp) = AssetId.decodeAssetId(_id);
+    ) external pure returns (AssetId.AssetIdPrefix, uint256) {
+        (AssetId.AssetIdPrefix prefix, uint256 timestamp) = AssetId
+            .decodeAssetId(_id);
         return (prefix, timestamp);
     }
 }
