f61bb3f9b71b0817d282481c5e514e0834fb55f7
Cash DeLeon <cashd@users.noreply.github.com>
2023-03-01 17:31:51 -0600

    Update ERC20Mintable.sol

--- contracts/test/ERC20Mintable.sol
+++ contracts/test/ERC20Mintable.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity ^0.8.15;
+pragma solidity ^0.8.0;
 
 import { ERC20Burnable } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
