f323197a4f70c35d50c90781f28d0a537fd9b401
Cash DeLeon <cashd@users.noreply.github.com>
2023-03-02 11:22:41 -0600

    Remove unused logic from mock pool

--- test/mocks/aave/MockAavePool.sol
+++ test/mocks/aave/MockAavePool.sol
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.10;
-import { Pool } from "@aave/core-v3/contracts/protocol/pool/Pool.sol";
+
 import { IPoolAddressesProvider } from "@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol";
+import { Pool } from "@aave/core-v3/contracts/protocol/pool/Pool.sol";
 
 contract MockAavePool is Pool {
-    uint16 internal _maxNumberOfReserves = 128;
-
     function getRevision() internal pure override returns (uint256) {
         return 0x3;
     }
@@ -13,17 +12,4 @@ contract MockAavePool is Pool {
     function _onlyPoolConfigurator() internal view override {}
 
     constructor(IPoolAddressesProvider provider) Pool(provider) {}
-
-    // function setMaxNumberOfReserves(uint16 newMaxNumberOfReserves) public {
-    //     _maxNumberOfReserves = newMaxNumberOfReserves;
-    // }
-
-    // function MAX_NUMBER_RESERVES() public view override returns (uint16) {
-    //     return _maxNumberOfReserves;
-    // }
-
-    // function dropReserve(address asset) external override {
-    //     _reservesList[_reserves[asset].id] = address(0);
-    //     delete _reserves[asset];
-    // }
 }
