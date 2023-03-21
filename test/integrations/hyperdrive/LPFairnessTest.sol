04cfe8411b3f8549624cf8f0711a6cdcabc1009c
jonny rhea <jonathan.rhea@gmail.com>
2023-03-21 15:37:52 -0500

    add multiple trade short test

--- test/integrations/hyperdrive/LPFairnessTest.sol
+++ test/integrations/hyperdrive/LPFairnessTest.sol
@@ -4,9 +4,12 @@ pragma solidity ^0.8.18;
 import { AssetId } from "contracts/src/libraries/AssetId.sol";
 import { FixedPointMath } from "contracts/src/libraries/FixedPointMath.sol";
 import { HyperdriveTest, HyperdriveUtils } from "../../utils/HyperdriveTest.sol";
+import "test/utils/Lib.sol";
 import "forge-std/console2.sol";
 
 contract LPFairnessTest is HyperdriveTest {
+    // TODO: remove this
+    using Lib for *;
     using FixedPointMath for uint256;
 
     function test_lp_fairness_short() external {
@@ -25,17 +28,17 @@ contract LPFairnessTest is HyperdriveTest {
         uint256 aprBefore = HyperdriveUtils.calculateAPRFromReserves(
             hyperdrive
         );
-        console2.log("aprBefore", aprBefore);
+        console2.log("aprBefore", aprBefore.toString(18));
     }
         // Celine opens a short.
         uint256 bondsShorted = 5_000_000e18;
-        console2.log("bondsShorted", bondsShorted);
+        console2.log("bondsShorted", bondsShorted.toString(18));
         (, uint256 baseSpent) = openShort(celine, bondsShorted);
-        console2.log("baseSpent", baseSpent);
+        console2.log("baseSpent", baseSpent.toString(18));
     {
         // Store the pool APR after Celine opens a short.
         uint256 aprAfter = HyperdriveUtils.calculateAPRFromReserves(hyperdrive);
-        console2.log("aprAfter", aprAfter);
+        console2.log("aprAfter", aprAfter.toString(18));
     }
         // Bob adds liquidity.
         uint256 contribution = 5_000_000e18;
@@ -50,10 +53,10 @@ contract LPFairnessTest is HyperdriveTest {
             apy,
             POSITION_DURATION
         );
-        console2.log("poolInterest", poolInterest);
-        console2.log("poolValue", poolValue);
+        console2.log("poolInterest", poolInterest.toString(18));
+        console2.log("poolValue", poolValue.toString(18));
 
-        // Calculate the value of the pool after interest is accrued.
+        // Calculate the value of the short after interest is accrued.
         (, int256 shortInterest) = HyperdriveUtils.calculateCompoundInterest(
             bondsShorted,
             apy,
@@ -62,19 +65,120 @@ contract LPFairnessTest is HyperdriveTest {
 
         // calculate the portion of the pool's value (after interest) that bob contributed.
         uint256 contributionWithInterest = uint256(poolValue - baseSpent).mulDivDown(lpShares, hyperdrive.totalSupply(AssetId._LP_ASSET_ID));
-        console2.log("contributionWithInterest", contributionWithInterest);
+        console2.log("contributionWithInterest", contributionWithInterest.toString(18));
 
         // calculate the portion of the fixed interest that bob earned
         uint256 fixedInterestEarned = baseSpent.mulDivDown(lpShares, hyperdrive.totalSupply(AssetId._LP_ASSET_ID));
-        console2.log("fixedInterestEarned", fixedInterestEarned);
+        console2.log("fixedInterestEarned", fixedInterestEarned.toString(18));
 
         // calculate the portion of the variable interest that bob owes
         uint256 variableInterestOwed = uint256(shortInterest).mulDivDown(lpShares, hyperdrive.totalSupply(AssetId._LP_ASSET_ID));
-        console2.log("variableInterestOwed", variableInterestOwed);
+        console2.log("variableInterestOwed", variableInterestOwed.toString(18));
 
         // calculate the expected withdrawal proceeds
         uint256 expectedWithdrawalProceeds = contributionWithInterest + fixedInterestEarned - variableInterestOwed;
-        console2.log("expectedWithdrawalProceeds", expectedWithdrawalProceeds);
+        console2.log("expectedWithdrawalProceeds", expectedWithdrawalProceeds.toString(18));
+
+        // Bob removes liquidity
+        uint256 withdrawalProceeds = removeLiquidity(bob, lpShares);
+        assertApproxEqAbs(withdrawalProceeds, expectedWithdrawalProceeds, 1 wei);
+    }
+
+    function test_lp_fairness_short_multiple_trades() external {
+        // limit the fuzz testing to apy's less than 100%
+        //vm.assume(param < 1e18);
+        // variable interest rate earned by the pool
+        int256 apy = 0.10e18;//int256(param);
+        // fixed interest rate the pool pays the longs
+        uint256 apr = 0.10e18;
+
+        // Initialize the pool with capital.
+        uint256 initialLiquidity = 5_000_000e18;
+        initialize(alice, apr, initialLiquidity);
+    {
+        // Store the pool APR before Celine opens a short.
+        uint256 aprBefore = HyperdriveUtils.calculateAPRFromReserves(
+            hyperdrive
+        );
+        console2.log("aprBefore", aprBefore.toString(18));
+    }
+        // Celine opens a short.
+        uint256 bondsShorted = 1_000_000e18;
+        console2.log("bondsShorted", bondsShorted.toString(18));
+        (, uint256 baseSpent) = openShort(celine, bondsShorted);
+        console2.log("baseSpent", baseSpent.toString(18));
+    {
+        // Store the pool APR after Celine opens a short.
+        uint256 aprAfter = HyperdriveUtils.calculateAPRFromReserves(hyperdrive);
+        console2.log("aprAfter", aprAfter.toString(18));
+    }
+        // 1/2 term passes.
+        advanceTime(POSITION_DURATION/2,apy);
+
+        // Calculate the value of the pool after interest is accrued.
+        (uint256 poolValue,) = HyperdriveUtils.calculateCompoundInterest(
+            initialLiquidity + baseSpent,
+            apy,
+            POSITION_DURATION/2
+        );
+        console2.log("poolValue", poolValue.toString(18));
+
+        // Calculate the value of the pool after interest is accrued.
+        (, int256 shortInterest) = HyperdriveUtils.calculateCompoundInterest(
+            bondsShorted,
+            apy,
+            POSITION_DURATION/2
+        );
+
+        // Celine opens another short.
+        uint256 bondsShorted2 = 1_000_000e18;
+        console2.log("bondsShorted2", bondsShorted2.toString(18));
+        (, uint256 baseSpent2) = openShort(celine, bondsShorted2);
+        console2.log("baseSpent2", baseSpent2.toString(18));
+        // uint256 bondsShorted2 = 0;
+        // uint256 baseSpent2 = 0;
+
+        // Bob adds liquidity.
+        uint256 contribution = 5_000_000e18;
+        uint256 lpShares = addLiquidity(bob, contribution);
+        console2.log("lpShares", lpShares.toString(18));
+
+        // 1/2 term passes.
+        advanceTime(POSITION_DURATION/2,apy);
+
+        // Calculate the value of the pool after interest is accrued.
+        (uint256 poolValue2, ) = HyperdriveUtils.calculateCompoundInterest(
+            poolValue + contribution + baseSpent2,
+            apy,
+            POSITION_DURATION/2
+        );
+        console2.log("poolValue2", poolValue2.toString(18));
+
+        // Calculate the value of the short after interest is accrued.
+        (, int256 shortInterest2) = HyperdriveUtils.calculateCompoundInterest(
+            bondsShorted + bondsShorted2 + uint256(shortInterest),
+            apy,
+            POSITION_DURATION/2
+        );
+
+        // calculate the portion of the pool's value (after interest) that bob contributed.
+        uint256 contributionWithInterest = uint256(poolValue2 - baseSpent2 - baseSpent).mulDivDown(lpShares, hyperdrive.totalSupply(AssetId._LP_ASSET_ID));
+        console2.log("contributionWithInterest", contributionWithInterest.toString(18));
+
+        // calculate the portion of the fixed interest that bob earned
+        uint256 fixedInterestEarned = (baseSpent + baseSpent2).mulDivDown(lpShares, hyperdrive.totalSupply(AssetId._LP_ASSET_ID));
+        console2.log("fixedInterestEarned", fixedInterestEarned.toString(18));
+
+        // calculate the portion of the variable interest that bob owes
+        uint256 variableInterestOwed = uint256(shortInterest + shortInterest2).mulDivDown(lpShares, hyperdrive.totalSupply(AssetId._LP_ASSET_ID));
+        console2.log("variableInterestOwed", variableInterestOwed.toString(18));
+
+        // calculate the expected withdrawal shares so they can be removed from the expected proceeds
+        uint256 withdrawalShares = bondsShorted2.mulDivDown(lpShares, hyperdrive.totalSupply(AssetId._LP_ASSET_ID));
+
+        // calculate the expected withdrawal proceeds
+        uint256 expectedWithdrawalProceeds = contributionWithInterest + fixedInterestEarned - variableInterestOwed - withdrawalShares;
+        console2.log("expectedWithdrawalProceeds", expectedWithdrawalProceeds.toString(18));
 
         // Bob removes liquidity
         uint256 withdrawalProceeds = removeLiquidity(bob, lpShares);
@@ -98,19 +202,19 @@ contract LPFairnessTest is HyperdriveTest {
             hyperdrive
         );
 
-        console2.log("aprBeforeLong", aprBeforeLong);
+        console2.log("aprBeforeLong", aprBeforeLong.toString(18));
 
         // Celine opens a long.
         uint256 baseSpent = 5_100_000e18;
-        console2.log("baseSpent", baseSpent);
+        console2.log("baseSpent", baseSpent.toString(18));
         (, uint256 bondsPurchased) = openLong(celine, baseSpent);
-        console2.log("bondsPurchased", bondsPurchased);
+        console2.log("bondsPurchased", bondsPurchased.toString(18));
 
 
         // Store the pool APR after Celine opens a short.
         uint256 aprAfterLong = HyperdriveUtils.calculateAPRFromReserves(hyperdrive);
 
-        console2.log("aprAfterLong", aprAfterLong);
+        console2.log("aprAfterLong", aprAfterLong.toString(18));
 
         // Bob adds liquidity.
         uint256 contribution = 5_000_000e18;
@@ -125,16 +229,16 @@ contract LPFairnessTest is HyperdriveTest {
             apy,
             POSITION_DURATION
         );
-        console2.log("poolValue", poolValue);
+        console2.log("poolValue", poolValue.toString(18));
         // calculate the portion of the pool's value (after interest) that bob contributed.
         uint256 contributionWithInterest = (poolValue - baseSpent).mulDivDown(lpShares, hyperdrive.totalSupply(AssetId._LP_ASSET_ID));
-        console2.log("contributionWithInterest", contributionWithInterest);
+        console2.log("contributionWithInterest", contributionWithInterest.toString(18));
         // calculate the portion of the fixed interest that bob owes
-        uint256 percentLongProceedsOwed = (bondsPurchased - baseSpent).mulDivDown(lpShares, hyperdrive.totalSupply(AssetId._LP_ASSET_ID));
-        console2.log("percentLongProceedsOwed", percentLongProceedsOwed);
+        uint256 fixedInterestOwed = (bondsPurchased - baseSpent).mulDivDown(lpShares, hyperdrive.totalSupply(AssetId._LP_ASSET_ID));
+        console2.log("fixedInterestOwed", fixedInterestOwed.toString(18));
         // calculate the expected withdrawal proceeds
-        uint256 expectedWithdrawalProceeds = contributionWithInterest - percentLongProceedsOwed;
-        console2.log("expectedWithdrawalProceeds", expectedWithdrawalProceeds);
+        uint256 expectedWithdrawalProceeds = contributionWithInterest - fixedInterestOwed;
+        console2.log("expectedWithdrawalProceeds", expectedWithdrawalProceeds.toString(18));
 
         // Ensure that if the new LP withdraws, they get their money back.
         uint256 withdrawalProceeds = removeLiquidity(bob, lpShares);
@@ -161,14 +265,14 @@ contract LPFairnessTest is HyperdriveTest {
 }
         // Celine opens a long.
         uint256 baseSpent = 1_000_000e18;
-        console2.log("baseSpent", baseSpent);
+        console2.log("baseSpent", baseSpent.toString(18));
         (, uint256 bondsPurchased) = openLong(celine, baseSpent);
-        console2.log("bondsPurchased", bondsPurchased);
+        console2.log("bondsPurchased", bondsPurchased.toString(18));
 
 {
         // Store the pool APR after Celine opens a short.
         uint256 aprAfterLong = HyperdriveUtils.calculateAPRFromReserves(hyperdrive);
-        console2.log("aprAfterLong", aprAfterLong);
+        console2.log("aprAfterLong", aprAfterLong.toString(18));
 }
         // 1/2 the term passes.
         advanceTime(POSITION_DURATION/2,apy);
@@ -180,9 +284,9 @@ contract LPFairnessTest is HyperdriveTest {
 
         // Celine opens another long.
         uint256 baseSpent2 = 2_000_000e18;
-        console2.log("baseSpent2", baseSpent2);
+        console2.log("baseSpent2", baseSpent2.toString(18));
         (, uint256 bondsPurchased2) = openLong(celine, baseSpent2);
-        console2.log("bondsPurchased2", bondsPurchased2);
+        console2.log("bondsPurchased2", bondsPurchased2.toString(18));
 
         // Bob adds liquidity.
         uint256 contribution = 5_000_000e18;
@@ -197,16 +301,19 @@ contract LPFairnessTest is HyperdriveTest {
             apy,
             POSITION_DURATION/2
         );
-        console2.log("poolValue2", poolValue2);
+        console2.log("poolValue2", poolValue2.toString(18));
         // calculate the portion of the pool's value (after interest) that bob contributed.
         uint256 contributionWithInterest = (poolValue2 - baseSpent2 - baseSpent).mulDivDown(lpShares, hyperdrive.totalSupply(AssetId._LP_ASSET_ID));
-        console2.log("contributionWithInterest", contributionWithInterest);
+        console2.log("contributionWithInterest", contributionWithInterest.toString(18));
         // calculate the portion of the fixed interest that bob owes
-        uint256 percentLongProceedsOwed = (bondsPurchased + bondsPurchased2 - baseSpent2 - baseSpent).mulDivDown(lpShares, hyperdrive.totalSupply(AssetId._LP_ASSET_ID));
-        console2.log("percentLongProceedsOwed", percentLongProceedsOwed);
+        uint256 fixedInterestOwed = (bondsPurchased + bondsPurchased2 - baseSpent2 - baseSpent).mulDivDown(lpShares, hyperdrive.totalSupply(AssetId._LP_ASSET_ID));
+        console2.log("fixedInterestOwed", fixedInterestOwed.toString(18));
+        // calculate the expected withdrawal shares so they can be removed from the expected proceeds
+        uint256 withdrawalShares = bondsPurchased2.mulDivDown(lpShares, hyperdrive.totalSupply(AssetId._LP_ASSET_ID));
+        console2.log("withdrawalShares", withdrawalShares.toString(18));
         // calculate the expected withdrawal proceeds
-        uint256 expectedWithdrawalProceeds = contributionWithInterest - percentLongProceedsOwed;
-        console2.log("expectedWithdrawalProceeds", expectedWithdrawalProceeds);
+        uint256 expectedWithdrawalProceeds = contributionWithInterest - fixedInterestOwed;
+        console2.log("expectedWithdrawalProceeds", expectedWithdrawalProceeds.toString(18));
 
         // Ensure that if the new LP withdraws, they get their money back.
         uint256 withdrawalProceeds = removeLiquidity(bob, lpShares);
