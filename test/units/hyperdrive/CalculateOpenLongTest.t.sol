721b96b91ef0eae174959b757ab5d51b97a60823
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2023-03-30 18:09:42 +0100

    Fixed math in CalculateOpenLongTest (#188)

--- test/units/hyperdrive/CalculateOpenLongTest.t.sol
+++ test/units/hyperdrive/CalculateOpenLongTest.t.sol
@@ -33,10 +33,23 @@ contract CalculateOpenLongTest is HyperdriveTest {
     IHyperdrive.Fees fees;
 
     // Intermediary calculation variables
+    uint256 dz_curve;
+    uint256 dy_$_flat;
+    uint256 one_minus_t_stretch;
+    uint256 c_div_mu;
+    uint256 mu_z;
+    uint256 mu_z_pow_one_minus_t_stretch;
+    uint256 y_pow_one_minus_t_stretch;
     uint256 k;
+    uint256 mu_z_and_dzcurve;
+    uint256 mu_z_and_dzcurve_pow_one_minus_t_stretch;
+    uint256 z_;
+    uint256 inverse_one_minus_t_stretch;
+    uint256 _y;
+    uint256 dy_$_curve;
+    uint256 dy;
     uint256 tau;
     uint256 p;
-    uint256 dy;
     uint256 flat_fee;
     uint256 curve_fee;
     uint256 governance_flat_fee;
@@ -48,7 +61,7 @@ contract CalculateOpenLongTest is HyperdriveTest {
     uint256 expectedBondProceeds;
     uint256 expectedShareReservesDelta;
 
-    function test_calculate_open_long() internal {
+    function test_calculate_open_long() external {
         // µ = 1 * 45/41
         initialSharePrice = 1.097560975609756097e18;
         // c = 1 * 49/41
@@ -96,21 +109,21 @@ contract CalculateOpenLongTest is HyperdriveTest {
         // Δz_curve = Δz · t
         // Δz_curve = 83673.469387755102049354 * 0.971428571428571428
         // Δz_curve = 81282.798833819241942987617492711370257512
-        uint256 dz_curve = shareAmount.mulDown(normalizedTimeRemaining);
+        dz_curve = shareAmount.mulDown(normalizedTimeRemaining);
         assertEq(dz_curve, 81282.798833819241942987e18, "dz_curve");
 
         // Δy'flat = Δz · (1 - t) * c
         // Δy'flat = 83673.469387755102049354 * (1 - 0.971428571428571428) * 1.195121951219512195
         // Δy'flat = 2857.14285714285719999998512408447699634341676740382564116
-        uint256 dy_apost_flat = shareAmount
+        dy_$_flat = shareAmount
             .mulDown(FixedPointMath.ONE_18.sub(normalizedTimeRemaining))
             .mulDown(sharePrice);
-        assertEq(dy_apost_flat, 2857.142857142857199999e18, "dy_apost_flat");
+        assertEq(dy_$_flat, 2857.142857142857199999e18, "dy_apost_flat");
 
         // For the curve trade, (1 - t) is reframed as (1 - timeStretch)
         // (1 - t) = (1 - 0.045071688063194094)
         // (1 - t) = 0.954928311936805906
-        uint256 one_minus_t_stretch = FixedPointMath.ONE_18.sub(timeStretch);
+        one_minus_t_stretch = FixedPointMath.ONE_18.sub(timeStretch);
         assertEq(
             one_minus_t_stretch,
             0.954928311936805906e18,
@@ -119,17 +132,17 @@ contract CalculateOpenLongTest is HyperdriveTest {
 
         // (c / µ) = (1.195121951219512195 / 1.097560975609756097)
         // (c / µ) = 1.0888888888888888893343209876543209878819862825788751715841263222...
-        uint256 c_div_mu = sharePrice.divDown(initialSharePrice);
+        c_div_mu = sharePrice.divDown(initialSharePrice);
         assertEq(c_div_mu, 1.088888888888888889e18, "c_div_mu");
 
         // (µ · z) = (1.097560975609756097 * 337003257.328990228013029315)
         // (µ · z) = 369881623.897672201288664494059346945260983555
-        uint256 mu_z = initialSharePrice.mulDown(shareReserves);
+        mu_z = initialSharePrice.mulDown(shareReserves);
         assertEq(mu_z, 369881623.897672201288664494e18, "mu_z");
 
         // (µ · z)^(1 - t) = 369881623.897672201288664494^(0.954928311936805906)
         // (µ · z)^(1 - t) = 152014740.974500528173941042763114476815073326183010430
-        uint256 mu_z_pow_one_minus_t_stretch = initialSharePrice
+        mu_z_pow_one_minus_t_stretch = initialSharePrice
             .mulDown(shareReserves)
             .pow(one_minus_t_stretch);
         assertApproxEqAbs(
@@ -141,9 +154,7 @@ contract CalculateOpenLongTest is HyperdriveTest {
 
         // y^(1 - t) = (383887915.936952714535901926)^(0.954928311936805906)
         // y^(1 - t) = 157506998.923528080399004587041700209859953096737243167
-        uint256 y_pow_one_minus_t_stretch = bondReserves.pow(
-            one_minus_t_stretch
-        );
+        y_pow_one_minus_t_stretch = bondReserves.pow(one_minus_t_stretch);
         assertApproxEqAbs(
             y_pow_one_minus_t_stretch,
             157506998.923528080399004587e18,
@@ -161,7 +172,7 @@ contract CalculateOpenLongTest is HyperdriveTest {
 
         // (µ · (z + Δz_curve)) = 1.097560975609756097 * (337003257.328990228013029315 + 81282.798833819241942987)
         // (µ · (z + Δz_curve)) = 369970836.725660539480995345538049924710625294
-        uint256 mu_z_and_dzcurve = initialSharePrice.mulDown(
+        mu_z_and_dzcurve = initialSharePrice.mulDown(
             shareReserves.add(dz_curve)
         );
         assertEq(
@@ -172,7 +183,7 @@ contract CalculateOpenLongTest is HyperdriveTest {
 
         // (µ · (z + Δz_curve))^(1 - t) = 369970836.725660539480995345^0.954928311936805906
         // (µ · (z + Δz_curve))^(1 - t) = 152049753.115098833898787866715676618030157129075568782
-        uint256 mu_z_and_dzcurve_pow_one_minus_t_stretch = mu_z_and_dzcurve.pow(
+        mu_z_and_dzcurve_pow_one_minus_t_stretch = mu_z_and_dzcurve.pow(
             one_minus_t_stretch
         );
         assertApproxEqAbs(
@@ -184,32 +195,32 @@ contract CalculateOpenLongTest is HyperdriveTest {
 
         // (c / µ) · (µ · (z + Δz_curve))^(1 - t) = 1.088888888888888889 * 152049753.115098833898787866
         // (c / µ) · (µ · (z + Δz_curve))^(1 - t) = 165565286.725329841373352315546122092655420874
-        uint256 z_ = c_div_mu.mulDown(mu_z_and_dzcurve_pow_one_minus_t_stretch);
+        z_ = c_div_mu.mulDown(mu_z_and_dzcurve_pow_one_minus_t_stretch);
         assertApproxEqAbs(z_, 165565286.725329841373352315e18, 5e7, "z_"); // TODO Precision
 
         // (1 / (1 - t)) = 1 / 0.954928311936805906
         // (1 / (1 - t)) = 1.0471990279267966596926226784985107956999882836058167426007625189...
-        uint256 one_minus_t_inverse = FixedPointMath.ONE_18.divUp(
+        inverse_one_minus_t_stretch = FixedPointMath.ONE_18.divUp(
             one_minus_t_stretch
         );
         assertEq(
-            one_minus_t_inverse,
+            inverse_one_minus_t_stretch,
             1.04719902792679666e18, // Rounded up
-            "one_minus_t_stretch"
+            "inverse_one_minus_t_stretch"
         );
 
         // (k - (c / µ) · (µ · (z + Δz_curve))^(1 - t))^(1 / (1 - t)) = (323034161.31798421109418647 - 165565286.725329841373352315)^1.04719902792679666
         // (k - (c / µ) · (µ · (z + Δz_curve))^(1 - t))^(1 / (1 - t)) = (157468874.592654369720834155)^1.04719902792679666
         // (k - (c / µ) · (µ · (z + Δz_curve))^(1 - t))^(1 / (1 - t)) = 383790611.293791832749419609975176889050228160919979985
-        uint256 _y = k.sub(z_).pow(one_minus_t_inverse);
+        _y = k.sub(z_).pow(inverse_one_minus_t_stretch);
         assertApproxEqAbs(_y, 383790611.293791832749419609e18, 7e8, "_y"); // TODO Precision
 
         // Δy'curve = y - (k - (c / µ) · (µ · (z + Δz_curve))^(1 - t))^(1 / (1 - t))
         // Δy'curve = 383887915.936952714535901926 - 383790611.293791832749419609
         // Δy'curve = 97304.643160881786482317
-        uint256 dy_apost_curve = bondReserves.sub(_y);
+        dy_$_curve = bondReserves.sub(_y);
         assertApproxEqAbs(
-            dy_apost_curve,
+            dy_$_curve,
             97304.643160881786482317e18,
             7e8,
             "dy_apost_curve"
@@ -217,9 +228,9 @@ contract CalculateOpenLongTest is HyperdriveTest {
 
         // Δy = Δ'y_curve + Δ'y_flat
         // Δy = 97304.643160881786482317 + 2857.142857142857199999
-        // Δy = 100161.786018025335116432
-        dy = dy_apost_curve.add(dy_apost_flat);
-        assertApproxEqAbs(dy, 100161.786018025335116432e18, 7e8, "dy");
+        // Δy = 100161.786018024643682316
+        dy = dy_$_curve.add(dy_$_flat);
+        assertApproxEqAbs(dy, 100161.786018024643682316e18, 7e8, "dy");
 
         // τ = t * timeStretch
         // τ = 0.971428571428571428 * 0.045071688063194094
@@ -264,65 +275,65 @@ contract CalculateOpenLongTest is HyperdriveTest {
         );
 
         // governance_curve_fee = Δz * (curve_fee / Δy) * c * Φ_governance
-        // governance_curve_fee = 83673.469387755102049354 * (3.955337805800847634 / 100161.786018025335116432) * 1.195121951219512195 * (42.5/100)
-        // governance_curve_fee = 1.6783033073739723940482360348307398192063707844224640334834848202...
+        // governance_curve_fee = 83673.469387755102049354 * (3.955337805800847634 / 100161.786018024643682316) * 1.195121951219512195 * (42.5/100)
+        // governance_curve_fee = 1.678303307373983979665963589851097667486890664120111384375177702967713899401845398...
         governance_curve_fee = shareAmount
             .mulDivDown(curve_fee, dy)
             .mulDown(sharePrice)
             .mulDown(fees.governance);
         assertApproxEqAbs(
             governance_curve_fee,
-            1.678303307373972394e18,
+            1.678303307373983979e18,
             6e4,
             "governance_curve_fee"
         ); // TODO Precision
 
-        // governance_fee = (flat_fee + curve_fee) / c
-        // governance_fee = (36.428571428571429299 + 1.686156053707779846) / 1.195121951219512195
-        // governance_fee = 31.891914832111175002131828044092977041033860004499283371534067347...
+        // governance_fee = (governance_flat_fee + governance_curve_fee) / c
+        // governance_fee = (36.428571428571429299 + 1.678303307373983979) / 1.195121951219512195
+        // governance_fee = 31.88534416681146825627401471089912941390551170519378871570464405155038660364333102...
         expectedTotalGovernanceFee = (
             governance_flat_fee.add(governance_curve_fee)
         ).divDown(sharePrice);
         assertApproxEqAbs(
             expectedTotalGovernanceFee,
-            31.891914832111175002e18,
+            31.885344166811468256e18,
             5e4,
             "expectedTotalGovernanceFee"
         ); // TODO Precision
 
         // bondReservesDelta = Δ'y_curve - (curve_fee - governance_curve_fee)
-        // bondReservesDelta = 97304.643160881786482317 - (3.955337805800847634 - 1.686156053707779846)
+        // bondReservesDelta = 97304.643160881786482317 - (3.955337805800847634 - 1.678303307373983979)
         // bondReservesDelta = 97302.373979129693414529
-        expectedBondReservesDelta = dy_apost_curve.sub(
+        expectedBondReservesDelta = dy_$_curve.sub(
             curve_fee.sub(governance_curve_fee)
         );
         assertApproxEqAbs(
             expectedBondReservesDelta,
-            97302.373979129693414529e18,
+            97302.366126383359618662e18,
             7e8,
             "expectedBondReservesDelta"
         ); // TODO Precision
 
         // bondProceeds = Δy - (curve_fee + flat_fee)
-        // bondProceeds = 99695.313714817646588683 - (3.955337805800847634 + 85.714285714285715999)
-        // bondProceeds = 99605.64409129756002505
+        // bondProceeds = 100161.786018024643682316 - (3.955337805800847634 + 85.714285714285715999)
+        // bondProceeds = 100072.116394504557118683
         expectedBondProceeds = dy.sub(curve_fee.add(flat_fee));
         assertApproxEqAbs(
             expectedBondProceeds,
-            99605.64409129756002505e18,
+            100072.116394504557118683e18,
             7e8,
             "expectedBondProceeds"
         ); // TODO Precision
 
         // shareReservesDelta = Δz_curve - (governance_curve_fee / c)
-        // shareReservesDelta = 81282.798833819241942987 - (1.686156053707779846 / 1.195121951219512195)
-        // shareReservesDelta = 81281.387968549812984340202972931690922587061527850172543121128727
+        // shareReservesDelta = 81282.798833819241942987 - (1.678303307373983979 / 1.195121951219512195)
+        // shareReservesDelta = 81281.39453921511269108606078626488477021418987614947803777695815062749775895683246...
         expectedShareReservesDelta = dz_curve.sub(
             governance_curve_fee.divDown(sharePrice)
         );
         assertApproxEqAbs(
             expectedShareReservesDelta,
-            81281.38796854981298434e18,
+            81281.394539215112691086e18,
             5e4,
             "expectedShareReservesDelta"
         ); // TODO Precision
@@ -362,13 +373,17 @@ contract CalculateOpenLongTest is HyperdriveTest {
         // Adding explicit delta assertions so that any change in how these
         // values are derived will fail the test
         // TODO Precision
-        assertWithDelta(shareReservesDelta, -49490, 81281.38796854981298434e18);
+        assertWithDelta(
+            shareReservesDelta,
+            -49215,
+            81281.394539215112691086e18
+        );
         assertWithDelta(
             bondReservesDelta,
-            -691486281,
-            97302.373979129693414529e18
+            -691486610,
+            97302.366126383359618662e18
         );
-        assertWithDelta(bondProceeds, -691545427, 99605.64409129756002505e18);
-        assertWithDelta(totalGovernanceFee, 49490, 31.891914832111175002e18);
+        assertWithDelta(bondProceeds, -691545427, 100072.116394504557118683e18);
+        assertWithDelta(totalGovernanceFee, 49214, 31.885344166811468256e18);
     }
 }
