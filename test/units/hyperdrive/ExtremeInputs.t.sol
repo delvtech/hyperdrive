d4788d313d5a8832b2cff2e01d637c5ad9afaae6
Alex Towle <jalextowle@gmail.com>
2023-03-30 21:08:30 -0500

    Added a calculation for the maximum amount of bonds that can be shorted

--- test/units/hyperdrive/ExtremeInputs.t.sol
+++ test/units/hyperdrive/ExtremeInputs.t.sol
@@ -17,27 +17,15 @@ contract ExtremeInputs is HyperdriveTest {
         IHyperdrive.PoolInfo memory poolInfoBefore = hyperdrive.getPoolInfo();
 
         // Max base amount
-        uint256 baseAmount = HyperdriveUtils.calculateMaxOpenLong(hyperdrive);
+        uint256 baseAmount = HyperdriveUtils.calculateMaxLong(hyperdrive);
 
         // Open long with max base amount
         (, uint256 bondAmount) = openLong(bob, baseAmount);
-
-        uint256 apr = HyperdriveUtils.calculateAPRFromReserves(hyperdrive);
-
-        IHyperdrive.PoolInfo memory poolInfoAfter = hyperdrive.getPoolInfo();
-
-        // FIXME: Can we get a similar check, or is this now
-        // fundamentally broken due to the way that the virtual
-        // reserves work?
-        //
-        // assertApproxEqAbs(
-        //     poolInfoAfter.bondReserves,
-        //     0,
-        //     1e15,
-        //     "bondReserves should be approximately empty"
-        // );
+        HyperdriveUtils.PoolInfo memory poolInfoAfter = HyperdriveUtils
+            .getPoolInfo(hyperdrive);
 
         // Ensure that the ending APR is approximately 0%.
+        uint256 apr = HyperdriveUtils.calculateAPRFromReserves(hyperdrive);
         assertApproxEqAbs(
             apr,
             0,
@@ -58,6 +46,54 @@ contract ExtremeInputs is HyperdriveTest {
                 INITIAL_SHARE_PRICE,
                 POSITION_DURATION,
                 hyperdrive.getPoolConfig().timeStretch
+            ),
+            5
+        );
+    }
+
+    function test_max_open_short() external {
+        // Initialize the pools with a large amount of capital.
+        initialize(alice, 0.05e18, 500_000_000e18);
+
+        // Calculate amount of base
+        HyperdriveUtils.PoolInfo memory poolInfoBefore = HyperdriveUtils
+            .getPoolInfo(hyperdrive);
+
+        // Max amount of bonds to short
+        uint256 bondAmount = HyperdriveUtils.calculateMaxShort(hyperdrive);
+
+        // Open long with max base amount
+        uint256 aprBefore = HyperdriveUtils.calculateAPRFromReserves(
+            hyperdrive
+        );
+        openShort(bob, bondAmount);
+        uint256 aprAfter = HyperdriveUtils.calculateAPRFromReserves(hyperdrive);
+
+        // Ensure the share reserves are approximately empty and that the apr
+        // increased.
+        HyperdriveUtils.PoolInfo memory poolInfoAfter = HyperdriveUtils
+            .getPoolInfo(hyperdrive);
+        assertApproxEqAbs(
+            poolInfoAfter.shareReserves,
+            0,
+            1e15,
+            "shareReserves should be approximately empty"
+        );
+        assertGt(aprAfter, aprBefore);
+
+        // Ensure that the bond reserves were updated to have the correct APR.
+        // Due to the way that the flat part of the trade is applied, the bond
+        // reserve updates may not exactly correspond to the amount of bonds
+        // transferred; however, the pool's APR should be identical to the APR
+        // that the bond amount transfer implies.
+        assertApproxEqAbs(
+            HyperdriveUtils.calculateAPRFromReserves(hyperdrive),
+            HyperdriveMath.calculateAPRFromReserves(
+                poolInfoAfter.shareReserves,
+                poolInfoBefore.bondReserves + bondAmount,
+                INITIAL_SHARE_PRICE,
+                POSITION_DURATION,
+                HyperdriveUtils.getPoolConfig(hyperdrive).timeStretch
             ),
             5
         );
