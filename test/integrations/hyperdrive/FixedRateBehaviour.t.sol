b5f6465ba631b524a976228a44360006c7d3938e
padraic <patrick.morris.310@gmail.com>
2023-03-31 16:58:53 +0100

    updating

--- test/integrations/hyperdrive/FixedRateBehaviour.t.sol
+++ test/integrations/hyperdrive/FixedRateBehaviour.t.sol
@@ -5,6 +5,17 @@ import "forge-std/console2.sol";
 import { FixedPointMath } from "contracts/src/libraries/FixedPointMath.sol";
 import { HyperdriveTest, HyperdriveUtils } from "../../utils/HyperdriveTest.sol";
 
+// TODO Cases
+// [] - long - interim - long - positive APR - full duration trades
+// [] - long - interim - long - positive APR - immediate close trades
+// [] - long - interim - long - negative APR - full duration trades
+// [] - long - interim - long - negative APR - immediate close trades
+//
+// [] - short - interim - short - positive APR - full duration trades
+// [] - short - interim - short - positive APR - immediate close trades
+// [] - short - interim - short - negative APR - full duration trades
+// [] - short - interim - short - negative APR - immediate close trades
+//
 contract FixedRateBehaviour is HyperdriveTest {
     using FixedPointMath for uint256;
 
@@ -16,20 +27,16 @@ contract FixedRateBehaviour is HyperdriveTest {
     // FAIL: [850158846099921994, 3555210678590931723272, 664330425]
     //   Breaks the assumption that 1st trade should be a better outcome than 2nd
     function test_fixed_rate_behaviour_long_interim_long_positive_interest_full_duration(
-        // uint64 _variableRate,
-        // uint256 baseAmount,
-        // uint32 interim
+        uint64 _variableRate,
+        uint256 baseAmount,
+        uint32 interim
     ) external {
-        // // 0% < variableRate < 100%
-        // // 1000 < baseAmount < 100,000,000
-        // // interim <= 25 years
-        // int256 variableRate = int256(uint256(_variableRate) % 1e18);
-        // vm.assume(baseAmount >= 1000e18 && baseAmount <= 100_000_000e18);
-        // vm.assume(interim <= POSITION_DURATION * 25);
-
-        int256 variableRate = 850158846099921994;
-        uint256 baseAmount = 3555210678590931723272;
-        uint32 interim = 664330425;
+        // 0% < variableRate < 100%
+        // 1000 < baseAmount < 100,000,000
+        // interim <= 25 years
+        int256 variableRate = int256(uint256(_variableRate) % 1e18);
+        vm.assume(baseAmount >= 1000e18 && baseAmount <= 100_000_000e18);
+        vm.assume(interim <= POSITION_DURATION * 25);
 
         // Initialize the pool with capital.
         uint256 fixedRate = 0.05e18;
@@ -77,6 +84,59 @@ contract FixedRateBehaviour is HyperdriveTest {
             "The first long should imply a better fixed rate than the second"
         );
     }
+
+    function test_fixed_rate_behaviour_breaking_case() external {
+        int256 variableRate = 850158846099921994; // ~ 85%
+        uint256 baseAmount = 3555210678590931723272; // ~3555.21
+        uint32 interim = 664330425; // ~21 years
+
+        // Initialize the pool with capital.
+        uint256 fixedRate = 0.05e18;
+        uint256 initialLiquidity = 500_000_000e18;
+        initialize(alice, fixedRate, initialLiquidity);
+
+        // Advance time a duration so that an amount of interest has accrued
+        advanceTime(POSITION_DURATION, variableRate);
+
+        (
+            uint256[4] memory spotFixedRates,
+            ,
+            uint256 celineBaseAmount,
+            uint256 celineQuotedAPR,
+            ,
+            uint256 danBaseAmount,
+            uint256 danQuotedAPR
+        ) = _scenarioLong(variableRate, baseAmount, interim, false);
+
+        assertGe(
+            spotFixedRates[0],
+            spotFixedRates[1],
+            "fixed rate should decrease after celine opening and closing a long"
+        );
+        assertEq(
+            spotFixedRates[1],
+            spotFixedRates[2],
+            "fixed rate should remain the same after accruing a long amount of interest"
+        );
+        assertGe(
+            spotFixedRates[2],
+            spotFixedRates[3],
+            "fixed rate should decrease after dan opening and closing a long"
+        );
+        // FAIL: [850158846099921994, 3555210678590931723272, 664330425]
+        assertGt(
+            celineBaseAmount,
+            danBaseAmount,
+            "The first long trade should return marginally more than the second"
+        );
+        // FAIL: [850158846099921994, 3555210678590931723272, 664330425]
+        assertGt(
+            celineQuotedAPR,
+            danQuotedAPR,
+            "The first long should imply a better fixed rate than the second"
+        );
+    }
+
 
     function _scenarioLong(
         int256 variableRate,
