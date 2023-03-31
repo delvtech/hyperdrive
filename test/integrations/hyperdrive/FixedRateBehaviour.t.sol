6bd9d065c7045522bdeaf5f5e2db2d7c87152acd
padraic <patrick.morris.310@gmail.com>
2023-03-31 17:28:10 +0100

    update

--- test/integrations/hyperdrive/FixedRateBehaviour.t.sol
+++ test/integrations/hyperdrive/FixedRateBehaviour.t.sol
@@ -171,10 +171,6 @@ contract FixedRateBehaviour is HyperdriveTest {
             uint256 danQuotedAPR
         )
     {
-        // Advance time using the baseAmount so that an arbitrary amount of
-        // interest has accrued
-        advanceTime(POSITION_DURATION, variableRate);
-
         spotFixedRates[0] = HyperdriveUtils.calculateAPRFromReserves(
             hyperdrive
         );
@@ -225,162 +221,123 @@ contract FixedRateBehaviour is HyperdriveTest {
         );
     }
 
-    // function test_fixed_rate_behaviour_long_interim_long_positive_interest_immediate_closing(
-    //     uint64 _variableRate,
-    //     uint256 baseAmount,
-    //     uint32 interim
-    // ) external {
-    //     uint256 fixedRate = 0.05e18;
+    function test_fixed_rate_behaviour_short_interim_short_positive_interest_full_duration(
+        uint64 _variableRate,
+        uint256 bondAmount,
+        uint32 interim
+    ) external {
+        // 0% < variableRate < 100%
+        // 1000 < baseAmount < 100,000,000
+        // interim <= 25 years
+        int256 variableRate = int256(uint256(_variableRate) % 1e18);
+        vm.assume(bondAmount >= 1000e18 && bondAmount <= hyperdrive.getPoolInfo().bondReserves());
+        vm.assume(interim <= POSITION_DURATION * 25);
 
-    //     // 0% < variableRate < 100%
-    //     int256 variableRate = int256(uint256(_variableRate) % 1e18);
+        // Initialize the pool with capital.
+        uint256 fixedRate = 0.05e18;
+        uint256 initialLiquidity = 500_000_000e18;
+        initialize(alice, fixedRate, initialLiquidity);
 
-    //     // 1000 < baseAmount < 100,000,000
-    //     vm.assume(baseAmount >= 1000e18 && baseAmount <= 100_000_000e18);
+        // Advance time a duration so that an amount of interest has accrued
+        advanceTime(POSITION_DURATION, variableRate);
 
-    //     // interim <= 50 years
-    //     vm.assume(interim <= POSITION_DURATION * 25);
+        (
+            uint256[4] memory spotFixedRates,
+            ,
+            uint256 celineBaseAmount,
+            uint256 celineQuotedAPR,
+            ,
+            uint256 danBaseAmount,
+            uint256 danQuotedAPR
+        ) = _scenarioShort(variableRate, baseAmount, interim, false);
 
-    //     // Initialize the pool with capital.
-    //     uint256 initialLiquidity = 500_000_000e18;
-    //     initialize(alice, fixedRate, initialLiquidity);
+        assertLe(
+            spotFixedRates[0],
+            spotFixedRates[1],
+            "fixed rate should decrease after celine opening and closing a long"
+        );
+        assertEq(
+            spotFixedRates[1],
+            spotFixedRates[2],
+            "fixed rate should remain the same after accruing a long amount of interest"
+        );
+        assertLe(
+            spotFixedRates[2],
+            spotFixedRates[3],
+            "fixed rate should decrease after dan opening and closing a long"
+        );
+        // assertGt(
+        //     celineBaseAmount,
+        //     danBaseAmount,
+        //     "The first long trade should return marginally more than the second"
+        // );
+        // assertGt(
+        //     celineQuotedAPR,
+        //     danQuotedAPR,
+        //     "The first long should imply a better fixed rate than the second"
+        // );
+    }
 
-    //     // Advance time using the baseAmount so that an arbitrary amount of
-    //     // interest has accrued
-    //     advanceTime(POSITION_DURATION, variableRate);
+    function _scenarioShort(
+        int256 variableRate,
+        uint256 bondAmount,
+        uint256 interim,
+        bool immediateClose
+    )
+        internal
+        returns (
+            uint256[4] memory spotFixedRates,
+            uint256 celineBasePaid,
+            uint256 celineBaseAmount,
+            uint256 celineQuotedAPR,
+            uint256 danBasePaid,
+            uint256 danBaseAmount,
+            uint256 danQuotedAPR
+        )
+    {
+        spotFixedRates[0] = HyperdriveUtils.calculateAPRFromReserves(
+            hyperdrive
+        );
 
-    //     spotFixedRate1 = HyperdriveUtils.calculateAPRFromReserves(
-    //         hyperdrive
-    //     );
+        // Open and close a long
+        uint256 celineMaturityTime;
+        (celineMaturityTime, celineBasePaid) = openShort(celine, bondAmount);
+        celineQuotedAPR = HyperdriveUtils.calculateAPRFromRealizedPrice(
+            celineBasePaid,
+            bondAmount,
+            FixedPointMath.ONE_18
+        );
+        if (!immediateClose) {
+            advanceTime(celineMaturityTime - block.timestamp, variableRate);
+        }
+        celineBaseAmount = closeShort(
+            celine,
+            celineMaturityTime,
+            bondAmount
+        );
 
-    //     // Open and close a long so that system accounting is cycled
-    //     (uint256 celineMaturityTime, uint256 celineBondAmount) = openLong(
-    //         celine,
-    //         baseAmount
-    //     );
-    //     uint256 celineBaseAmount = closeLong(
-    //         celine,
-    //         celineMaturityTime,
-    //         celineBondAmount
-    //     );
+        spotFixedRates[1] = HyperdriveUtils.calculateAPRFromReserves(
+            hyperdrive
+        );
+        advanceTime(interim, variableRate);
+        spotFixedRates[2] = HyperdriveUtils.calculateAPRFromReserves(
+            hyperdrive
+        );
 
-    //     spotFixedRate2 = HyperdriveUtils.calculateAPRFromReserves(
-    //         hyperdrive
-    //     );
+        uint256 danMaturityTime;
+        (danMaturityTime, danBasePaid) = openShort(dan, bondAmount);
+        danQuotedAPR = HyperdriveUtils.calculateAPRFromRealizedPrice(
+            danBasePaid,
+            bondAmount,
+            FixedPointMath.ONE_18
+        );
+        if (!immediateClose) {
+            advanceTime(danMaturityTime - block.timestamp, variableRate);
+        }
+        danBaseAmount = closeShort(dan, danMaturityTime, bondAmount);
 
-    //     // Advance the interim amount of time accruing variable rate
-    //     advanceTime(uint256(interim), variableRate);
-
-    //     spotFixedRate3 = HyperdriveUtils.calculateAPRFromReserves(
-    //         hyperdrive
-    //     );
-
-    //     // Open and close a long
-    //     (uint256 danMaturityTime, uint256 danBondAmount) = openLong(
-    //         dan,
-    //         baseAmount
-    //     );
-    //     uint256 danBaseAmount = closeLong(dan, danMaturityTime, danBondAmount);
-
-    //     spotFixedRate4 = HyperdriveUtils.calculateAPRFromReserves(
-    //         hyperdrive
-    //     );
-
-    //     assertGe(
-    //         spotFixedRate1,
-    //         spotFixedRate2,
-    //         "fixed rate should decrease after celine opening and closing a long"
-    //     );
-    //     assertEq(
-    //         spotFixedRate2,
-    //         spotFixedRate3,
-    //         "fixed rate should remain the same after accruing a long amount of interest"
-    //     );
-    //     assertGe(
-    //         spotFixedRate3,
-    //         spotFixedRate4,
-    //         "fixed rate should decrease after dan opening and closing a long"
-    //     );
-    // }
-
-    // // [-731931430838270598, 69000408175263547825293173, 0] Evm revert
-    // function test_fixed_rate_behaviour_long_interim_long_negative_interest(
-    //     int64 variableRate,
-    //     uint256 baseAmount,
-    //     uint32 interim
-    // ) external {
-    //     uint256 fixedRate = 0.05e18;
-
-    //     // 0% < variableRate < -100%
-    //     vm.assume(variableRate < 0 && variableRate >= -1e18);
-
-    //     // 1000 < baseAmount < 100,000,000
-    //     vm.assume(baseAmount >= 1000e18 && baseAmount <= 100_000_000e18);
-
-    //     // interim <= 50 years
-    //     vm.assume(interim <= POSITION_DURATION);
-
-    //     // Initialize the pool with capital.
-    //     uint256 initialLiquidity = 500_000_000e18;
-    //     initialize(alice, fixedRate, initialLiquidity);
-
-    //     // Advance time using the baseAmount so that an arbitrary amount of
-    //     // interest has accrued
-    //     advanceTime(POSITION_DURATION, variableRate);
-
-    //     spotFixedRate1 = HyperdriveUtils.calculateAPRFromReserves(
-    //         hyperdrive
-    //     );
-
-    //     // Open and close a long so that system accounting is cycled
-    //     (uint256 celineMaturityTime, uint256 celineBondAmount) = openLong(
-    //         celine,
-    //         baseAmount
-    //     );
-    //     advanceTime(celineMaturityTime - block.timestamp, variableRate);
-    //     uint256 celineBaseAmount = closeLong(
-    //         celine,
-    //         celineMaturityTime,
-    //         celineBondAmount
-    //     );
-
-    //     spotFixedRate2 = HyperdriveUtils.calculateAPRFromReserves(
-    //         hyperdrive
-    //     );
-
-    //     // Advance the interim amount of time accruing variable rate
-    //     advanceTime(uint256(interim), variableRate);
-
-    //     spotFixedRate3 = HyperdriveUtils.calculateAPRFromReserves(
-    //         hyperdrive
-    //     );
-
-    //     // Open and close a long
-    //     (uint256 danMaturityTime, uint256 danBondAmount) = openLong(
-    //         dan,
-    //         baseAmount
-    //     );
-    //     advanceTime(danMaturityTime - block.timestamp, variableRate);
-    //     uint256 danBaseAmount = closeLong(dan, danMaturityTime, danBondAmount);
-
-    //     spotFixedRate4 = HyperdriveUtils.calculateAPRFromReserves(
-    //         hyperdrive
-    //     );
-
-    //     assertGe(
-    //         spotFixedRate1,
-    //         spotFixedRate2,
-    //         "fixed rate should decrease after celine opening and closing a long"
-    //     );
-    //     assertEq(
-    //         spotFixedRate2,
-    //         spotFixedRate3,
-    //         "fixed rate should remain the same after accruing a long amount of interest"
-    //     );
-    //     assertGe(
-    //         spotFixedRate3,
-    //         spotFixedRate4,
-    //         "fixed rate should decrease after dan opening and closing a long"
-    //     );
-    // }
+        spotFixedRates[3] = HyperdriveUtils.calculateAPRFromReserves(
+            hyperdrive
+        );
+    }
 }
