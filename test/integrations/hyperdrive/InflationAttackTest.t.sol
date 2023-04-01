b63d95b544a75b3bfc051ebad43d71f2c64b8cc3
Alex Towle <jalextowle@gmail.com>
2023-04-01 14:57:07 -0500

    Converted the remaining inflation attack tests to fuzz tests

--- test/integrations/hyperdrive/InflationAttackTest.t.sol
+++ test/integrations/hyperdrive/InflationAttackTest.t.sol
@@ -91,7 +91,14 @@ contract InflationAttackTest is HyperdriveTest {
 
     // This test ensures that a malicious user cannot steal from a long on open
     // by inflating the assets of the pool.
-    function test_inflation_attack_open_long() external {
+    function test_inflation_attack_open_long(
+        uint256 basePaid,
+        uint256 donation
+    ) external {
+        // Ensure that the testing parameters are within bounds.
+        basePaid = basePaid.normalizeToRange(0.001e18, 10_000_000e18);
+        donation = donation.normalizeToRange(0, 100_000_000_000e18);
+
         // Initialize the pool with one wei.
         initialize(alice, 0.02e18, 1);
 
@@ -100,12 +107,11 @@ contract InflationAttackTest is HyperdriveTest {
         // inflation attacks.
         vm.stopPrank();
         vm.startPrank(alice);
-        baseToken.mint(address(hyperdrive), 100_000_000_000e18);
+        baseToken.mint(address(hyperdrive), donation);
 
         // Bob opens a long.
         vm.stopPrank();
         vm.startPrank(bob);
-        uint256 basePaid = 10e18;
         baseToken.mint(basePaid);
         baseToken.approve(address(hyperdrive), basePaid);
         vm.expectRevert(Errors.FixedPointMath_SubOverflow.selector);
@@ -114,12 +120,21 @@ contract InflationAttackTest is HyperdriveTest {
 
     // This test ensures that a malicious user cannot steal from a long by
     // inflating the assets of the pool.
-    function test_inflation_attack_close_long() external {
+    function test_inflation_attack_close_long(
+        uint256 basePaid,
+        uint256 donation,
+        uint256 variableRate
+    ) external {
+        // Ensure that the testing parameters are within bounds.
+        basePaid = basePaid.normalizeToRange(0.001e18, 10_000_000e18);
+        donation = donation.normalizeToRange(basePaid, 100_000_000_000e18);
+        variableRate = variableRate.normalizeToRange(0, 2e18);
+
+        // Initialize the pool.
         uint256 contribution = 500_000_000e18;
         uint256 lpShares = initialize(alice, 0.02e18, contribution);
 
         // Bob opens a long.
-        uint256 basePaid = 10_000_000e18;
         (uint256 maturityTime, uint256 bondAmount) = openLong(bob, basePaid);
 
         // A malicious donation is made to the pool. This donation is comically
@@ -127,19 +142,17 @@ contract InflationAttackTest is HyperdriveTest {
         // inflation attacks.
         vm.stopPrank();
         vm.startPrank(alice);
-        baseToken.mint(address(hyperdrive), 100_000_000_000e18);
+        baseToken.mint(address(hyperdrive), donation);
 
         // Bob closes the long. Bob should receive a small benefit from the sale.
         uint256 baseProceeds = closeLong(bob, maturityTime, bondAmount);
-        assertGt(baseProceeds, basePaid);
+        assertGt(baseProceeds, basePaid - 1e15);
         assertLt(baseProceeds, bondAmount);
 
         // Bob opens a long.
-        basePaid = 10_000_000e18;
         (maturityTime, bondAmount) = openLong(bob, basePaid);
 
         // Time passes and interest accrues.
-        uint256 variableRate = 0.2e18;
         advanceTime(POSITION_DURATION, int256(variableRate));
 
         // A malicious donation is made to the pool. This donation is comically
@@ -147,18 +160,18 @@ contract InflationAttackTest is HyperdriveTest {
         // inflation attacks.
         vm.stopPrank();
         vm.startPrank(alice);
-        baseToken.mint(address(hyperdrive), 100_000_000_000e18);
+        baseToken.mint(address(hyperdrive), donation);
 
         // Bob redeems the long. He should receive the bond amount.
         baseProceeds = closeLong(bob, maturityTime, bondAmount);
-        assertApproxEqAbs(baseProceeds, bondAmount, 500);
+        assertApproxEqAbs(baseProceeds, bondAmount, 1e4);
 
         // Alice removes her liquidity. She should receive more than her
         // original contribution plus interest.
         (uint256 contributionPlusInterest, ) = HyperdriveUtils
             .calculateCompoundInterest(
                 contribution,
-                0.02e18,
+                int256(variableRate),
                 POSITION_DURATION
             );
         (uint256 withdrawalProceeds, ) = removeLiquidity(alice, lpShares);
@@ -167,7 +180,14 @@ contract InflationAttackTest is HyperdriveTest {
 
     // This test ensures that a malicious user cannot steal from a short on open
     // by inflating the assets of the pool.
-    function test_inflation_attack_open_short() external {
+    function test_inflation_attack_open_short(
+        uint256 bondAmount,
+        uint256 donation
+    ) external {
+        // Ensure that the testing parameters are within bounds.
+        bondAmount = bondAmount.normalizeToRange(0.001e18, 10_000_000e18);
+        donation = donation.normalizeToRange(0, 100_000_000_000e18);
+
         // Initialize the pool with one wei.
         initialize(alice, 0.02e18, 1);
 
@@ -176,26 +196,35 @@ contract InflationAttackTest is HyperdriveTest {
         // inflation attacks.
         vm.stopPrank();
         vm.startPrank(alice);
-        baseToken.mint(address(hyperdrive), 100_000_000_000e18);
+        baseToken.mint(address(hyperdrive), donation);
 
         // Bob opens a short.
         vm.stopPrank();
         vm.startPrank(bob);
-        uint256 bondAmount = 10e18;
         baseToken.mint(bondAmount);
         baseToken.approve(address(hyperdrive), bondAmount);
         vm.expectRevert(Errors.FixedPointMath_SubOverflow.selector);
-        hyperdrive.openShort(bondAmount, type(uint256).max, bob, true);
+        hyperdrive.openShort(bondAmount, bondAmount, bob, true);
     }
 
     // This test ensures that a malicious user cannot steal from a short by
     // inflating the assets of the pool.
-    function test_inflation_attack_close_short() external {
+    function test_inflation_attack_close_short(
+        uint256 bondAmount,
+        uint256 donation,
+        uint256 variableRate
+    ) external {
+        // Ensure that the testing parameters are within bounds.
+        bondAmount = bondAmount.normalizeToRange(0.001e18, 10_000_000e18);
+        donation = donation.normalizeToRange(bondAmount, 100_000_000_000e18);
+        variableRate = variableRate.normalizeToRange(0, 2e18);
+
+        // Initialize the pool.
+        uint256 fixedRate = 0.02e18;
         uint256 contribution = 500_000_000e18;
-        uint256 lpShares = initialize(alice, 0.02e18, contribution);
+        uint256 lpShares = initialize(alice, fixedRate, contribution);
 
         // Bob opens a short.
-        uint256 bondAmount = 10_000_000e18;
         (uint256 maturityTime, uint256 basePaid) = openShort(bob, bondAmount);
 
         // A malicious donation is made to the pool. This donation is comically
@@ -203,13 +232,12 @@ contract InflationAttackTest is HyperdriveTest {
         // inflation attacks.
         vm.stopPrank();
         vm.startPrank(alice);
-        uint256 donation = 100_000_000_000e18;
         baseToken.mint(address(hyperdrive), donation);
 
         // Bob closes the short. Bob should receive a benefit from the sale
         // because the share price is inflated.
         uint256 baseProceeds = closeShort(bob, maturityTime, bondAmount);
-        assertGt(baseProceeds, basePaid);
+        assertGt(baseProceeds, basePaid - 1e12);
 
         // TODO: Investigate this more. How much of a donation is needed to make
         // reasonably sized shorts impractical? We should reconsider whether or
@@ -221,11 +249,9 @@ contract InflationAttackTest is HyperdriveTest {
         // price, the short must backpay more interest than the bond amount.
         // They will get this back upon closing. If they just waited until the
         // next checkpoint, this wouldn't be a problem.
-        bondAmount = 10_000_000e18;
-        (maturityTime, basePaid) = openShort(bob, bondAmount, bondAmount * 200);
+        (maturityTime, basePaid) = openShort(bob, bondAmount, donation / 20);
 
         // Time passes and interest accrues.
-        uint256 variableRate = 0.2e18;
         advanceTime(POSITION_DURATION, int256(variableRate));
 
         // A malicious donation is made to the pool. This donation is comically
@@ -233,27 +259,25 @@ contract InflationAttackTest is HyperdriveTest {
         // inflation attacks.
         vm.stopPrank();
         vm.startPrank(alice);
-        donation = 100_000_000_000e18;
         baseToken.mint(address(hyperdrive), donation);
 
         // Bob redeems the short. He should receive more than the interest
         // accrued on the short because the share price is inflated by the
         // attack.
-        (uint256 bondAmountPlusInterest, ) = HyperdriveUtils
-            .calculateCompoundInterest(
-                bondAmount,
-                int256(variableRate),
-                POSITION_DURATION
-            );
+        (, int256 shortInterest) = HyperdriveUtils.calculateCompoundInterest(
+            bondAmount,
+            int256(variableRate),
+            POSITION_DURATION
+        );
         baseProceeds = closeShort(bob, maturityTime, bondAmount);
-        assertGt(baseProceeds, bondAmountPlusInterest);
+        assertGt(baseProceeds, uint256(shortInterest));
 
         // Alice removes her liquidity. She should receive more than her
         // original contribution plus interest.
         (uint256 contributionPlusInterest, ) = HyperdriveUtils
             .calculateCompoundInterest(
                 contribution,
-                0.02e18,
+                int256(variableRate),
                 POSITION_DURATION
             );
         (uint256 withdrawalProceeds, ) = removeLiquidity(alice, lpShares);
