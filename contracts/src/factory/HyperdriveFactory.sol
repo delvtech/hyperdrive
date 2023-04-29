963588d40427d2d78629b7ffa1e05995d7bceecf
Violet Vienhage <>
2023-04-29 19:52:57 +0200

    merge bug fix

--- contracts/src/factory/HyperdriveFactory.sol
+++ contracts/src/factory/HyperdriveFactory.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.18;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { IHyperdriveDeployer } from "../interfaces/IHyperdriveDeployer.sol";
+import { IHyperdrive } from "../interfaces/IHyperdrive.sol";
 import { Errors } from "../libraries/Errors.sol";
 
 /// @author DELV
