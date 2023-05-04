76c3e628604b82408eab357baaa717f84d830ceb
Tadeas Kucera <tadeas@certora.com>
2023-05-04 17:18:54 +0200

    Improve DummyDsrManager

--- certora/helpers/DummyDsrManager.sol
+++ certora/helpers/DummyDsrManager.sol
@@ -1,19 +1,19 @@
 import {DsrManager} from "../../contracts/src/interfaces/IMaker.sol";
 import {DummyERC20A} from "./DummyERC20A.sol";
 import {DummyERC20B} from "./DummyERC20B.sol";
+// import {DummyPot} from "./DummyPot.sol";
 
 
 interface VatLike {
     function hope(address) external;
 }
 
-interface JoinLike {
-    function dai() external view returns (address);
-    function join(address, uint256) external;
-    function exit(address, uint256) external;
-}
-
-contract JoinToken is DummyERC20B
+// Not used as JoinLikeImpl is DummyERC20B
+// interface JoinLike {
+//     function dai() external view returns (address);
+//     function join(address, uint256) external;
+//     function exit(address, uint256) external;
+// }
 
 interface PotLike {
     function vat() external view returns (address);
@@ -24,21 +24,84 @@ interface PotLike {
     function exit(uint256) external;
 }
 
+// minimal Vat contract based on https://github.com/makerdao/dss/blob/master/src/vat.sol
+// TODO: review, that this minimal version is enough.
+contract VatLikeImpl is VatLike {
+    mapping (address => uint) public wards;
+    mapping(address => mapping (address => uint)) public can;
+    uint256 public live;  // Active Flag
+    function hope(address usr) external { can[msg.sender][usr] = 1; }
+
+    constructor(address usr) public {
+        wards[usr] = 1;
+        live = 1;
+    }
+}
+
+
+contract PotLikeImpl is PotLike {
+    // TODO: Delete these munged values
+    address _pot;
+
+    address randAddress;
+    uint256 randUint1;
+    uint256 randUint2;
+    uint256 randUint3;
+    constructor(address pot) {
+        _pot = pot;
+    }
+    function vat() external view returns (address) {
+        //TODO: implement
+        return randAddress;
+    }
+    function chi() external view returns (uint256) {
+        //TODO: implement
+        return randUint1;
+    }
+    function rho() external view returns (uint256) {
+        //TODO: implement
+        return randUint2;
+    }
+    function drip() external returns (uint256) {
+        //TODO: implement
+        return randUint3;
+    }
+    function join(uint256) external {
+        //TODO: implement
+    }
+    function exit(uint256) external {
+        //TODO: implement
+    }
+}
+
+
+contract JoinLikeImpl is DummyERC20B {
+    address _dai;
+    constructor(address dai) DummyERC20B("Dummy","JoinLikeImpl") {
+        _dai = dai;
+    }
+
+    function join(address dst, uint256 wad) external {
+        //TODO : Add join implementation
+    }
+
+    function exit(address, uint256) external {
+        //TODO : Add exit implementation
+    }
+
+    function dai() external view returns (address) {
+        return _dai;
+    }
+}
+
 contract DummyDsrManager is DsrManager {
-    PotLike public _pot;
+    PotLikeImpl  public _pot;
     DummyERC20A  public _dai;
-    JoinLike public _daiJoin;
+    JoinLikeImpl public _daiJoin;
 
     uint256 public supply;
 
     mapping (address => uint256) public _pieOf;
-    // address randAddress1;
-    // address randAddress2;
-    // uint256 randNumber1;
-    // uint256 randNumber2;
-
-
-
 
     uint256 constant RAY = 10 ** 27;
     function add(uint256 x, uint256 y) internal pure returns (uint256 z) {
@@ -64,32 +127,34 @@ contract DummyDsrManager is DsrManager {
     }
 
     constructor(address pot_, address daiJoin_) public {
-        _pot = PotLike(pot_);
-        _daiJoin = JoinLike(daiJoin_);
-        _dai = DummyERC20A("DummyERC20A", "DummySymbolA");
+        _pot = new PotLikeImpl(pot_);
+        _daiJoin = new JoinLikeImpl(daiJoin_);
+        _dai = new DummyERC20A("DummyDai", "DummySymbol");
 
-        VatLike vat = VatLike(_pot.vat());
+        VatLikeImpl vat = new VatLikeImpl(_pot.vat());
         vat.hope(address(_daiJoin));
         vat.hope(address(_pot));
-        _dai.approve(address(_daiJoin), uint256(-1));
+        //TODO: get MAX_INT from local math libs.
+        uint256 MAX_INT = 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
+        _dai.approve(address(_daiJoin), MAX_INT);
     }
 
 
-    function dai() external view returns (address) { return (address(_dai); }
+    function dai() external view returns (address) { return address(_dai); }
 
-    function pot() external view returns (address) { return (address(_pot); }
+    function pot() external view returns (address) { return address(_pot); }
 
     function pieOf(address usr) external view returns (uint256) {
         return _pieOf[usr];
     }
 
     function daiBalance(address usr) external returns (uint256 wad) {
-        uint256 chi = (now > _pot.rho()) ? _pot.drip() : _pot.chi();
+        uint256 chi = (block.timestamp > _pot.rho()) ? _pot.drip() : _pot.chi();
         wad = rmul(chi, _pieOf[usr]);
     }
 
     function join(address dst, uint256 wad) external {
-        uint256 chi = (now > _pot.rho()) ? _pot.drip() : _pot.chi();
+        uint256 chi = (block.timestamp > _pot.rho()) ? _pot.drip() : _pot.chi();
         uint256 pie = rdiv(wad, chi);
         _pieOf[dst] = add(_pieOf[dst], pie);
         supply = add(supply, pie);
@@ -100,7 +165,7 @@ contract DummyDsrManager is DsrManager {
     }
 
     function exit(address dst, uint256 wad) external {
-        uint256 chi = (now > _pot.rho()) ? _pot.drip() : _pot.chi();
+        uint256 chi = (block.timestamp > _pot.rho()) ? _pot.drip() : _pot.chi();
         uint256 pie = rdivup(wad, chi);
 
         require(_pieOf[msg.sender] >= pie, "insufficient-balance");
@@ -114,7 +179,7 @@ contract DummyDsrManager is DsrManager {
     }
 
     function exitAll(address dst) external {
-        uint256 chi = (now > pot.rho()) ? pot.drip() : pot.chi();
+        uint256 chi = (block.timestamp > _pot.rho()) ? _pot.drip() : _pot.chi();
         uint256 pie = _pieOf[msg.sender];
 
         _pieOf[msg.sender] = 0;
