3e10439e67d4e919fc8b9a6525b3bb1baf4e5343
Violet Vienhage <>
2023-05-11 17:06:10 +0200

    change prank

--- test/units/hyperdrive/FeeTest.t.sol
+++ test/units/hyperdrive/FeeTest.t.sol
@@ -34,7 +34,7 @@ contract FeeTest is HyperdriveTest {
         advanceTime(POSITION_DURATION.mulDown(0.5e18), int256(apr));
 
         // Collect fees and test that the fees received in the governance address have earned interest.
-        vm.prank(bob);
+        vm.changePrank(bob);
         MockHyperdrive(address(hyperdrive)).collectGovernanceFee(true);
         uint256 governanceBalanceAfter = baseToken.balanceOf(governance);
         assertGt(governanceBalanceAfter, governanceFeesAfterOpenLong);
@@ -82,7 +82,7 @@ contract FeeTest is HyperdriveTest {
         assertGt(governanceFeesAfterCloseLong, governanceFeesAfterOpenLong);
 
         // Collect fees to governance address
-        vm.prank(bob);
+        vm.changePrank(bob);
         MockHyperdrive(address(hyperdrive)).collectGovernanceFee(true);
 
         // Ensure that governance fees after collection are zero.
@@ -140,7 +140,7 @@ contract FeeTest is HyperdriveTest {
         // collect governance fees
         vm.expectRevert(Errors.Unauthorized.selector);
         MockHyperdrive(address(hyperdrive)).collectGovernanceFee(true);
-        vm.prank(bob);
+        vm.changePrank(bob);
         MockHyperdrive(address(hyperdrive)).collectGovernanceFee(true);
 
         // Ensure that governance fees after collection are zero.
