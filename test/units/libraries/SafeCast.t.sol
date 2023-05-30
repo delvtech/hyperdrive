1f5b096ac61582f4449d3cd31268222a4e0af47f
ControlCplusControlV <44706811+ControlCplusControlV@users.noreply.github.com>
2023-05-30 11:03:49 -0600

    weird coverage behavior with SafeCast

--- test/units/libraries/SafeCast.t.sol
+++ test/units/libraries/SafeCast.t.sol
@@ -3,16 +3,37 @@ pragma solidity ^0.8.18;
 
 import "contracts/src/libraries/SafeCast.sol";
 import "forge-std/Vm.sol";
-import {BaseTest } from "test/utils/BaseTest.sol";
+import { BaseTest } from "test/utils/BaseTest.sol";
 
 contract SafeCastTest is BaseTest {
     function testSafeCastToUint128(uint256 num) public {
-        if (num >= (2**128-1)) {
+        if (num >= (2 ** 128 - 1)) {
             vm.expectRevert();
             SafeCast.toUint128(num);
         } else {
             uint128 cast_num = SafeCast.toUint128(num);
             assertEq(cast_num, uint128(num));
         }
+    }
+
+    function test_toUint128() public {
+        uint128 cast_num = SafeCast.toUint128(0);
+        assertEq(cast_num, uint128(0));
+        cast_num = SafeCast.toUint128(1);
+        assertEq(cast_num, uint128(1));
+        cast_num = SafeCast.toUint128(2 ** 128 - 1);
+        assertEq(cast_num, uint128(2 ** 128 - 1));
+
+        vm.expectRevert();
+        SafeCast.toUint128(2 ** 128);
+
+        vm.expectRevert();
+        SafeCast.toUint128(2 ** 256 - 1);
+
+        vm.expectRevert();
+        SafeCast.toUint128(2 ** 136);
+
+        vm.expectRevert();
+        SafeCast.toUint128(2 ** 255);
     }
 }
