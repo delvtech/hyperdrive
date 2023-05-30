76f2c1e6d5823fa4cd168fcaf9c69d21997923f3
Violet Vienhage <>
2023-05-30 20:22:17 +0200

    Adds the draft contracts for a 4626 integration

---
+++ contracts/src/factory/ERC4626HyperdriveDeployer.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.18;
+
+import { ERC4626Hyperdrive } from "../instances/ERC4626Hyperdrive.sol";
+import { IHyperdrive } from "../interfaces/IHyperdrive.sol";
+import { IHyperdriveDeployer } from "../interfaces/IHyperdriveDeployer.sol";
+import { IERC4626 } from "../interfaces/IERC4626.sol";
+
+/// @author DELV
+/// @title ERC4626HyperdriveFactory
+/// @notice This is a minimal factory which contains only the logic to deploy
+///         hyperdrive and is called by a more complex factory which
+///         initializes the Hyperdrive instances and acts as a registry.
+/// @dev We use two contracts to avoid any code size limit issues with Hyperdrive.
+/// @custom:disclaimer The language used in this code is for coding convenience
+///                    only, and is not intended to, and does not, have any
+///                    particular legal or regulatory significance.
+contract ERC4626HyperdriveDeployer is IHyperdriveDeployer {
+    IERC4626 internal immutable pool;
+
+    constructor(IERC4626 _pool) {
+        pool = _pool;
+    }
+
+    /// @notice Deploys a copy of hyperdrive with the given params.
+    /// @param _config The configuration of the Hyperdrive pool.
+    /// @param _dataProvider The address of the data provider.
+    /// @param _linkerCodeHash The hash of the ERC20 linker contract's
+    ///        constructor code.
+    /// @param _linkerFactory The address of the factory which is used to deploy
+    ///        the ERC20 linker contracts.
+    function deploy(
+        IHyperdrive.PoolConfig memory _config,
+        address _dataProvider,
+        bytes32 _linkerCodeHash,
+        address _linkerFactory,
+        bytes32[] calldata
+    ) external override returns (address) {
+        return (
+            address(
+                new ERC4626Hyperdrive(
+                    _config,
+                    _dataProvider,
+                    _linkerCodeHash,
+                    _linkerFactory,
+                    pool
+                )
+            )
+        );
+    }
+}
---
+++ contracts/src/factory/ERC4626HyperdriveFactory.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.18;
+
+import { IERC20 } from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import { IHyperdrive } from "../interfaces/IHyperdrive.sol";
+import { IHyperdriveDeployer } from "../interfaces/IHyperdriveDeployer.sol";
+import { ERC4626DataProvider } from "../instances/ERC4626DataProvider.sol";
+import { HyperdriveFactory } from "./HyperdriveFactory.sol";
+import { IERC4626 } from "../interfaces/IERC4626.sol";
+
+/// @author DELV
+/// @title ERC4626HyperdriveFactory
+/// @notice Deploys hyperdrive instances and initializes them. It also holds a
+///         registry of all deployed hyperdrive instances.
+/// @custom:disclaimer The language used in this code is for coding convenience
+///                    only, and is not intended to, and does not, have any
+///                    particular legal or regulatory significance.
+contract ERC4626HyperdriveFactory is HyperdriveFactory {
+    IERC4626 internal immutable pool;
+
+    /// @notice Deploys the contract
+    /// @param _governance The address which can update this factory.
+    /// @param _deployer The contract which holds the bytecode and deploys new versions.
+    /// @param _hyperdriveGovernance The address which is set as the governor of hyperdrive
+    /// @param _feeCollector The address which should be set as the fee collector in new deployments
+    /// @param _fees The fees each deployed instance from this contract will have
+    /// @param _defaultPausers The default addresses which will be set to have the pauser role
+    /// @param _pool The Maker ERC4626 manger contract address
+    constructor(
+        address _governance,
+        IHyperdriveDeployer _deployer,
+        address _hyperdriveGovernance,
+        address _feeCollector,
+        IHyperdrive.Fees memory _fees,
+        address[] memory _defaultPausers,
+        IERC4626 _pool
+    )
+        HyperdriveFactory(
+            _governance,
+            _deployer,
+            _hyperdriveGovernance,
+            _feeCollector,
+            _fees,
+            _defaultPausers
+        )
+    {
+        pool = _pool;
+    }
+
+    /// @notice This deploys a data provider for the ERC4626 hyperdrive instance
+    /// @param _config The configuration of the pool we are deploying
+    /// @param _linkerCodeHash The code hash from the multitoken deployer
+    /// @param _linkerFactory The factory of the multitoken deployer
+    function deployDataProvider(
+        IHyperdrive.PoolConfig memory _config,
+        bytes32[] memory,
+        bytes32 _linkerCodeHash,
+        address _linkerFactory
+    ) internal override returns (address) {
+        return (
+            address(
+                new ERC4626DataProvider(
+                    _config,
+                    _linkerCodeHash,
+                    _linkerFactory,
+                    pool
+                )
+            )
+        );
+    }
+}
