0ee55736d8b0b6d874b9f389f6bcea670df3f7ec
Alex Towle <jalextowle@gmail.com>
2023-06-05 14:56:31 -0500

    Update contracts/src/interfaces/IWETH.sol
    Co-authored-by: ControlCplusControlV <44706811+ControlCplusControlV@users.noreply.github.com>

--- contracts/src/interfaces/IWETH.sol
+++ contracts/src/interfaces/IWETH.sol
@@ -6,5 +6,5 @@ import { IERC20 } from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol"
 interface IWETH is IERC20 {
     function deposit() external payable;
 
-    function withdraw(uint256 _amount) external;
+    function withdraw(uint256 wad) external;
 }
