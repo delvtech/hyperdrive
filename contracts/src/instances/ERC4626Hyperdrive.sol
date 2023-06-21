b3aa7e9c97422112aeafbe2f6b6b35a92b22485b
George Towle <george@delv.tech>
2023-06-21 17:12:53 -0500

    Added natspec comments

--- contracts/src/instances/ERC4626Hyperdrive.sol
+++ contracts/src/instances/ERC4626Hyperdrive.sol
@@ -10,7 +10,7 @@ import { IHyperdrive } from "../interfaces/IHyperdrive.sol";
 
 /// @author DELV
 /// @title ERC4626Hyperdrive
-/// @notice An instance of Hyperdrive that utilizes Erc4626's tokenized 
+/// @notice An instance of Hyperdrive that utilizes Erc4626's tokenized
 ///         vault standard as a yield source.
 /// @custom:disclaimer The language used in this code is for coding convenience
 ///                    only, and is not intended to, and does not, have any
