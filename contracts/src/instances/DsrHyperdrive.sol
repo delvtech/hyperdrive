38dd1ffdf5d768a628df3e56af2b22b3da9b6e12
George Towle <george@delv.tech>
2023-06-21 12:16:17 -0500

    parent 0d42bbe55076d9ee354cde21fb71ce0b0ea74345 author George Towle <george@delv.tech> 1687367777 -0500 committer Alex Towle <jalextowle@gmail.com> 1688396906 -0500
    Added configuration for max fees in HyperdriveFactory

--- contracts/src/instances/DsrHyperdrive.sol
+++ contracts/src/instances/DsrHyperdrive.sol
@@ -9,7 +9,7 @@ import { FixedPointMath } from "../libraries/FixedPointMath.sol";
 
 /// @author DELV
 /// @title DsrHyperdrive
-/// @notice An instance of Hyperdrive that utilizes Maker's DSR as a yield source.
+/// @notice An instance of Hyperdrive that utilizes DSR pools as a yield source.
 /// @custom:disclaimer The language used in this code is for coding convenience
 ///                    only, and is not intended to, and does not, have any
 ///                    particular legal or regulatory significance.
