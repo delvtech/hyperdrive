3c8de533a55244bf6fd35cf29563fdeafee559ba
Aleksander Kryukov <firealexkryukov@gmail.com>
2023-06-22 10:49:45 +0100

    BondWrapper setup adjustments

--- certora/helpers/SymbolicHyperdrive/AssetIdMock.sol
+++ certora/helpers/SymbolicHyperdrive/AssetIdMock.sol
@@ -1,7 +1,7 @@
 /// SPDX-License-Identifier: Apache-2.0
 pragma solidity 0.8.19;
 
-import { AssetId } from "../../../contracts/src/libraries/AssetId.sol";
+import { AssetId } from "../../munged/AssetId.sol";
 
 contract AssetIdMock {
 
--- certora/scripts/verifyBondWrapper.sh
+++ certora/scripts/verifyBondWrapper.sh
@@ -15,7 +15,7 @@ certoraRun contracts/src/token/BondWrapper.sol \
     certora/helpers/DummyERC20B.sol \
     certora/helpers/SymbolicHyperdrive/DummyERC20Impl.sol:DummyMintableERC20Impl \
     certora/helpers/SymbolicHyperdrive/AssetIdMock.sol \
-    contracts/src/libraries/AssetId.sol \
+    certora/munged/AssetId.sol \
     \
     --verify BondWrapper:certora/spec/BondWrapper.spec \
     --link BondWrapper:hyperdrive=SymbolicHyperdrive \
