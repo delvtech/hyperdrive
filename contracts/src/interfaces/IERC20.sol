9ff10a20eb60a4ba8179c3b36a6c7a0eff27f281
ControlCplusControlV <44706811+ControlCplusControlV@users.noreply.github.com>
2023-06-28 13:44:42 -0600

    fix review comments

--- contracts/src/interfaces/IERC20.sol
+++ contracts/src/interfaces/IERC20.sol
@@ -1,14 +1,13 @@
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts (last updated v4.9.0) (token/ERC20/IERC20.sol)
-
-import { IERC20 as OZRC20 } from "openzeppelin/token/ERC20/IERC20.sol";
-
 pragma solidity ^0.8.19;
+
+import { IERC20 as OZERC20 } from "openzeppelin/token/ERC20/IERC20.sol";
 
 /**
  * @dev Interface of the ERC20 standard as defined in the EIP.
  */
-interface IERC20 is OZRC20 {
+interface IERC20 is OZERC20 {
     function name() external view returns (string memory);
 
     function symbol() external view returns (string memory);
--- contracts/src/token/BondWrapper.sol
+++ contracts/src/token/BondWrapper.sol
@@ -2,11 +2,11 @@
 pragma solidity 0.8.19;
 
 import { ERC20 } from "solmate/tokens/ERC20.sol";
+import { SafeERC20 } from "openzeppelin/token/ERC20/utils/SafeERC20.sol";
 import { IERC20 } from "../interfaces/IERC20.sol";
 import { IHyperdrive } from "../interfaces/IHyperdrive.sol";
 import { AssetId } from "../libraries/AssetId.sol";
 import { Errors } from "../libraries/Errors.sol";
-import { SafeERC20 } from "openzeppelin/token/ERC20/utils/SafeERC20.sol";
 
 /// @author DELV
 /// @title BondWrapper
