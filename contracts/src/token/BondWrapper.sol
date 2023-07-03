661b4181b990da0ccf01d44ca8a7fb5d8352b2be
Mihai <mcosma@gmail.com>
2023-07-03 16:28:29 +0100

    fix imports (#410)

--- contracts/src/token/BondWrapper.sol
+++ contracts/src/token/BondWrapper.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.19;
 
 import { ERC20 } from "solmate/tokens/ERC20.sol";
-import { SafeERC20 } from "openzeppelin/token/ERC20/utils/SafeERC20.sol";
+import { SafeERC20 } from "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
 import { IERC20 } from "../interfaces/IERC20.sol";
 import { IHyperdrive } from "../interfaces/IHyperdrive.sol";
 import { AssetId } from "../libraries/AssetId.sol";
