bc08588d9dafe57324a62c6b63818ec5872421ff
Alex Towle <jalextowle@gmail.com>
2023-07-03 10:26:58 -0500

    Implemented `MockHyperdriveFactory`

--- test/mocks/MockHyperdriveFactory.sol
+++ test/mocks/MockHyperdriveFactory.sol
@@ -1,22 +1,45 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity 0.8.19;
 
+import { IHyperdrive } from "contracts/src/interfaces/IHyperdrive.sol";
+import { IHyperdriveDeployer } from "contracts/src/interfaces/IHyperdriveDeployer.sol";
 import { HyperdriveFactory } from "contracts/src/factory/HyperdriveFactory.sol";
+import { MockHyperdriveDataProvider } from "test/mocks/MockHyperdrive.sol";
 
-contract MockHyperdriveFactory {
-    function encodeAssetId(
-        AssetId.AssetIdPrefix _prefix,
-        uint256 _timestamp
-    ) external pure returns (uint256) {
-        uint256 id = AssetId.encodeAssetId(_prefix, _timestamp);
-        return id;
-    }
+contract MockHyperdriveFactory is HyperdriveFactory {
+    constructor(
+        address _governance,
+        IHyperdriveDeployer _deployer,
+        address _hyperdriveGovernance,
+        address _feeCollector,
+        IHyperdrive.Fees memory _fees,
+        IHyperdrive.Fees memory _maxFees,
+        address[] memory _defaultPausers,
+        address _linkerFactory,
+        bytes32 _linkerCodeHash
+    )
+        HyperdriveFactory(
+            _governance,
+            _deployer,
+            _hyperdriveGovernance,
+            _feeCollector,
+            _fees,
+            _maxFees,
+            _defaultPausers,
+            _linkerFactory,
+            _linkerCodeHash
+        )
+    {}
 
-    function decodeAssetId(
-        uint256 _id
-    ) external pure returns (AssetId.AssetIdPrefix, uint256) {
-        (AssetId.AssetIdPrefix prefix, uint256 timestamp) = AssetId
-            .decodeAssetId(_id);
-        return (prefix, timestamp);
+    function deployDataProvider(
+        IHyperdrive.PoolConfig memory _config,
+        bytes32[] memory,
+        bytes32,
+        address
+    ) internal override returns (address) {
+        MockHyperdriveDataProvider dataProvider = new MockHyperdriveDataProvider(
+                _config
+            );
+        return address(dataProvider);
     }
 }
