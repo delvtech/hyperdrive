5d4c7f76aeae5bc6d148139e2c53d663c6cff9ee
George Towle <george@delv.tech>
2023-07-21 15:47:30 -0500

    branch work

--- test/units/DataProvider.t.sol
+++ test/units/DataProvider.t.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.18;
 
 import { Test } from "forge-std/Test.sol";
+import { IHyperdrive } from "contracts/src/interfaces/IHyperdrive.sol";
 import { DataProvider } from "contracts/src/DataProvider.sol";
 
 contract MockProvider {
