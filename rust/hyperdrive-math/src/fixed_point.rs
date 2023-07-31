d53b0c607af554ddc09f1acb2986824ee0611897
Alex Towle <jalextowle@gmail.com>
2023-07-31 00:21:43 -0500

    Completed the Rust YieldSpace implementation and fixed tests

--- rust/hyperdrive-math/src/fixed_point.rs
+++ rust/hyperdrive-math/src/fixed_point.rs
@@ -1,8 +1,12 @@
 use ethers::types::{I256, U256};
+use rand::distributions::uniform::{SampleBorrow, SampleUniform, UniformSampler};
 use rand::distributions::{Distribution, Standard};
 use rand::Rng;
-use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Sub, SubAssign};
+use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Shr, Sub, SubAssign};
 
+// FIXME: I should write a macro that makes it easy to specify FixedPoint numbers
+// in Solidity notation. Let's call it "fixed!". It would be awesome to also have
+// macros for "one!()" and "zero!()" that return FixedPoint numbers.
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
 pub struct FixedPoint(U256);
 
@@ -27,12 +31,6 @@ impl From<FixedPoint> for U256 {
 impl From<FixedPoint> for u128 {
     fn from(f: FixedPoint) -> u128 {
         f.0.as_u128()
-    }
-}
-
-impl Distribution<FixedPoint> for Standard {
-    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> FixedPoint {
-        FixedPoint(U256::from(rng.gen::<[u8; 32]>()))
     }
 }
 
@@ -64,6 +62,8 @@ impl SubAssign for FixedPoint {
     }
 }
 
+/// The operator overloaded multiplication is the version that rounds down. A
+/// `mul_up` function is also available.
 impl Mul for FixedPoint {
     type Output = FixedPoint;
 
@@ -78,6 +78,8 @@ impl MulAssign for FixedPoint {
     }
 }
 
+/// The operator overloaded multiplication is the version that rounds down. A
+/// `div_up` function is also available.
 impl Div for FixedPoint {
     type Output = FixedPoint;
 
@@ -187,7 +189,7 @@ impl FixedPoint {
         // Solving this gives k = round(x / log(2)) and x' = x - k * log(2).
         let k: I256 = ((x.wrapping_shl(96) / I256::from(54916777467707473351141471128_u128))
             .wrapping_add(I256::from(2).pow(95)))
-        .wrapping_shr(96);
+        .asr(96);
         x = x.wrapping_sub(k.wrapping_mul(54916777467707473351141471128_u128.into()));
 
         // k is in the range [-61, 195].
@@ -197,14 +199,14 @@ impl FixedPoint {
         let mut y: I256 = x.wrapping_add(1346386616545796478920950773328_u128.into());
         y = y
             .wrapping_mul(x)
-            .wrapping_shr(96)
+            .asr(96)
             .wrapping_add(57155421227552351082224309758442_u128.into());
         let mut p: I256 = y
             .wrapping_add(x)
             .wrapping_sub(94201549194550492254356042504812_u128.into());
         p = p
             .wrapping_mul(y)
-            .wrapping_shr(96)
+            .asr(96)
             .wrapping_add(28719021644029726153956944680412240_u128.into());
         p = p
             .wrapping_mul(x)
@@ -214,28 +216,26 @@ impl FixedPoint {
         let mut q: I256 = x.wrapping_sub(2855989394907223263936484059900_u128.into());
         q = q
             .wrapping_mul(x)
-            .wrapping_shr(96)
+            .asr(96)
             .wrapping_add(50020603652535783019961831881945_u128.into());
         q = q
             .wrapping_mul(x)
-            .wrapping_shr(96)
+            .asr(96)
             .wrapping_sub(533845033583426703283633433725380_u128.into());
         q = q
             .wrapping_mul(x)
-            .wrapping_shr(96)
+            .asr(96)
             .wrapping_add(3604857256930695427073651918091429_u128.into());
         q = q
             .wrapping_mul(x)
-            .wrapping_shr(96)
+            .asr(96)
             .wrapping_sub(14423608567350463180887372962807573_u128.into());
         q = q
             .wrapping_mul(x)
-            .wrapping_shr(96)
+            .asr(96)
             .wrapping_add(26449188498355588339934803723976023_u128.into());
 
         let mut r = p.wrapping_div(q);
-
-        I256::zero();
 
         // r should be in the range (0.09, 0.25) * 2**96.
 
@@ -246,18 +246,20 @@ impl FixedPoint {
         // We do this all at once, with an intermediate result in 2**213
         // basis, so the final right shift is always by a positive amount.
         r = I256::from_raw(
-            r.into_raw()
+            (r.into_raw()
                 .overflowing_mul(U256::from(
                     "0x0000000000000000000000029d9dc38563c32e5c2f6dc192ee70ef65f9978af3",
                 ))
-                .0,
-        )
-        .wrapping_shr(I256::from(195).wrapping_sub(k).low_usize());
+                .0)
+                .shr(I256::from(195).wrapping_sub(k).low_usize()),
+        );
 
         r
     }
 
     // FIXME: If we use statics for the large values, the cost should be very low.
+    // 0x00000000000000000000000000000000000000000003fffff9b1c6c49a60f968
+    // 0xfffffffffffffffffffffffffffffffffffffffffffffffff9b1c6c49a60f968
     fn ln(mut x: I256) -> I256 {
         if x <= I256::zero() {
             panic!("ln of negative number or zero");
@@ -271,40 +273,34 @@ impl FixedPoint {
         let mut r: I256 =
             I256::from((x > I256::from(0xffffffffffffffffffffffffffffffff_u128)) as u128)
                 .wrapping_shl(7);
-        r = r | I256::from(
-            (x.wrapping_shr(r.as_usize()) > I256::from(0xffffffffffffffff_u128)) as u128,
-        )
-        .wrapping_shl(6);
-        r = r | I256::from((x.wrapping_shr(r.as_usize()) > I256::from(0xffffffff_u128)) as u128)
+        r = r | I256::from((x.asr(r.as_usize()) > I256::from(0xffffffffffffffff_u128)) as u128)
+            .wrapping_shl(6);
+        r = r | I256::from((x.asr(r.as_usize()) > I256::from(0xffffffff_u128)) as u128)
             .wrapping_shl(5);
-        r = r | I256::from((x.wrapping_shr(r.as_usize()) > I256::from(0xffff_u128)) as u128)
-            .wrapping_shl(4);
-        r = r | I256::from((x.wrapping_shr(r.as_usize()) > I256::from(0xff_u128)) as u128)
-            .wrapping_shl(3);
-        r = r | I256::from((x.wrapping_shr(r.as_usize()) > I256::from(0xf_u128)) as u128)
-            .wrapping_shl(2);
-        r = r | I256::from((x.wrapping_shr(r.as_usize()) > I256::from(0x3_u128)) as u128)
-            .wrapping_shl(1);
-        r = r | I256::from((x.wrapping_shr(r.as_usize()) > I256::from(0x1_u128)) as u128);
+        r = r | I256::from((x.asr(r.as_usize()) > I256::from(0xffff_u128)) as u128).wrapping_shl(4);
+        r = r | I256::from((x.asr(r.as_usize()) > I256::from(0xff_u128)) as u128).wrapping_shl(3);
+        r = r | I256::from((x.asr(r.as_usize()) > I256::from(0xf_u128)) as u128).wrapping_shl(2);
+        r = r | I256::from((x.asr(r.as_usize()) > I256::from(0x3_u128)) as u128).wrapping_shl(1);
+        r = r | I256::from((x.asr(r.as_usize()) > I256::from(0x1_u128)) as u128);
 
         // Reduce range of x to (1, 2) * 2**96
         // ln(2^k * x) = k * ln(2) + ln(x)
         let k: I256 = r.wrapping_sub(I256::from(96));
         x = x.wrapping_shl(I256::from(159).wrapping_sub(k).as_usize());
-        x = x.wrapping_shr(159);
+        x = I256::from_raw(x.into_raw().shr(159));
 
         // Evaluate using a (8, 8)-term rational approximation.
         // p is made monic, we will multiply by a scale factor later.
         let mut p: I256 = x.wrapping_add(I256::from(3273285459638523848632254066296_u128));
-        p = ((p.wrapping_mul(x)).wrapping_shr(96))
+        p = ((p.wrapping_mul(x)).asr(96))
             .wrapping_add(I256::from(24828157081833163892658089445524_u128));
-        p = ((p.wrapping_mul(x)).wrapping_shr(96))
+        p = ((p.wrapping_mul(x)).asr(96))
             .wrapping_add(I256::from(43456485725739037958740375743393_u128));
-        p = ((p.wrapping_mul(x)).wrapping_shr(96))
+        p = ((p.wrapping_mul(x)).asr(96))
             .wrapping_sub(I256::from(11111509109440967052023855526967_u128));
-        p = ((p.wrapping_mul(x)).wrapping_shr(96))
+        p = ((p.wrapping_mul(x)).asr(96))
             .wrapping_sub(I256::from(45023709667254063763336534515857_u128));
-        p = ((p.wrapping_mul(x)).wrapping_shr(96))
+        p = ((p.wrapping_mul(x)).asr(96))
             .wrapping_sub(I256::from(14706773417378608786704636184526_u128));
         p = p
             .wrapping_mul(x)
@@ -313,27 +309,27 @@ impl FixedPoint {
         // We leave p in 2**192 basis so we don't need to scale it back up for the division.
         // q is monic by convention.
         let mut q: I256 = x.wrapping_add(I256::from(5573035233440673466300451813936_u128));
-        q = (q.wrapping_mul(x).wrapping_shr(96))
+        q = (q.wrapping_mul(x).asr(96))
             .wrapping_add(I256::from(71694874799317883764090561454958_u128));
         q = q
             .wrapping_mul(x)
-            .wrapping_shr(96)
+            .asr(96)
             .wrapping_add(I256::from(283447036172924575727196451306956_u128));
         q = q
             .wrapping_mul(x)
-            .wrapping_shr(96)
+            .asr(96)
             .wrapping_add(I256::from(401686690394027663651624208769553_u128));
         q = q
             .wrapping_mul(x)
-            .wrapping_shr(96)
+            .asr(96)
             .wrapping_add(I256::from(204048457590392012362485061816622_u128));
         q = q
             .wrapping_mul(x)
-            .wrapping_shr(96)
+            .asr(96)
             .wrapping_add(I256::from(31853899698501571402653359427138_u128));
         q = q
             .wrapping_mul(x)
-            .wrapping_shr(96)
+            .asr(96)
             .wrapping_add(I256::from(909429971244387300277376558375_u128));
 
         r = p.wrapping_div(q);
@@ -364,9 +360,64 @@ impl FixedPoint {
             "0x000057115e47018c7177eebf7cd370a3356a1b7863008a5ae8028c72b8864284",
         )));
         // base conversion: mul 2**18 / 2**192
-        r = r.wrapping_shr(174);
+        r = r.asr(174);
 
         r
+    }
+}
+
+impl Distribution<FixedPoint> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> FixedPoint {
+        FixedPoint(U256::from(rng.gen::<[u8; 32]>()))
+    }
+}
+
+pub struct UniformFixedPoint {
+    low: FixedPoint,
+    high: FixedPoint,
+}
+
+impl SampleUniform for FixedPoint {
+    type Sampler = UniformFixedPoint;
+}
+
+impl UniformSampler for UniformFixedPoint {
+    type X = FixedPoint;
+
+    #[inline]
+    fn new<B1, B2>(low_b: B1, high_b: B2) -> Self
+    where
+        B1: SampleBorrow<Self::X> + Sized,
+        B2: SampleBorrow<Self::X> + Sized,
+    {
+        let low = *low_b.borrow();
+        let high = *high_b.borrow();
+        if !(low < high) {
+            panic!("UniformFixedPoint::new called with invalid range");
+        }
+        UniformFixedPoint { low, high }
+    }
+
+    #[inline]
+    fn new_inclusive<B1, B2>(low_b: B1, high_b: B2) -> Self
+    where
+        B1: SampleBorrow<Self::X> + Sized,
+        B2: SampleBorrow<Self::X> + Sized,
+    {
+        let low = *low_b.borrow();
+        let high = *high_b.borrow();
+        if !(low <= high) {
+            panic!("UniformFixedPoint::new called with invalid range");
+        }
+        UniformFixedPoint::new(low, high + FixedPoint::from(1))
+    }
+
+    #[inline]
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> FixedPoint {
+        let value = rng.gen::<FixedPoint>();
+        let size: FixedPoint = self.high - self.low;
+        let narrowed = FixedPoint::from(value.0 % size.0);
+        return narrowed + self.low;
     }
 }
 
@@ -627,6 +678,31 @@ mod tests {
     }
 
     #[tokio::test]
+    async fn fuzz_pow_narrow() -> Result<()> {
+        let runner = setup().await?;
+
+        // Fuzz the rust and solidity implementations against each other.
+        let mut rng = thread_rng();
+        for _ in 0..FUZZ_RUNS {
+            let x: FixedPoint = rng.gen_range(FixedPoint::from(0)..=FixedPoint::one());
+            let y: FixedPoint = rng.gen_range(FixedPoint::from(0)..=FixedPoint::one());
+            let actual = panic::catch_unwind(|| x.pow(y));
+            match runner.mock.pow(x.into(), y.into()).call().await {
+                Ok(expected) => {
+                    println!("x: {:?}", x);
+                    println!("y: {:?}", y);
+                    assert_eq!(actual.unwrap(), FixedPoint::from(expected));
+                }
+                Err(_) => {
+                    let _ = actual.unwrap_err();
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    #[tokio::test]
     async fn fuzz_pow() -> Result<()> {
         let runner = setup().await?;
 
@@ -648,6 +724,26 @@ mod tests {
     }
 
     #[tokio::test]
+    async fn fuzz_exp_narrow() -> Result<()> {
+        let runner = setup().await?;
+
+        // Fuzz the rust and solidity implementations against each other.
+        let mut rng = thread_rng();
+        for _ in 0..FUZZ_RUNS {
+            let x: I256 = I256::from_raw(rng.gen_range(FixedPoint::zero()..=FixedPoint::one()).0);
+            let actual = panic::catch_unwind(|| FixedPoint::ln(x));
+            match runner.mock.ln(x).call().await {
+                Ok(expected) => assert_eq!(actual.unwrap(), expected),
+                Err(_) => {
+                    let _ = actual.unwrap_err();
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    #[tokio::test]
     async fn fuzz_exp() -> Result<()> {
         let runner = setup().await?;
 
@@ -657,6 +753,26 @@ mod tests {
             let x: I256 = I256::from_raw(rng.gen::<FixedPoint>().0);
             let actual = panic::catch_unwind(|| FixedPoint::exp(x));
             match runner.mock.exp(x).call().await {
+                Ok(expected) => assert_eq!(actual.unwrap(), expected),
+                Err(_) => {
+                    let _ = actual.unwrap_err();
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn fuzz_ln_narrow() -> Result<()> {
+        let runner = setup().await?;
+
+        // Fuzz the rust and solidity implementations against each other.
+        let mut rng = thread_rng();
+        for _ in 0..FUZZ_RUNS {
+            let x: I256 = I256::from_raw(rng.gen_range(FixedPoint::zero()..=FixedPoint::one()).0);
+            let actual = panic::catch_unwind(|| FixedPoint::ln(x));
+            match runner.mock.ln(x).call().await {
                 Ok(expected) => assert_eq!(actual.unwrap(), expected),
                 Err(_) => {
                     let _ = actual.unwrap_err();
