4f71cf0bb5571567fd379377157ee95b41f69d4a
Alex Towle <jalextowle@gmail.com>
2023-10-10 21:24:20 -0500

    Negative Interest: Zeta Adjustment (#599)
    * Made some progress on the negative interest issues
    
    * Simplified the mature negative interest flow and fixed the flow for shorts
    
    * Fixed a discrepancy in the negative interest logic in `closeLong`
    
    * Cleanup
    
    * Fixed issue after merge
    
    * Refactored out `sharePaymentWithoutFees`
    
    * Refactored out `shareProceedsWithFees`
    
    * Addressed review feedback from @jrhea
    
    * "Fixed" an intermittent failure in `test_lp_withdrawal_long_and_short_maturity
    
    * Commit intermmediate progress
    
    * Made the share adjustment fixes
    
    * Added a `minSharePrice`
    
    * Fixed some tests
    
    * Fixed the Rust codebase
    
    * Name changes in `HyperdriveLong`
    
    1. `shareReservesDelta` and `bondReservesDelta` always refer to the
       amount of shares or bonds that are being added or subtracted from the
       share reserves.
    2. `shareCurvePayment` refers to the amount of shares priced by the
       curve. We think of this as a payment because the LPs are paying
       shares in return for a proceed in bonds, `bondCurveProceeds`.
    
    * Removed the long share price and did some renaming in `HyperdriveShort`
    
    * De-duplicated the negative interest logic between `closeLong` and `closeShort`
    
    * Renamed `shareCurvePayment` and `shareCurveProceeds` to `shareCurveDelta`
    
    * Negative Interest: Round Trip (#600)
    
    * Added a `minSharePrice`
    
    * Fixed some tests
    
    * Fixed the Rust codebase
    
    * Renamed `bondCurvePayment` and `bondCurveProceeds` to `bondCurveDelta`
    
    * Fixed issues after merge
    
    * Fixed the path independence test and added a test for k invariance
    
    * Addressed review feedback
    
    * Cleaned up the comments
    
    * Fixed a bug in `openShort` caused by negative interest
    
    * Added negative interest to `test_path_independence`

--- contracts/src/HyperdriveShort.sol
+++ contracts/src/HyperdriveShort.sol
@@ -67,12 +67,6 @@ abstract contract HyperdriveShort is HyperdriveLP {
         maturityTime = latestCheckpoint + _positionDuration;
         uint256 shareReservesDelta;
         {
-            // FIXME: After thinking about this a bit, I think that negative
-            // interest makes it possible to open shorts for free. We should
-            // test this edge case as it will cause issues.
-            //
-            // TODO: What happens to the short deposit if the open share price
-            // is greater than the current share price?
             uint256 totalGovernanceFee;
             (
                 traderDeposit,
@@ -436,7 +430,10 @@ abstract contract HyperdriveShort is HyperdriveLP {
 
         // The trader will need to deposit capital to pay for the fixed rate,
         // the curve fee, the flat fee, and any back-paid interest that will be
-        // received back upon closing the trade.
+        // received back upon closing the trade. If negative interest has
+        // accrued during the current checkpoint, we set close share price to
+        // equal the open share price. This ensures that shorts don't benefit
+        // from negative interest that accrued during the current checkpoint.
         traderDeposit = HyperdriveMath
             .calculateShortProceeds(
                 _bondAmount,
@@ -445,7 +442,7 @@ abstract contract HyperdriveShort is HyperdriveLP {
                 // their deposit.
                 shareReservesDelta - totalGovernanceFee,
                 _openSharePrice,
-                _sharePrice,
+                _sharePrice.max(_openSharePrice),
                 _sharePrice,
                 _flatFee
             )
@@ -454,10 +451,6 @@ abstract contract HyperdriveShort is HyperdriveLP {
         return (traderDeposit, shareReservesDelta, totalGovernanceFee);
     }
 
-    // FIXME: We should calculate the share adjustment here. There is a
-    // component of the share adjustment needed for negative interest on the
-    // curve and another for flat updates.
-    //
     /// @dev Calculate the pool reserve and trader deltas that result from
     ///      closing a short. This calculation includes trading fees.
     /// @param _bondAmount The amount of bonds being purchased to close the
