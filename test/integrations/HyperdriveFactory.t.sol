534d55ca052bab4facf325563ef54909325d7460
lucas-manuel <lucasmanuel.tech@gmail.com>
2023-11-12 18:46:50 -0500

    fix: update pragm

--- test/integrations/HyperdriveFactory.t.sol
+++ test/integrations/HyperdriveFactory.t.sol
@@ -1,5 +1,5 @@
 // // SPDX-License-Identifier: Apache-2.0
-// pragma solidity 0.8.19;
+pragma solidity 0.8.19;
 
 import { HyperdriveFactory } from "contracts/src/factory/HyperdriveFactory.sol";
 import { IERC20 } from "contracts/src/interfaces/IERC20.sol";
