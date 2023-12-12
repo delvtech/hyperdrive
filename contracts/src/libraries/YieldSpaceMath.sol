f1673fc23f5b2ea24edf19a8e232edd66a873f20
Alex Towle <jalextowle@gmail.com>
2023-12-12 12:43:12 -0600

    Appeased the linter in `YieldSpaceMath`

--- contracts/src/libraries/YieldSpaceMath.sol
+++ contracts/src/libraries/YieldSpaceMath.sol
@@ -34,7 +34,7 @@ library YieldSpaceMath {
     /// @dev Calculates the amount of bonds a user will receive from the pool by
     ///      providing a specified amount of shares. We underestimate the amount
     ///      of bonds out.
-    /// @param z_e The effective share reserves.
+    /// @param ze The effective share reserves.
     /// @param y The bond reserves.
     /// @param dz The amount of shares paid to the pool.
     /// @param t The time elapsed since the term's start.
@@ -42,7 +42,7 @@ library YieldSpaceMath {
     /// @param mu The initial share price.
     /// @return The amount of bonds the trader receives.
     function calculateBondsOutGivenSharesInDown(
-        uint256 z_e,
+        uint256 ze,
         uint256 y,
         uint256 dz,
         uint256 t,
@@ -51,20 +51,20 @@ library YieldSpaceMath {
     ) internal pure returns (uint256) {
         // NOTE: We round k up to make the rhs of the equation larger.
         //
-        // k = (c / µ) * (µ * z_e)^(1 - t) + y^(1 - t)
-        uint256 k = kUp(z_e, y, t, c, mu);
+        // k = (c / µ) * (µ * ze)^(1 - t) + y^(1 - t)
+        uint256 k = kUp(ze, y, t, c, mu);
 
-        // NOTE: We round z_e down to make the rhs of the equation larger.
+        // NOTE: We round ze down to make the rhs of the equation larger.
         //
-        //  (µ * (z_e + dz))^(1 - t)
-        z_e = mu.mulDown(z_e + dz).pow(t);
-        //  (c / µ) * (µ * (z_e + dz))^(1 - t)
-        z_e = c.mulDivDown(z_e, mu);
+        //  (µ * (ze + dz))^(1 - t)
+        ze = mu.mulDown(ze + dz).pow(t);
+        //  (c / µ) * (µ * (ze + dz))^(1 - t)
+        ze = c.mulDivDown(ze, mu);
 
         // NOTE: We round _y up to make the rhs of the equation larger.
         //
-        // (k - (c / µ) * (µ * (z_e + dz))^(1 - t))^(1 / (1 - t))
-        uint256 _y = k - z_e;
+        // (k - (c / µ) * (µ * (ze + dz))^(1 - t))^(1 / (1 - t))
+        uint256 _y = k - ze;
         if (_y >= ONE) {
             // Rounding up the exponent results in a larger result.
             _y = _y.pow(ONE.divUp(t));
@@ -73,14 +73,14 @@ library YieldSpaceMath {
             _y = _y.pow(ONE.divDown(t));
         }
 
-        // Δy = y - (k - (c / µ) * (µ * (z_e + dz))^(1 - t))^(1 / (1 - t))
+        // Δy = y - (k - (c / µ) * (µ * (ze + dz))^(1 - t))^(1 / (1 - t))
         return y - _y;
     }
 
     /// @dev Calculates the amount of shares a user must provide the pool to
     ///      receive a specified amount of bonds. We overestimate the amount of
     ///      shares in.
-    /// @param z_e The effective share reserves.
+    /// @param ze The effective share reserves.
     /// @param y The bond reserves.
     /// @param dy The amount of bonds paid to the trader.
     /// @param t The time elapsed since the term's start.
@@ -88,7 +88,7 @@ library YieldSpaceMath {
     /// @param mu The initial share price.
     /// @return The amount of shares the trader pays.
     function calculateSharesInGivenBondsOutUp(
-        uint256 z_e,
+        uint256 ze,
         uint256 y,
         uint256 dy,
         uint256 t,
@@ -97,8 +97,8 @@ library YieldSpaceMath {
     ) internal pure returns (uint256) {
         // NOTE: We round k up to make the lhs of the equation larger.
         //
-        // k = (c / µ) * (µ * z_e)^(1 - t) + y^(1 - t)
-        uint256 k = kUp(z_e, y, t, c, mu);
+        // k = (c / µ) * (µ * ze)^(1 - t) + y^(1 - t)
+        uint256 k = kUp(ze, y, t, c, mu);
 
         // (y - dy)^(1 - t)
         y = (y - dy).pow(t);
@@ -117,14 +117,14 @@ library YieldSpaceMath {
         // ((k - (y - dy)^(1 - t) ) / (c / µ))^(1 / (1 - t))) / µ
         _z = _z.divUp(mu);
 
-        // Δz = (((k - (y - dy)^(1 - t) ) / (c / µ))^(1 / (1 - t))) / µ - z_e
-        return _z - z_e;
+        // Δz = (((k - (y - dy)^(1 - t) ) / (c / µ))^(1 / (1 - t))) / µ - ze
+        return _z - ze;
     }
 
     /// @dev Calculates the amount of shares a user must provide the pool to
     ///      receive a specified amount of bonds. We underestimate the amount of
     ///      shares in.
-    /// @param z_e The effective share reserves.
+    /// @param ze The effective share reserves.
     /// @param y The bond reserves.
     /// @param dy The amount of bonds paid to the trader.
     /// @param t The time elapsed since the term's start.
@@ -132,7 +132,7 @@ library YieldSpaceMath {
     /// @param mu The initial share price.
     /// @return The amount of shares the user pays.
     function calculateSharesInGivenBondsOutDown(
-        uint256 z_e,
+        uint256 ze,
         uint256 y,
         uint256 dy,
         uint256 t,
@@ -141,8 +141,8 @@ library YieldSpaceMath {
     ) internal pure returns (uint256) {
         // NOTE: We round k down to make the lhs of the equation smaller.
         //
-        // k = (c / µ) * (µ * z_e)^(1 - t) + y^(1 - t)
-        uint256 k = kDown(z_e, y, t, c, mu);
+        // k = (c / µ) * (µ * ze)^(1 - t) + y^(1 - t)
+        uint256 k = kDown(ze, y, t, c, mu);
 
         // (y - dy)^(1 - t)
         y = (y - dy).pow(t);
@@ -161,15 +161,15 @@ library YieldSpaceMath {
         // ((k - (y - dy)^(1 - t) ) / (c / µ))^(1 / (1 - t))) / µ
         _z = _z.divDown(mu);
 
-        // Δz = (((k - (y - dy)^(1 - t) ) / (c / µ))^(1 / (1 - t))) / µ - z_e
-        return _z - z_e;
+        // Δz = (((k - (y - dy)^(1 - t) ) / (c / µ))^(1 / (1 - t))) / µ - ze
+        return _z - ze;
     }
 
     /// @dev Calculates the amount of shares a user will receive from the pool
     ///      by providing a specified amount of bonds. This function reverts if
     ///      an integer overflow or underflow occurs. We underestimate the
     ///      amount of shares out.
-    /// @param z_e The effective share reserves.
+    /// @param ze The effective share reserves.
     /// @param y The bond reserves.
     /// @param dy The amount of bonds paid to the pool.
     /// @param t The time elapsed since the term's start.
@@ -177,7 +177,7 @@ library YieldSpaceMath {
     /// @param mu The initial share price.
     /// @return result The amount of shares the user receives.
     function calculateSharesOutGivenBondsInDown(
-        uint256 z_e,
+        uint256 ze,
         uint256 y,
         uint256 dy,
         uint256 t,
@@ -186,7 +186,7 @@ library YieldSpaceMath {
     ) internal pure returns (uint256 result) {
         bool success;
         (result, success) = calculateSharesOutGivenBondsInDownSafe(
-            z_e,
+            ze,
             y,
             dy,
             t,
@@ -202,7 +202,7 @@ library YieldSpaceMath {
     ///      by providing a specified amount of bonds. This function returns a
     ///      success flag instead of reverting. We underestimate the amount of
     ///      shares out.
-    /// @param z_e The effective share reserves.
+    /// @param ze The effective share reserves.
     /// @param y The bond reserves.
     /// @param dy The amount of bonds paid to the pool.
     /// @param t The time elapsed since the term's start.
@@ -211,7 +211,7 @@ library YieldSpaceMath {
     /// @return result The amount of shares the user receives
     /// @return success A flag indicating if the calculation succeeded.
     function calculateSharesOutGivenBondsInDownSafe(
-        uint256 z_e,
+        uint256 ze,
         uint256 y,
         uint256 dy,
         uint256 t,
@@ -220,8 +220,8 @@ library YieldSpaceMath {
     ) internal pure returns (uint256 result, bool success) {
         // NOTE: We round k up to make the rhs of the equation larger.
         //
-        // k = (c / µ) * (µ * z_e)^(1 - t) + y^(1 - t)
-        uint256 k = kUp(z_e, y, t, c, mu);
+        // k = (c / µ) * (µ * ze)^(1 - t) + y^(1 - t)
+        uint256 k = kUp(ze, y, t, c, mu);
 
         // (y + dy)^(1 - t)
         y = (y + dy).pow(t);
@@ -245,23 +245,23 @@ library YieldSpaceMath {
         // ((k - (y + dy)^(1 - t) ) / (c / µ))^(1 / (1 - t))) / µ
         _z = _z.divUp(mu);
 
-        // Δz = z_e - ((k - (y + dy)^(1 - t) ) / (c / µ))^(1 / (1 - t)) / µ
-        if (z_e > _z) {
-            result = z_e - _z;
+        // Δz = ze - ((k - (y + dy)^(1 - t) ) / (c / µ))^(1 / (1 - t)) / µ
+        if (ze > _z) {
+            result = ze - _z;
         }
         success = true;
     }
 
     /// @dev Calculates the share payment required to purchase the maximum
     ///      amount of bonds from the pool.
-    /// @param z_e The effective share reserves.
+    /// @param ze The effective share reserves.
     /// @param y The bond reserves.
     /// @param t The time elapsed since the term's start.
     /// @param c The share price.
     /// @param mu The initial share price.
     /// @return The share payment to purchase the maximum amount of bonds.
     function calculateMaxBuySharesIn(
-        uint256 z_e,
+        uint256 ze,
         uint256 y,
         uint256 t,
         uint256 c,
@@ -269,40 +269,40 @@ library YieldSpaceMath {
     ) internal pure returns (uint256) {
         // We solve for the maximum buy using the constraint that the pool's
         // spot price can never exceed 1. We do this by noting that a spot price
-        // of 1, ((mu * z_e) / y) ** tau = 1, implies that mu * z_e = y. This
+        // of 1, ((mu * ze) / y) ** tau = 1, implies that mu * ze = y. This
         // simplifies YieldSpace to:
         //
-        // k = ((c / mu) + 1) * (mu * z_e') ** (1 - tau),
+        // k = ((c / mu) + 1) * (mu * ze') ** (1 - tau),
         //
         // This gives us the maximum effective share reserves of:
         //
-        // z_e' = (1 / mu) * (k / ((c / mu) + 1)) ** (1 / (1 - tau)).
-        uint256 k = kDown(z_e, y, t, c, mu);
-        uint256 optimal_z_e = k.divDown(c.divUp(mu) + ONE);
-        if (optimal_z_e >= ONE) {
+        // ze' = (1 / mu) * (k / ((c / mu) + 1)) ** (1 / (1 - tau)).
+        uint256 k = kDown(ze, y, t, c, mu);
+        uint256 optimalZe = k.divDown(c.divUp(mu) + ONE);
+        if (optimalZe >= ONE) {
             // Rounding the exponent down results in a smaller outcome.
-            optimal_z_e = optimal_z_e.pow(ONE.divDown(t));
+            optimalZe = optimalZe.pow(ONE.divDown(t));
         } else {
             // Rounding the exponent up results in a smaller outcome.
-            optimal_z_e = optimal_z_e.pow(ONE.divUp(t));
+            optimalZe = optimalZe.pow(ONE.divUp(t));
         }
-        optimal_z_e = optimal_z_e.divDown(mu);
+        optimalZe = optimalZe.divDown(mu);
 
-        // The optimal trade size is given by dz = z_e' - z_e.
-        return optimal_z_e - z_e;
+        // The optimal trade size is given by dz = ze' - ze.
+        return optimalZe - ze;
     }
 
     /// @dev Calculates the maximum amount of bonds that can be purchased with
     ///      the specified reserves. We round so that the max buy amount is
     ///      underestimated.
-    /// @param z_e The effective share reserves.
+    /// @param ze The effective share reserves.
     /// @param y The bond reserves.
     /// @param t The time elapsed since the term's start.
     /// @param c The share price.
     /// @param mu The initial share price.
     /// @return The maximum amount of bonds that can be purchased.
     function calculateMaxBuyBondsOut(
-        uint256 z_e,
+        uint256 ze,
         uint256 y,
         uint256 t,
         uint256 c,
@@ -312,36 +312,27 @@ library YieldSpaceMath {
         // calculate the minimum bond reserves as:
         //
         // y' = (k / ((c / mu) + 1)) ** (1 / (1 - tau)).
-        uint256 k = kUp(z_e, y, t, c, mu);
-        uint256 optimal_y = k.divUp(c.divDown(mu) + ONE);
-        if (optimal_y >= ONE) {
+        uint256 k = kUp(ze, y, t, c, mu);
+        uint256 optimalY = k.divUp(c.divDown(mu) + ONE);
+        if (optimalY >= ONE) {
             // Rounding the exponent up results in a larger outcome.
-            optimal_y = optimal_y.pow(ONE.divUp(t));
+            optimalY = optimalY.pow(ONE.divUp(t));
         } else {
             // Rounding the exponent down results in a larger outcome.
-            optimal_y = optimal_y.pow(ONE.divDown(t));
+            optimalY = optimalY.pow(ONE.divDown(t));
         }
 
         // The optimal trade size is given by dy = y - y'.
-        return y - optimal_y;
+        return y - optimalY;
     }
 
-    // FIXME: If the share adjustment is negative, then this isn't right.
-    //        With this in mind, I can do one of two things. First, I can
-    //        just pass in the share reserves and share adjustment. Second, I
-    //        can pass in a larger minimum share reserves in this case (since
-    //        the share reserves can never fall below the minimum share reserves,
-    //        our max delta is really:
-    //
-    //
-    //
     /// @dev Calculates the maximum amount of bonds that can be sold with the
     ///      specified reserves. We round so that the max sell amount is
     ///      underestimated.
     /// @param z The share reserves.
     /// @param zeta The share adjustment.
     /// @param y The bond reserves.
-    /// @param z_min The minimum share reserves.
+    /// @param zMin The minimum share reserves.
     /// @param t The time elapsed since the term's start.
     /// @param c The share price.
     /// @param mu The initial share price.
@@ -351,88 +342,88 @@ library YieldSpaceMath {
         uint256 z,
         int256 zeta,
         uint256 y,
-        uint256 z_min,
+        uint256 zMin,
         uint256 t,
         uint256 c,
         uint256 mu
     ) internal pure returns (uint256, bool) {
         // If the share adjustment is negative, the minimum share reserves is
-        // given by `z_min - zeta`, which ensures that the share reserves never
+        // given by `zMin - zeta`, which ensures that the share reserves never
         // fall below the minimum share reserves. Otherwise, the minimum share
-        // reserves is just z_min.
+        // reserves is just zMin.
         if (zeta < 0) {
-            z_min = z_min + uint256(-zeta);
+            zMin = zMin + uint256(-zeta);
         }
 
         // We solve for the maximum bond amount using the constraint that the
         // pool's share reserves can never fall below the minimum share reserves
-        // `z_min`. Substituting `z_e = z_min` simplifies YieldSpace to:
+        // `zMin`. Substituting `ze = zMin` simplifies YieldSpace to:
         //
-        // k = (c / mu) * (mu * z_min) ** (1 - tau) + y' ** (1 - tau)
+        // k = (c / mu) * (mu * zMin) ** (1 - tau) + y' ** (1 - tau)
         //
         // This gives us the maximum bonds that can be sold to the pool as:
         //
-        // y' = (k - (c / mu) * (mu * z_min) ** (1 - tau)) ** (1 / (1 - tau)).
-        uint256 z_e = HyperdriveMath.calculateEffectiveShareReserves(z, zeta);
-        uint256 k = kDown(z_e, y, t, c, mu);
-        uint256 optimal_y = k - c.mulDivUp(mu.mulUp(z_min).pow(t), mu);
-        if (optimal_y >= ONE) {
+        // y' = (k - (c / mu) * (mu * zMin) ** (1 - tau)) ** (1 / (1 - tau)).
+        uint256 ze = HyperdriveMath.calculateEffectiveShareReserves(z, zeta);
+        uint256 k = kDown(ze, y, t, c, mu);
+        uint256 optimalY = k - c.mulDivUp(mu.mulUp(zMin).pow(t), mu);
+        if (optimalY >= ONE) {
             // Rounding the exponent down results in a smaller outcome.
-            optimal_y = optimal_y.pow(ONE.divDown(t));
+            optimalY = optimalY.pow(ONE.divDown(t));
         } else {
             // Rounding the exponent up results in a smaller outcome.
-            optimal_y = optimal_y.pow(ONE.divUp(t));
+            optimalY = optimalY.pow(ONE.divUp(t));
         }
 
         // The optimal trade size is given by dy = y' - y. If this subtraction
         // will underflow, we return a failure flag.
-        if (optimal_y < y) {
+        if (optimalY < y) {
             return (0, false);
         }
-        return (optimal_y - y, true);
+        return (optimalY - y, true);
     }
 
     /// @dev Calculates the YieldSpace invariant k. This invariant is given by:
     ///
-    ///      k = (c / µ) * (µ * z_e)^(1 - t) + y^(1 - t)
+    ///      k = (c / µ) * (µ * ze)^(1 - t) + y^(1 - t)
     ///
     ///      This variant of the calculation overestimates the result.
-    /// @param z_e The effective share reserves.
+    /// @param ze The effective share reserves.
     /// @param y The bond reserves.
     /// @param t The time elapsed since the term's start.
     /// @param c The share price.
     /// @param mu The initial share price.
     /// @return The YieldSpace invariant, k.
     function kUp(
-        uint256 z_e,
+        uint256 ze,
         uint256 y,
         uint256 t,
         uint256 c,
         uint256 mu
     ) internal pure returns (uint256) {
-        /// k = (c / µ) * (µ * z_e)^(1 - t) + y^(1 - t)
-        return c.mulDivUp(mu.mulUp(z_e).pow(t), mu) + y.pow(t);
+        /// k = (c / µ) * (µ * ze)^(1 - t) + y^(1 - t)
+        return c.mulDivUp(mu.mulUp(ze).pow(t), mu) + y.pow(t);
     }
 
     /// @dev Calculates the YieldSpace invariant k. This invariant is given by:
     ///
-    ///      k = (c / µ) * (µ * z_e)^(1 - t) + y^(1 - t)
+    ///      k = (c / µ) * (µ * ze)^(1 - t) + y^(1 - t)
     ///
     ///      This variant of the calculation underestimates the result.
-    /// @param z_e The effective share reserves.
+    /// @param ze The effective share reserves.
     /// @param y The bond reserves.
     /// @param t The time elapsed since the term's start.
     /// @param c The share price.
     /// @param mu The initial share price.
     /// @return The modified YieldSpace Constant.
     function kDown(
-        uint256 z_e,
+        uint256 ze,
         uint256 y,
         uint256 t,
         uint256 c,
         uint256 mu
     ) internal pure returns (uint256) {
-        /// k = (c / µ) * (µ * z_e)^(1 - t) + y^(1 - t)
-        return c.mulDivDown(mu.mulDown(z_e).pow(t), mu) + y.pow(t);
+        /// k = (c / µ) * (µ * ze)^(1 - t) + y^(1 - t)
+        return c.mulDivDown(mu.mulDown(ze).pow(t), mu) + y.pow(t);
     }
 }
