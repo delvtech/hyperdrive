b82de8dbbaa2e98af43402a929b54d3aba0bb4c0
Dylan <dpaiton@gmail.com>
2024-01-16 15:34:01 -0800

    removes weird extra import

--- crates/hyperdrive-math/src/utils.rs
+++ crates/hyperdrive-math/src/utils.rs
@@ -1,7 +1,4 @@
-use ethers::{
-    core::k256::elliptic_curve::consts::U2,
-    types::{I256, U256},
-};
+use ethers::types::{I256, U256};
 use fixed_point::FixedPoint;
 use fixed_point_macros::{fixed, uint256};
 
