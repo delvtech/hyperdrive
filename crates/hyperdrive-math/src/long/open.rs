33705cf3a8ecf39068787a68ee75806b6b0862dc
Sheng Lundquist <sheng@delv.tech>
2024-02-08 14:51:49 -0800

    Fixing bug and avoiding duplicate calculation

--- crates/hyperdrive-math/src/long/open.rs
+++ crates/hyperdrive-math/src/long/open.rs
@@ -27,7 +27,7 @@ impl State {
         let long_amount =
             self.calculate_bonds_out_given_shares_in_down(base_amount / self.vault_share_price());
         
-        let ending_spot_price = self.get_spot_price_after_long(long_amount);
+        let ending_spot_price = self.spot_price_after_long(base_amount, long_amount);
         let max_spot_price = self.get_max_spot_price();
         if ending_spot_price > max_spot_price{
             panic!("InsufficientLiquidity: Negative Interest");
@@ -43,12 +43,16 @@ impl State {
 
     /// Gets the spot price after opening the long on the YieldSpace curve and
     /// before calculating the fees.
-    pub fn get_spot_price_after_long(&self, long_amount: FixedPoint) -> FixedPoint {
+    pub fn get_spot_price_after_long(&self, base_amount: FixedPoint) -> FixedPoint {
+        let bond_amount = self.calculate_bonds_out_given_shares_in_down(base_amount / self.vault_share_price());
+        self.spot_price_after_long(base_amount, bond_amount)
+    }
+
+    fn spot_price_after_long(&self, base_amount: FixedPoint, bond_amount: FixedPoint) -> FixedPoint {
         let mut state: State = self.clone();
-        state.info.bond_reserves -= state
-            .calculate_bonds_out_given_shares_in_down(long_amount / state.vault_share_price())
-            .into();
-        state.info.share_reserves += (long_amount / state.vault_share_price()).into();
+        state.info.bond_reserves -= bond_amount.into();
+        state.info.share_reserves += (base_amount / state.vault_share_price()).into();
         state.get_spot_price()
     }
+
 }
