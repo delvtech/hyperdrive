b547027c50d4e1be1b852cc36dae5dced5a55493
Alex Towle <jalextowle@gmail.com>
2024-02-25 00:14:29 -0600

    Removed duplicate check in `shouldShortCircuitDistributeExcessIdleShareProceeds`

--- contracts/src/libraries/LPMath.sol
+++ contracts/src/libraries/LPMath.sol
@@ -893,9 +893,6 @@ library LPMath {
         uint256 lpSharePriceAfter = _presentValue.divDown(
             _params.activeLpTotalSupply
         );
-        if (lpSharePriceAfter < lpSharePriceBefore) {
-            return false;
-        }
         return
             lpSharePriceAfter >= lpSharePriceBefore &&
             lpSharePriceAfter <=
