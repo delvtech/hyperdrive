29443da2dfb0f5b4a733dc948dd90ca85c8ff798
Alex Towle <jalextowle@gmail.com>
2024-02-25 00:22:59 -0600

    Avoid unnecessary subtraction in `calculateSharesOutGivenBondsInDerivativeSafe`

--- contracts/src/libraries/LPMath.sol
+++ contracts/src/libraries/LPMath.sol
@@ -1070,7 +1070,7 @@ library LPMath {
         );
 
         // derivative = 1 - derivative
-        if (ONE >= derivative) {
+        if (ONE > derivative) {
             derivative = ONE - derivative;
         } else {
             // NOTE: Small rounding errors can result in the derivative being
