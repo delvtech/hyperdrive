c11b2d164d58153d5c282d357e316183ea4ed16c
Cash Baller <cashd@users.noreply.github.com>
2024-03-12 14:33:16 -0500

    made basePaidAfterFee function clearer

---
+++ contracts/src/interfaces/IRocketPoolDAOProtocolSettingsDeposit.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity 0.8.20;
+
+interface IRocketPoolDAOProtocolSettingsDeposit {
+    function getDepositEnabled() external view returns (bool);
+
+    function getAssignDepositsEnabled() external view returns (bool);
+
+    function getMinimumDeposit() external view returns (uint256);
+
+    function getMaximumDepositPoolSize() external view returns (uint256);
+
+    function getMaximumDepositAssignments() external view returns (uint256);
+
+    function getMaximumDepositSocialisedAssignments()
+        external
+        view
+        returns (uint256);
+
+    function getDepositFee() external view returns (uint256);
+}
