0c30b201fb6946ce6c5ec7e3287a078ac90f1f63
Cash Baller <cashd@users.noreply.github.com>
2024-03-13 20:48:35 -0500

    deployer contracts

---
+++ contracts/src/deployers/lseth/LsETHHyperdriveDeployerCoordinator.sol
@@ -0,0 +1,117 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity 0.8.20;
+
+import { IHyperdrive } from "../../interfaces/IHyperdrive.sol";
+import { IHyperdriveDeployerCoordinator } from "../../interfaces/IHyperdriveDeployerCoordinator.sol";
+import { IRiverV1 } from "../../interfaces/lseth/IRiverV1.sol";
+import { FixedPointMath, ONE } from "../../libraries/FixedPointMath.sol";
+import { HyperdriveDeployerCoordinator } from "../HyperdriveDeployerCoordinator.sol";
+
+/// @author DELV
+/// @title LsETHHyperdriveDeployerCoordinator
+/// @notice The deployer coordinator for the LsETHHyperdrive implementation.
+/// @custom:disclaimer The language used in this code is for coding convenience
+///                    only, and is not intended to, and does not, have any
+///                    particular legal or regulatory significance.
+contract LsETHHyperdriveDeployerCoordinator is HyperdriveDeployerCoordinator {
+    using FixedPointMath for uint256;
+
+    /// @dev The Lido contract.
+    IRiverV1 internal immutable river;
+
+    /// @notice Instantiates the deployer coordinator.
+    /// @param _coreDeployer The core deployer.
+    /// @param _target0Deployer The target0 deployer.
+    /// @param _target1Deployer The target1 deployer.
+    /// @param _target2Deployer The target2 deployer.
+    /// @param _target3Deployer The target3 deployer.
+    /// @param _target4Deployer The target4 deployer.
+    /// @param _river The Lido contract.
+    constructor(
+        address _coreDeployer,
+        address _target0Deployer,
+        address _target1Deployer,
+        address _target2Deployer,
+        address _target3Deployer,
+        address _target4Deployer,
+        IRiverV1 _river
+    )
+        HyperdriveDeployerCoordinator(
+            _coreDeployer,
+            _target0Deployer,
+            _target1Deployer,
+            _target2Deployer,
+            _target3Deployer,
+            _target4Deployer
+        )
+    {
+        river = _river;
+    }
+
+    /// @dev Prepares the coordinator for initialization by drawing funds from
+    ///      the LP, if necessary.
+    /// @param _hyperdrive The Hyperdrive instance that is being initialized.
+    /// @param _lp The LP that is initializing the pool.
+    /// @param _contribution The amount of capital to supply. The units of this
+    ///        quantity are either base or vault shares, depending on the value
+    ///        of `_options.asBase`.
+    /// @param _options The options that configure how the initialization is
+    ///        settled.
+    /// @return value The value that should be sent in the initialize
+    ///         transaction.
+    function _prepareInitialize(
+        IHyperdrive _hyperdrive,
+        address _lp,
+        uint256 _contribution,
+        IHyperdrive.Options memory _options
+    ) internal override returns (uint256 value) {
+        // If base is the deposit asset, ensure that enough ether was sent to
+        // the contract and return the amount of ether that should be sent for
+        // the contribution.
+        if (_options.asBase) {
+            revert IHyperdrive.UnsupportedToken();
+        }
+        // Otherwise, transfer vault shares from the LP and approve the
+        // Hyperdrive pool.
+        river.transferFrom(_lp, address(this), _contribution);
+        river.approve(address(_hyperdrive), _contribution);
+
+        return value;
+    }
+
+    /// @dev Allows the contract to receive ether.
+    function _checkMessageValue() internal view override {}
+
+    /// @notice Checks the pool configuration to ensure that it is valid.
+    /// @param _deployConfig The deploy configuration of the Hyperdrive pool.
+    function _checkPoolConfig(
+        IHyperdrive.PoolDeployConfig memory _deployConfig
+    ) internal view override {
+        // Perform the default checks.
+        super._checkPoolConfig(_deployConfig);
+
+        // Ensure that the minimum share reserves are equal to 1e15. This value
+        // has been tested to prevent arithmetic overflows in the
+        // `_updateLiquidity` function when the share reserves are as high as
+        // 200 million.
+        if (_deployConfig.minimumShareReserves != 1e15) {
+            revert IHyperdriveDeployerCoordinator.InvalidMinimumShareReserves();
+        }
+
+        // Ensure that the minimum transaction amount are equal to 1e15. This
+        // value has been tested to prevent precision issues.
+        if (_deployConfig.minimumTransactionAmount != 1e15) {
+            revert IHyperdriveDeployerCoordinator
+                .InvalidMinimumTransactionAmount();
+        }
+    }
+
+    /// @dev Gets the initial vault share price of the Hyperdrive pool.
+    /// @return The initial vault share price of the Hyperdrive pool.
+    function _getInitialVaultSharePrice(
+        bytes memory // unused extra data
+    ) internal view override returns (uint256) {
+        // Return LsETH's current vault share price.
+        return river.sharesFromUnderlyingBalance(ONE);
+    }
+}
---
+++ contracts/src/deployers/lseth/LsETHTarget0Deployer.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity 0.8.20;
+
+import { LsETHTarget0 } from "../../instances/lseth/LsETHTarget0.sol";
+import { IHyperdrive } from "../../interfaces/IHyperdrive.sol";
+import { IHyperdriveTargetDeployer } from "../../interfaces/IHyperdriveTargetDeployer.sol";
+import { IRiverV1 } from "../../interfaces/lseth/IRiverV1.sol";
+
+/// @author DELV
+/// @title LsETHTarget0Deployer
+/// @notice The target0 deployer for the LsETHHyperdrive implementation.
+/// @custom:disclaimer The language used in this code is for coding convenience
+///                    only, and is not intended to, and does not, have any
+///                    particular legal or regulatory significance.
+contract LsETHTarget0Deployer is IHyperdriveTargetDeployer {
+    /// @dev The Lido contract.
+    IRiverV1 internal immutable _river;
+
+    /// @notice Instantiates the target0 deployer.
+    /// @param __river The Lido contract.
+    constructor(IRiverV1 __river) {
+        _river = __river;
+    }
+
+    /// @notice Deploys a target0 instance with the given parameters.
+    /// @param _config The configuration of the Hyperdrive pool.
+    /// @param _salt The create2 salt used in the deployment.
+    /// @return The address of the newly deployed LsETHTarget0 instance.
+    function deploy(
+        IHyperdrive.PoolConfig memory _config,
+        bytes memory, // unused extra data
+        bytes32 _salt
+    ) external override returns (address) {
+        return
+            address(
+                // NOTE: We hash the sender with the salt to prevent the
+                // front-running of deployments.
+                new LsETHTarget0{
+                    salt: keccak256(abi.encode(msg.sender, _salt))
+                }(_config, _river)
+            );
+    }
+}
