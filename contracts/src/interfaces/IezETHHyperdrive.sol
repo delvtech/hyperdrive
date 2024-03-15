8500531ab596f77d857684118f4ea01b00b2735d
Matthew Brown <mattfdbrown@gmail.com>
2024-03-15 12:33:01 -0700

    remove IEzETHHyperdriveCore.sol

--- contracts/src/interfaces/IezETHHyperdrive.sol
+++ contracts/src/interfaces/IezETHHyperdrive.sol
@@ -2,12 +2,10 @@
 pragma solidity 0.8.20;
 
 import { IHyperdrive } from "./IHyperdrive.sol";
-import { IEzETHHyperdriveCore } from "./IEzETHHyperdriveCore.sol";
 import { IEzETHHyperdriveRead } from "./IEzETHHyperdriveRead.sol";
 
 // prettier-ignore
 interface IEzETHHyperdrive is
     IHyperdrive,
-    IEzETHHyperdriveRead,
-    IEzETHHyperdriveCore
+    IEzETHHyperdriveRead
 {}
