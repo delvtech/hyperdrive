1c37a1731621dc73e536b1b36ba8e4f1f594feb8
Matthew Brown <mattfdbrown@gmail.com>
2024-03-20 14:41:32 -0500

    setup the directory

--- codegen/hyperdrive_codegen/codegen.py
+++ codegen/hyperdrive_codegen/codegen.py
@@ -6,7 +6,7 @@ from pathlib import Path
 import yaml
 
 from hyperdrive_codegen.config import TemplateConfig
-from hyperdrive_codegen.file import write_string_to_file
+from hyperdrive_codegen.file import get_output_folder_structure, setup_directory, write_string_to_file
 from hyperdrive_codegen.jinja import get_jinja_env
 
 
@@ -28,7 +28,9 @@ def codegen(config_file_path: Path | str, output_dir: Path | str):
     rendered_code = core_deployer_template.render(name=template_config.name)
 
     # write to file
+    folder_structure = get_output_folder_structure(template_config.name.lowercase)
+    setup_directory(output_dir, folder_structure, True)
     output_path = Path(output_dir)
     contract_file_name = f"{template_config.name.capitalized}HyperdriveCoreDeployer.sol"
-    contract_file_path = Path(os.path.join(output_path, contract_file_name))
+    contract_file_path = Path(os.path.join(output_path, "deployers", contract_file_name))
     write_string_to_file(contract_file_path, rendered_code)
