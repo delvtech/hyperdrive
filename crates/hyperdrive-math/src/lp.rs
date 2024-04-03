b7fb36b8d7cfaf3a9e2f2be26363001f0662429d
Matthew Brown <mattfdbrown@gmail.com>
2024-04-03 10:27:41 -0500

    add calculate_add_liquidity function

--- crates/hyperdrive-math/src/lp.rs
+++ crates/hyperdrive-math/src/lp.rs
@@ -1,10 +1,10 @@
-use std::cmp::Ordering;
+use std::cmp::{max, Ordering};
 
 use ethers::types::{I256, U256};
 use fixed_point::FixedPoint;
 use fixed_point_macros::{fixed, int256};
 
-use crate::{State, YieldSpace};
+use crate::{get_effective_share_reserves, State, YieldSpace};
 
 impl State {
     /// Calculates the number of base that are not reserved by open positions.
@@ -184,6 +184,134 @@ impl State {
                 self.vault_share_price(),
             ))
             .unwrap()
+    }
+
+    pub fn calculate_add_liquidity(
+        &self,
+        current_block_timestamp: U256,
+        contribution: FixedPoint,
+        min_apr: Option<FixedPoint>,
+        max_apr: Option<FixedPoint>,
+    ) -> Result<FixedPoint, &str> {
+        // Ensure that the contribution is greater than or equal to the minimum
+        // transaction amount.
+        if contribution < self.minimum_transaction_amount() {
+            return Err("MinimumTransactionAmount");
+        }
+
+        // Enforce the slippage guard.
+        if min_apr.is_some() && max_apr.is_some() {
+            let apr = self.get_spot_rate();
+            let invalid_apr = apr < min_apr.unwrap() || apr > max_apr.unwrap();
+            if invalid_apr {
+                return Err("InvalidApr");
+            }
+        }
+
+        // Get withdraw_shares_outstanding and lp_total_supply for the lp_shares calculation.
+        let withdraw_shares_outstanding =
+            self.withdrawal_shares_proceeds() - self.withdrawal_shares_ready_to_withdraw();
+        let lp_total_supply = self.lp_total_supply() - withdraw_shares_outstanding;
+
+        // Get the starting_present_value.
+        let starting_present_value = self.calculate_present_value(current_block_timestamp);
+
+        // Get the ending_present_value.
+        let share_contribution = I256::try_from(contribution * self.vault_share_price()).unwrap();
+        let new_state = self.update_liquidity(share_contribution);
+        let ending_present_value = new_state.calculate_present_value(current_block_timestamp);
+
+        // Ensure the present value didn't decreased after adding liquidity.
+        if ending_present_value < starting_present_value {
+            return Err("DecreasedPresentValueWhenAddingLiquidity");
+        }
+
+        // Calculate the lp_shares.
+        let lp_shares = (ending_present_value - starting_present_value)
+            .mul_div_down(lp_total_supply, starting_present_value);
+
+        // Ensure that enough lp_shares are minted so that they can be redeemed.
+        if lp_shares < self.minimum_transaction_amount() {
+            return Err("MinimumTransactionAmount");
+        }
+
+        // TODO: add the solidity _distributeExcessIdleSafe check here.
+
+        return Ok(lp_shares);
+    }
+
+    fn update_liquidity(&self, share_reserves_delta: I256) -> State {
+        let share_reserves = self.share_reserves();
+        let share_adjustment = self.share_adjustment();
+        let bond_reserves = self.bond_reserves();
+        let minimum_share_reserves = self.minimum_share_reserves();
+
+        // Calculate new reserve and adjustment levels.
+        let (updated_share_reserves, updated_share_adjustment, updated_bond_reserves) = self
+            .calculate_update_liquidity(
+                share_reserves,
+                share_adjustment,
+                bond_reserves,
+                minimum_share_reserves,
+                share_reserves_delta,
+            )
+            .unwrap();
+
+        // Update and return the new state.
+        let mut new_info = self.info.clone();
+        new_info.share_reserves = U256::from(updated_share_reserves);
+        new_info.share_adjustment = updated_share_adjustment;
+        new_info.bond_reserves = U256::from(updated_bond_reserves);
+        State {
+            config: self.config.clone(),
+            info: new_info,
+        }
+    }
+
+    fn calculate_update_liquidity(
+        &self,
+        share_reserves: FixedPoint,
+        share_adjustment: I256,
+        bond_reserves: FixedPoint,
+        minimum_share_reserves: FixedPoint,
+        share_reserves_delta: I256,
+    ) -> Result<(FixedPoint, I256, FixedPoint), &'static str> {
+        if share_reserves_delta == I256::zero() {
+            return Ok((share_reserves, share_adjustment, bond_reserves));
+        }
+
+        // Get the updated share reserves.
+        let new_share_reserves = if share_reserves_delta > I256::zero() {
+            share_reserves + FixedPoint::from(share_reserves_delta)
+        } else {
+            share_reserves - FixedPoint::from(share_reserves_delta)
+        };
+
+        // Ensure the minimum share reserve level.
+        if new_share_reserves < minimum_share_reserves {
+            return Err("Update would result in share reserves below minimum.");
+        }
+
+        // Get the updated share adjustment.
+        let new_share_adjustment = if share_adjustment >= I256::zero() {
+            let share_adjustment_fp = FixedPoint::from(share_adjustment);
+            I256::try_from(new_share_reserves.mul_div_down(share_adjustment_fp, share_reserves))
+                .unwrap()
+        } else {
+            let share_adjustment_fp = FixedPoint::from(-share_adjustment);
+            -I256::try_from(new_share_reserves.mul_div_up(share_adjustment_fp, share_reserves))
+                .unwrap()
+        };
+
+        // Get the updated bond reserves.
+        let old_effective_share_reserves =
+            get_effective_share_reserves(self.effective_share_reserves(), self.share_adjustment());
+        let new_effective_share_reserves =
+            get_effective_share_reserves(new_share_reserves, new_share_adjustment);
+        let new_bond_reserves =
+            bond_reserves.mul_div_down(new_effective_share_reserves, old_effective_share_reserves);
+
+        Ok((new_share_reserves, new_share_adjustment, new_bond_reserves))
     }
 }
 
