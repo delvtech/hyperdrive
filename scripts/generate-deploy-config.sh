18ffe824dc3abbd5edaa27a9a1aa1cc299326633
John McClure (pickleback) <mcclurejt@gmail.com>
2024-05-20 17:10:48 -0500

    responding to feedback

--- scripts/generate-deploy-config.sh
+++ scripts/generate-deploy-config.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
@@ -8,23 +8,25 @@ factories=()
 coordinators=()
 instances=()
 
-function update_indexes(){
-  local network=$1
-  echo " - updating $config_dir/$network/index.ts"
-  echo "" > "$config_dir/$network/index.ts"
-  for file in "$config_dir/$network"/*; do
-    base=$(basename $file)
-    if [[ "$base" != 'index.ts' ]]; then
-      echo "export * from \"./$(echo $base | cut -d"." -f1)\";" >> "$config_dir/$network/index.ts"
-    fi
-  done
-  network_export="$(grep -i "$network" < "$config_dir/index.ts" || true)"
-  if [[ -z $network_export ]]; then
-    echo " - updating $config_dir/index.ts"
-    echo "export * from \"./$network\";" >> "$config_dir/index.ts"
-  fi
+# Non-destructively updates the `index.ts` file for the network and global config
+function update_indexes() {
+	local network=$1
+	echo " - updating $config_dir/$network/index.ts"
+	echo "" >"$config_dir/$network/index.ts"
+	for file in "$config_dir/$network"/*; do
+		base=$(basename $file)
+		if [[ "$base" != 'index.ts' ]]; then
+			echo "export * from \"./$(echo $base | cut -d"." -f1)\";" >>"$config_dir/$network/index.ts"
+		fi
+	done
+	network_export="$(grep -i "$network" <"$config_dir/index.ts" || true)"
+	if [[ -z $network_export ]]; then
+		echo " - updating $config_dir/index.ts"
+		echo "export * from \"./$network\";" >>"$config_dir/index.ts"
+	fi
 }
 
+# Generates factory from `factory.env` if present, see `echo` statements for context
 factory_input=factory.env
 factory_template=$config_dir/factory.ts.tmpl
 factory_output_filename=factory.ts
@@ -41,12 +43,13 @@ if [ -f $factory_input ]; then
 		echo " - writing configuration to ${factory_output_file}"
 		mkdir -p $factory_output_dir
 		envsubst <$factory_template >$factory_output_file
-    update_indexes $NETWORK_NAME
-    factories+="${NETWORK_NAME}_FACTORY"
-	fi 
+		update_indexes $NETWORK_NAME
+		factories+="${NETWORK_NAME}_FACTORY"
+	fi
 	echo "done! \n"
 fi
 
+# Generates coordinator from `coordinator.env` if present, see `echo` statements for context
 coordinator_input=coordinator.env
 coordinator_template=$config_dir/coordinator.ts.tmpl
 if [ -f $coordinator_input ]; then
@@ -63,12 +66,13 @@ if [ -f $coordinator_input ]; then
 		echo " - writing configuration to ${coordinator_output_file}"
 		mkdir -p $coordinator_output_dir
 		envsubst <$coordinator_template >$coordinator_output_file
-    update_indexes $NETWORK_NAME
-    coordinators+="$NAME"
+		update_indexes $NETWORK_NAME
+		coordinators+="$NAME"
 	fi
 	echo "done! \n"
 fi
 
+# Generates instance from `instance.env` if present, see `echo` statements for context
 instance_input=instance.env
 instance_template=$config_dir/instance.ts.tmpl
 if [ -f $instance_input ]; then
@@ -85,49 +89,49 @@ if [ -f $instance_input ]; then
 		echo " - writing configuration to ${instance_output_file}"
 		mkdir -p $instance_output_dir
 		envsubst <$instance_template >$instance_output_file
-    update_indexes $NETWORK_NAME
-    instances+="$NAME"
+		update_indexes $NETWORK_NAME
+		instances+="$NAME"
 	fi
 	echo "done! \n"
 fi
 
-
+# Outputs instructions for integrating the generated configurations with
+# existing deploy configurations in `hardhat.config.ts`
 names=(${factories[@]} ${coordinators[@]} ${instances[@]})
 IFS=,\n
 if [ ! ${#names[@]} -eq 0 ]; then
-    echo "
-All configurations have been generated.
+	echo "
+    All configurations have been generated.
 
-Add: 
+    Add: 
 
-    ${names[*]}
+        ${names[*]}
 
-to the import from \"./tasks/deploy/config/\" in hardhat.config.ts
+    to the import from \"./tasks/deploy/config/\" in hardhat.config.ts
 
-Then, merge the following into the network configuration for \"${NETWORK_NAME}\".\n"
-
-    if [ ! ${#factories[@]} -eq 0 ]; then
-      echo "
+    Then, merge the following into the network configuration for \"${NETWORK_NAME}\".
+  "
+	if [ ! ${#factories[@]} -eq 0 ]; then
+		echo "
     factories: [
           ${factories[*]}
     ]
       "
-    fi
-    if [ ! ${#coordinators[@]} -eq 0 ]; then
-      echo "
+	fi
+	if [ ! ${#coordinators[@]} -eq 0 ]; then
+		echo "
     coordinators: [
           ${coordinators[*]}
     ]
       "
-    fi
-    if [ ! ${#instances[@]} -eq 0 ]; then
-      echo "
+	fi
+	if [ ! ${#instances[@]} -eq 0 ]; then
+		echo "
     instances: [
           ${instances[*]}
     ]
       "
-    fi
+	fi
 else
-    echo "No configuration was generated."
+	echo "No configuration was generated."
 fi
-
