f0cf38a361b351d4838737353786f2d2a3df54c3
John McClure (pickleback) <mcclurejt@gmail.com>
2024-05-22 12:20:08 -0500

    set deployer as defaultPauser for sepolia

--- tasks/deploy/config/sepolia/factory.ts
+++ tasks/deploy/config/sepolia/factory.ts
@@ -1,4 +1,4 @@
-import { parseEther } from "viem";
+import { Address, parseEther } from "viem";
 import { HyperdriveFactoryConfig, parseDuration } from "../../lib";
 
 export const SEPOLIA_FACTORY: HyperdriveFactoryConfig = {
@@ -17,7 +17,9 @@ export const SEPOLIA_FACTORY: HyperdriveFactoryConfig = {
             deployerCoordinatorManager:
                 "0xd94a3A0BfC798b98a700a785D5C610E8a2d5DBD8",
             hyperdriveGovernance: "0xc187a246Ee5A4Fe4395a8f6C0f9F2AA3A5a06e9b",
-            defaultPausers: ["0xc187a246Ee5A4Fe4395a8f6C0f9F2AA3A5a06e9b"],
+            defaultPausers: [
+                (await hre.getNamedAccounts())["deployer"] as Address,
+            ],
             feeCollector: "0xc187a246Ee5A4Fe4395a8f6C0f9F2AA3A5a06e9b",
             sweepCollector: "0xc187a246Ee5A4Fe4395a8f6C0f9F2AA3A5a06e9b",
             checkpointDurationResolution: parseDuration("8 hours"),
