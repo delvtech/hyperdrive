14abe16f8e973f5c12f11d3452d45527d2342c8f
John McClure (pickleback) <mcclurejt@gmail.com>
2024-05-31 06:16:06 -0500

    keep process alive

--- scripts/fork-entrypoint.sh
+++ scripts/fork-entrypoint.sh
@@ -7,6 +7,10 @@ trap 'kill $(jobs -p) 2>/dev/null' EXIT
 
 # Run anvil chain as a background process.
 anvil --fork-url ${MAINNET_RPC_URL} --chain-id 42069 &
+ANVIL=$!
 
 # Deploy the contract to anvil.
 scripts/deploy-fork.sh
+
+# Don't exit unless anvil errors out.
+wait $ANVIL
