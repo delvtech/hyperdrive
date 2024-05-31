d535a18b4dfd506d5a80d227bb0ccbd1ff7cd5a6
John McClure (pickleback) <mcclurejt@gmail.com>
2024-05-31 04:39:46 -0500

    fix shell quoting issues

--- scripts/fork-entrypoint.sh
+++ scripts/fork-entrypoint.sh
@@ -12,7 +12,7 @@ anvil --fork-url ${MAINNET_RPC_URL} --chain-id 42069 &
 scripts/deploy-fork.sh
 
 # Output the deployed addresses in a `delvtech/hyperdrive-infra` address server compatible format.
-cat ./deployments.local.json | jq '.mainnet_fork | {
+cat ./deployments.local.json | jq ".mainnet_fork | {
   dai14Day: .DAI_14_DAY.address,
   dai30Day: .DAI_30_DAY.address,
   steth14Day: .STETH_14_DAY.address,
@@ -21,5 +21,5 @@ cat ./deployments.local.json | jq '.mainnet_fork | {
   reth30Day: .RETH_30_DAY.address,
   factory: .FACTORY.address,
   hyperdriveRegistry: .MAINNET_FORK_REGISTRY.address,
-  }' >./artifacts/addresses.json
+  }" >./artifacts/addresses.json
 cp ./deployments.local.json ./artifacts/
