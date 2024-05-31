4ff5f95d20545513924711fa3b692c08473d602f
John McClure (pickleback) <mcclurejt@gmail.com>
2024-05-31 05:55:01 -0500

    fix

--- scripts/deploy-fork.sh
+++ scripts/deploy-fork.sh
@@ -28,13 +28,14 @@ npx hardhat registry:add --name RETH_30_DAY --value 1 --network mainnet_fork --c
 
 # Extract the deployed contract addresses to `artifacts/addresses.json`
 # for use with the delvtech/infra address server.
-cat deployments.local.json | jq '.mainnet_fork | { \
-  dai14Day: .DAI_14_DAY.address, \
-  dai30Day: .DAI_30_DAY.address, \
-  steth14Day: .STETH_14_DAY.address, \
-  steth30Day: .STETH_30_DAY.address, \
-  reth14Day: .RETH_14_DAY.address, \
-  reth30Day: .RETH_30_DAY.address, \
-  factory: .FACTORY.address, \
-  hyperdriveRegistry: .MAINNET_FORK_REGISTRY.address, \
-  }' >artifacts/addresses.json
+cat ./deployments.local.json | jq ".mainnet_fork | {
+  dai14Day: .DAI_14_DAY.address,
+  dai30Day: .DAI_30_DAY.address,
+  steth14Day: .STETH_14_DAY.address,
+  steth30Day: .STETH_30_DAY.address,
+  reth14Day: .RETH_14_DAY.address,
+  reth30Day: .RETH_30_DAY.address,
+  factory: .FACTORY.address,
+  hyperdriveRegistry: .MAINNET_FORK_REGISTRY.address,
+  }" >./artifacts/addresses.json
+cp ./deployments.local.json ./artifacts/
--- scripts/fork-entrypoint.sh
+++ scripts/fork-entrypoint.sh
@@ -10,7 +10,3 @@ anvil --fork-url ${MAINNET_RPC_URL} --chain-id 42069 &
 
 # Deploy the contract to anvil.
 scripts/deploy-fork.sh
-
-# Output the deployed addresses in a `delvtech/hyperdrive-infra` address server compatible format.
-cat ./deployments.local.json | jq ".mainnet_fork | { dai14Day: .DAI_14_DAY.address, dai30Day: .DAI_30_DAY.address, steth14Day: .STETH_14_DAY.address, steth30Day: .STETH_30_DAY.address, reth14Day: .RETH_14_DAY.address, reth30Day: .RETH_30_DAY.address, factory: .FACTORY.address, hyperdriveRegistry: .MAINNET_FORK_REGISTRY.address, }" >./artifacts/addresses.json
-cp ./deployments.local.json ./artifacts/
