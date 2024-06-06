e6268634f2b0a6bb452bcc4010ca6096dd16e220
John McClure (pickleback) <mcclurejt@gmail.com>
2024-06-06 12:11:21 -0500

    update sepolia config to derive from deployer address

--- tasks/deploy/config/sepolia/factory.ts
+++ tasks/deploy/config/sepolia/factory.ts
@@ -13,9 +13,10 @@ export const SEPOLIA_FACTORY: HyperdriveFactoryConfig = {
     },
     constructorArguments: async (hre) => [
         {
-            governance: "0xd94a3A0BfC798b98a700a785D5C610E8a2d5DBD8",
-            deployerCoordinatorManager:
-                "0xd94a3A0BfC798b98a700a785D5C610E8a2d5DBD8",
+            governance: "0xc187a246Ee5A4Fe4395a8f6C0f9F2AA3A5a06e9b",
+            deployerCoordinatorManager: (await hre.getNamedAccounts())[
+                "deployer"
+            ] as Address,
             hyperdriveGovernance: "0xc187a246Ee5A4Fe4395a8f6C0f9F2AA3A5a06e9b",
             defaultPausers: [
                 (await hre.getNamedAccounts())["deployer"] as Address,
