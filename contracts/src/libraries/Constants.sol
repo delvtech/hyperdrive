016dd0b04d67fccf43b633f58b6c2c5c24aa9092
John McClure (pickleback) <mcclurejt@gmail.com>
2024-06-28 16:05:48 -0500

    update version

--- contracts/src/libraries/Constants.sol
+++ contracts/src/libraries/Constants.sol
@@ -5,7 +5,7 @@ pragma solidity 0.8.20;
 address constant ETH = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
 
 /// @dev The version of the contracts.
-string constant VERSION = "v1.0.13";
+string constant VERSION = "v1.0.14";
 
 /// @dev The number of targets that must be deployed for a full deployment.
 uint256 constant NUM_TARGETS = 4;
