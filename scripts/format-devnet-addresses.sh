4b6656c77ab39852a7b2c0ebf7158037d9ecad0e
John McClure (pickleback) <mcclurejt@gmail.com>
2024-06-28 02:29:09 -0500

    fix deploy fork scripts

--- scripts/format-devnet-addresses.sh
+++ scripts/format-devnet-addresses.sh
@@ -5,5 +5,5 @@ cat deployments.local.json | jq '.anvil | {
   erc4626Hyperdrive: .ERC4626_HYPERDRIVE.address,
   stethHyperdrive: .STETH_HYPERDRIVE.address,
   factory: .FACTORY.address,
-  hyperdriveRegistry: .ANVIL_REGISTRY.address,
+  hyperdriveRegistry: .["DELV Hyperdrive Registry"].address,
 }' >./artifacts/addresses.json
