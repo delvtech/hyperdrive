7926d0edf58d63d62997984da1a7bd5fb491cac0
John McClure (pickleback) <mcclurejt@gmail.com>
2024-07-02 14:09:25 -0500

    add clone network config, clean up scripts

--- hardhat.config.anvil.ts
+++ hardhat.config.anvil.ts
@@ -14,23 +14,6 @@ import {
     ANVIL_FACTORY,
     ANVIL_STETH_COORDINATOR,
     ANVIL_STETH_HYPERDRIVE,
-    SEPOLIA_CHECKPOINT_REWARDER,
-    SEPOLIA_CHECKPOINT_SUBREWARDER,
-    SEPOLIA_DAI_14DAY,
-    SEPOLIA_DAI_30DAY,
-    SEPOLIA_ERC4626_COORDINATOR,
-    SEPOLIA_EZETH_14DAY,
-    SEPOLIA_EZETH_30DAY,
-    SEPOLIA_EZETH_COORDINATOR,
-    SEPOLIA_FACTORY,
-    SEPOLIA_MORPHO_DAI_14DAY,
-    SEPOLIA_MORPHO_DAI_30DAY,
-    SEPOLIA_RETH_14DAY,
-    SEPOLIA_RETH_30DAY,
-    SEPOLIA_RETH_COORDINATOR,
-    SEPOLIA_STETH_14DAY,
-    SEPOLIA_STETH_30DAY,
-    SEPOLIA_STETH_COORDINATOR,
 } from "./tasks/deploy/config/";
 
 const { env } = process;
@@ -40,37 +23,6 @@ let DEFAULT_PK =
 const config: HardhatUserConfig = {
     ...baseConfig,
     networks: {
-        hardhat: {
-            accounts: [
-                {
-                    privateKey: env.PRIVATE_KEY ?? DEFAULT_PK,
-                    balance: "1000000000000000000",
-                },
-            ],
-            hyperdriveDeploy: {
-                checkpointRewarders: [SEPOLIA_CHECKPOINT_REWARDER],
-                checkpointSubrewarders: [SEPOLIA_CHECKPOINT_SUBREWARDER],
-                factories: [SEPOLIA_FACTORY],
-                coordinators: [
-                    SEPOLIA_ERC4626_COORDINATOR,
-                    SEPOLIA_STETH_COORDINATOR,
-                    SEPOLIA_RETH_COORDINATOR,
-                    SEPOLIA_EZETH_COORDINATOR,
-                ],
-                instances: [
-                    SEPOLIA_DAI_14DAY,
-                    SEPOLIA_DAI_30DAY,
-                    SEPOLIA_EZETH_14DAY,
-                    SEPOLIA_EZETH_30DAY,
-                    SEPOLIA_MORPHO_DAI_14DAY,
-                    SEPOLIA_MORPHO_DAI_30DAY,
-                    SEPOLIA_RETH_14DAY,
-                    SEPOLIA_RETH_30DAY,
-                    SEPOLIA_STETH_14DAY,
-                    SEPOLIA_STETH_30DAY,
-                ],
-            },
-        },
         anvil: {
             live: false,
             url: env.HYPERDRIVE_ETHEREUM_URL ?? "http://127.0.0.1:8545",
