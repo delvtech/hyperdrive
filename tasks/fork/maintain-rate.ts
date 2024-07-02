5154a7ac292865e62c403f2e1e2860d103e49a27
John McClure (pickleback) <mcclurejt@gmail.com>
2024-07-02 17:25:19 -0500

    fork rate calculation and target0 address fixes (#1075)
    * fix rate calculation
    
    * use less addresses
    
    * fix target0 incorrect address
    
    * simplify logic for updating fork vault balances
    
    * properly handle integer math
    
    * remove extra parens
    
    * simplify double division

--- tasks/fork/maintain-rate.ts
+++ tasks/fork/maintain-rate.ts
@@ -10,6 +10,7 @@ import { sleep } from "./lib";
 
 export type MaintainRateParams = HyperdriveDeployBaseTaskParams & {
     rate: string;
+    interval: number;
 };
 
 HyperdriveDeployBaseTask(
@@ -24,13 +25,17 @@ HyperdriveDeployBaseTask(
         "0.03",
         types.string,
     )
-    .setAction(async ({ rate }, { viem }) => {
+    .addOptionalParam(
+        "interval",
+        "time in hours between balance updates",
+        1,
+        types.int,
+    )
+    .setAction(async ({ rate, interval }: MaintainRateParams, { viem }) => {
         let tc = await viem.getTestClient({
             mode: "anvil",
         });
         let pc = await viem.getPublicClient();
-        let interval = 60; // minutes
-        let rateFraction = parseEther("1") / parseEther(rate);
         // Every hour, increase the balance of each vault by an amount proportional to the annual rate.
         while (true) {
             console.log("increasing balance of underlying vaults...");
@@ -44,8 +49,8 @@ HyperdriveDeployBaseTask(
                 }))!,
             );
             let stethBalanceIncrease =
-                (stethCurrentBalance * BigInt(interval)) /
-                (365n * 24n * rateFraction);
+                (stethCurrentBalance * BigInt(interval) * parseEther(rate)) /
+                (365n * 24n * BigInt(1e18));
             await tc.setStorageAt({
                 address: MAINNET_STETH_ADDRESS,
                 index: slot,
@@ -58,20 +63,21 @@ HyperdriveDeployBaseTask(
             });
             console.log(`steth balance increased by ${stethBalanceIncrease}`);
 
-            // For RETH, send ETH to the RocketTokenRETH contract.
+            // For RETH, update the ETH balance of the RocketTokenRETH contract.
             let rethCurrentBalance = await pc.getBalance({
                 address: MAINNET_RETH_ADDRESS,
             });
             let rethBalanceIncrease =
-                (rethCurrentBalance * BigInt(interval)) /
-                (365n * 24n * rateFraction);
+                (rethCurrentBalance * BigInt(interval) * parseEther(rate)) /
+                (365n * 24n * BigInt(1e18));
             await tc.setBalance({
                 address: MAINNET_RETH_ADDRESS,
-                value: rethCurrentBalance + rateFraction,
+                value: rethCurrentBalance + rethBalanceIncrease,
             });
             console.log(`reth balance increased by ${rethBalanceIncrease}`);
 
             // For SDAI, nothing needs to be done so long as the underlying `rho` value is not recalculated.
-            await sleep(interval);
+
+            await sleep(interval * 60);
         }
     });
