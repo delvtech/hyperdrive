a80401fc8662f0f07d62d1f38c2c9bfb92b68aed
John McClure (pickleback) <mcclurejt@gmail.com>
2024-07-03 02:40:05 -0500

    fix fork factory names

--- tasks/deploy/config/mainnet-fork/reth-coordinator.ts
+++ tasks/deploy/config/mainnet-fork/reth-coordinator.ts
@@ -1,4 +1,5 @@
 import { HyperdriveCoordinatorConfig, MAINNET_RETH_ADDRESS } from "../../lib";
+import { MAINNET_FORK_FACTORY_NAME } from "./factory";
 
 export const MAINNET_FORK_RETH_COORDINATOR_NAME = "RETH_COORDINATOR";
 export const MAINNET_FORK_RETH_COORDINATOR: HyperdriveCoordinatorConfig<"RETH"> =
@@ -6,7 +7,8 @@ export const MAINNET_FORK_RETH_COORDINATOR: HyperdriveCoordinatorConfig<"RETH"> 
         name: "RETH_COORDINATOR",
         prefix: "RETH",
         factoryAddress: async (hre) =>
-            hre.hyperdriveDeploy.deployments.byName("FACTORY").address,
+            hre.hyperdriveDeploy.deployments.byName(MAINNET_FORK_FACTORY_NAME)
+                .address,
         targetCount: 4,
         token: MAINNET_RETH_ADDRESS,
     };
