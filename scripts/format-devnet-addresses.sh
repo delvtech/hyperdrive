27a7f47d2c1332bcf38bf265c0ff0c8ce95f21cc
John McClure (pickleback) <mcclurejt@gmail.com>
2024-07-05 01:09:46 -0500

    fix address formatting to include factory address

--- scripts/format-devnet-addresses.sh
+++ scripts/format-devnet-addresses.sh
@@ -4,6 +4,6 @@ cat deployments.local.json | jq '.anvil | {
   baseToken: .BASE_TOKEN.address,
   erc4626Hyperdrive: .ERC4626_HYPERDRIVE.address,
   stethHyperdrive: .STETH_HYPERDRIVE.address,
-  factory: .FACTORY.address,
+  factory: .["ElementDAO Hyperdrive Factory"].address,
   hyperdriveRegistry: .["DELV Hyperdrive Registry"].address,
 }' >./artifacts/addresses.json
