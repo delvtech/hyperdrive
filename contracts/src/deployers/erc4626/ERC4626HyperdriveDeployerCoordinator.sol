6a64d4bd5b5a0a471c5d7f357f75736d1c654465
Alex Towle <jalextowle@gmail.com>
2024-07-14 14:25:29 +0200

    Update contracts/src/deployers/erc4626/ERC4626HyperdriveDeployerCoordinator.sol
    Co-authored-by: Jonny Rhea <5555162+jrhea@users.noreply.github.com>

--- contracts/src/deployers/erc4626/ERC4626HyperdriveDeployerCoordinator.sol
+++ contracts/src/deployers/erc4626/ERC4626HyperdriveDeployerCoordinator.sol
@@ -115,7 +115,7 @@ contract ERC4626HyperdriveDeployerCoordinator is
     /// @notice Convert an amount of base to an amount of vault shares.
     /// @param _vaultSharesToken The vault shares asset.
     /// @param _baseAmount The base amount.
-    /// @return The base amount.
+    /// @return The vault shares amount.
     function convertToShares(
         IERC20 _vaultSharesToken,
         uint256 _baseAmount
