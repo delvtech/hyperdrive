43d6ac13529a8208e0ab7522e3d98ca8e5da4174
Alex Towle <jalextowle@gmail.com>
2024-07-14 14:25:22 +0200

    Update contracts/src/deployers/aave/AaveHyperdriveDeployerCoordinator.sol
    Co-authored-by: Jonny Rhea <5555162+jrhea@users.noreply.github.com>

--- contracts/src/deployers/aave/AaveHyperdriveDeployerCoordinator.sol
+++ contracts/src/deployers/aave/AaveHyperdriveDeployerCoordinator.sol
@@ -131,7 +131,7 @@ contract AaveHyperdriveDeployerCoordinator is
     /// @param _baseToken The base token.
     /// @param _vault The Aave vault.
     /// @param _baseAmount The base amount.
-    /// @return The base amount.
+    /// @return The vault shares amount.
     function convertToShares(
         IERC20 _baseToken,
         IPool _vault,
