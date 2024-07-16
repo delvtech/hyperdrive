ec25dc7deafa8fa2fc4f1c882f902924fbaef260
Alex Towle <jalextowle@gmail.com>
2024-07-17 01:06:15 +0200

    FIXME: Temporarily updated deployment.json and deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,16 +9,16 @@ fi
 # When deploying to "live" networks, ensure that the repository is equivalent
 # to the latest remote tag. This avoids accidentally deploying out-of-date or
 # ambiguously versioned contracts.
-if [[ "${NETWORK}" != "anvil" && "${NETWORK}" != "hardhat" && "${NETWORK}" != "mainnet_fork" ]]; then
-	git remote update
-	tag=$(git describe --tags --abbrev=0)
-	diff=$(git diff ${tag} --raw -- contracts lib)
-	if [[ ! -z "${diff}" ]]; then
-		echo "$diff"
-		echo "Error: repository contents must match tag ${tag}"
-		exit 1
-	fi
-fi
+# if [[ "${NETWORK}" != "anvil" && "${NETWORK}" != "hardhat" && "${NETWORK}" != "mainnet_fork" ]]; then
+# 	git remote update
+# 	tag=$(git describe --tags --abbrev=0)
+# 	diff=$(git diff ${tag} --raw -- contracts lib)
+# 	if [[ ! -z "${diff}" ]]; then
+# 		echo "$diff"
+# 		echo "Error: repository contents must match tag ${tag}"
+# 		exit 1
+# 	fi
+# fi
 
 npx hardhat deploy:hyperdrive --show-stack-traces --network ${NETWORK} --config hardhat.config.${NETWORK}.ts
 npx hardhat deploy:verify --show-stack-traces --network ${NETWORK} --config hardhat.config.${NETWORK}.ts
