ed57e2ba2de4372124dd1111961d165b5a53aa3b
John McClure (pickleback) <mcclurejt@gmail.com>
2024-07-18 10:19:15 -0500

    test event

--- test/units/everlong/Admin.t.sol
+++ test/units/everlong/Admin.t.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity 0.8.20;
-
 import { IEverlongAdmin } from "contracts/src/interfaces/IEverlongAdmin.sol";
+import { EverlongAdmin } from "contracts/src/everlong/EverlongAdmin.sol";
 import { EverlongTest } from "test/utils/EverlongTest.sol";
 
-contract EverlongAdminTest is EverlongTest {
+contract EverlongAdminTest is EverlongTest, EverlongAdmin {
     function test_set_admin_failure_unauthorized() external {
         // Ensure that an unauthorized user cannot set the admin address.
         vm.stopPrank();
@@ -15,6 +15,8 @@ contract EverlongAdminTest is EverlongTest {
 
     function test_set_admin_success_deployer() external {
         // Ensure that the deployer can set the admin address.
+        vm.expectEmit(true, false, false, true);
+        emit AdminUpdated(address(0));
         everlong.setAdmin(address(0));
         assertEq(everlong.admin(), address(0));
     }
