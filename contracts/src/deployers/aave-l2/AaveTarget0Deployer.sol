b5fee57664344dd8de713790ccb3e0ffae7c6111
Matthew Brown <mattfdbrown@gmail.com>
2024-07-26 10:00:02 -0500

    copy aave

---
+++ contracts/src/deployers/aave-l2/AaveTarget0Deployer.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity 0.8.20;
+
+import { AaveTarget0 } from "../../instances/aave/AaveTarget0.sol";
+import { IHyperdrive } from "../../interfaces/IHyperdrive.sol";
+import { IHyperdriveTargetDeployer } from "../../interfaces/IHyperdriveTargetDeployer.sol";
+
+/// @author DELV
+/// @title AaveTarget0Deployer
+/// @notice The target0 deployer for the AaveHyperdrive implementation.
+/// @custom:disclaimer The language used in this code is for coding convenience
+///                    only, and is not intended to, and does not, have any
+///                    particular legal or regulatory significance.
+contract AaveTarget0Deployer is IHyperdriveTargetDeployer {
+    /// @notice Deploys a target0 instance with the given parameters.
+    /// @param _config The configuration of the Hyperdrive pool.
+    /// @param _salt The create2 salt used in the deployment.
+    /// @return The address of the newly deployed AaveTarget0 instance.
+    function deployTarget(
+        IHyperdrive.PoolConfig memory _config,
+        bytes memory, // unused _extraData
+        bytes32 _salt
+    ) external returns (address) {
+        return
+            address(
+                // NOTE: We hash the sender with the salt to prevent the
+                // front-running of deployments.
+                new AaveTarget0{
+                    salt: keccak256(abi.encode(msg.sender, _salt))
+                }(_config)
+            );
+    }
+}
---
+++ contracts/src/instances/aave-l2/AaveTarget4.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity 0.8.20;
+
+import { HyperdriveTarget4 } from "../../external/HyperdriveTarget4.sol";
+import { IHyperdrive } from "../../interfaces/IHyperdrive.sol";
+import { AaveBase } from "./AaveBase.sol";
+
+/// @author DELV
+/// @title AaveTarget4
+/// @notice AaveHyperdrive's target4 logic contract. This contract contains
+///         several stateful functions that couldn't fit into the Hyperdrive
+///         contract.
+/// @custom:disclaimer The language used in this code is for coding convenience
+///                    only, and is not intended to, and does not, have any
+///                    particular legal or regulatory significance.
+contract AaveTarget4 is HyperdriveTarget4, AaveBase {
+    /// @notice Initializes the target4 contract.
+    /// @param _config The configuration of the Hyperdrive pool.
+    constructor(
+        IHyperdrive.PoolConfig memory _config
+    ) HyperdriveTarget4(_config) {}
+}
