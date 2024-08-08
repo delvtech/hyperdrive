92345af3ba34981032dbe7419b1893b5b0e76e8d
John McClure (pickleback) <mcclurejt@gmail.com>
2024-08-08 08:51:13 -0500

    remove wsteth/usdc from mainnet config

--- hardhat.config.mainnet.ts
+++ hardhat.config.mainnet.ts
@@ -12,7 +12,6 @@ import {
     MAINNET_FACTORY,
     MAINNET_MORPHO_BLUE_COORDINATOR,
     MAINNET_MORPHO_BLUE_SUSDE_DAI_182DAY,
-    MAINNET_MORPHO_BLUE_WSTETH_USDC_182DAY,
     MAINNET_RETH_182DAY,
     MAINNET_RETH_COORDINATOR,
 } from "./tasks/deploy/config/mainnet";
@@ -37,7 +36,6 @@ const config: HardhatUserConfig = {
                     MAINNET_EZETH_182DAY,
                     MAINNET_RETH_182DAY,
                     MAINNET_MORPHO_BLUE_SUSDE_DAI_182DAY,
-                    MAINNET_MORPHO_BLUE_WSTETH_USDC_182DAY,
                 ],
                 checkpointRewarders: [],
                 checkpointSubrewarders: [],
