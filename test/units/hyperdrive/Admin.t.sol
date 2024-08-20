7bda11e2213c54a3d0b9c079bd709f7d13a31b68
Alex Towle <jalextowle@gmail.com>
2024-08-20 00:46:01 -0500

    Adds some tests that verify that the feature works

--- test/units/hyperdrive/Admin.t.sol
+++ test/units/hyperdrive/Admin.t.sol
@@ -5,6 +5,53 @@ import { IHyperdrive } from "../../../contracts/src/interfaces/IHyperdrive.sol";
 import { HyperdriveTest } from "../../utils/HyperdriveTest.sol";
 
 contract AdminTest is HyperdriveTest {
+    function test_update_governance() external {
+        // Update the pool's governance address.
+        vm.stopPrank();
+        vm.startPrank(factory.governance());
+        address governance = address(0xdeadbeef);
+        factory.updateHyperdriveGovernance(governance);
+
+        // Ensure that governance was updated.
+        assertEq(hyperdrive.getPoolConfig().governance, governance);
+    }
+
+    function test_update_feeCollector() external {
+        // Update the fee collector.
+        vm.stopPrank();
+        vm.startPrank(factory.governance());
+        address feeCollector = address(0xdeadbeef);
+        factory.updateFeeCollector(feeCollector);
+
+        // Ensure that the fee collector was updated.
+        assertEq(hyperdrive.getPoolConfig().feeCollector, feeCollector);
+    }
+
+    function test_update_sweepCollector() external {
+        // Update the sweep collector.
+        vm.stopPrank();
+        vm.startPrank(factory.governance());
+        address sweepCollector = address(0xdeadbeef);
+        factory.updateSweepCollector(sweepCollector);
+
+        // Ensure that the sweep collector was updated.
+        assertEq(hyperdrive.getPoolConfig().sweepCollector, sweepCollector);
+    }
+
+    function test_update_checkpointRewarder() external {
+        // Update the checkpoint rewarder.
+        vm.stopPrank();
+        vm.startPrank(factory.governance());
+        address checkpointRewarder = address(0xdeadbeef);
+        factory.updateCheckpointRewarder(checkpointRewarder);
+
+        // Ensure that the checkpoint rewarder was updated.
+        assertEq(
+            hyperdrive.getPoolConfig().checkpointRewarder,
+            checkpointRewarder
+        );
+    }
+
     function test_pause_failure_unauthorized() external {
         // Ensure that an unauthorized user cannot pause the contract.
         vm.stopPrank();
