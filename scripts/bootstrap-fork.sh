2a94534704f0b9df3b368e0c66d25c2f6c95a104
John McClure (pickleback) <mcclurejt@gmail.com>
2024-08-21 03:59:59 -0500

    dockerignore fixes

--- scripts/bootstrap-fork.sh
+++ scripts/bootstrap-fork.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-# Bootstrap the "mainnet_fork" addresses in `deployements.local.json`.
+# Bootstrap the "mainnet_fork" addresses in `deployments.local.json`.
 jq '.mainnet | { mainnet_fork: . }' <deployments.json >deployments.local.json
