72cac92e84110b242efea426c04020860f296c94
John McClure (pickleback) <mcclurejt@gmail.com>
2024-08-27 16:41:05 -0500

    registry deploy script cleanup and commenting

--- tasks/deploy/registry.ts
+++ tasks/deploy/registry.ts
@@ -15,7 +15,7 @@ HyperdriveDeployBaseTask(
 ).setAction(
     async (
         { name }: DeployRegistryParams,
-        { hyperdriveDeploy, artifacts, getNamedAccounts },
+        { hyperdriveDeploy, artifacts, getNamedAccounts, viem },
     ) => {
         console.log("\nRunning deploy:registry ...");
         // Skip if the registry is already deployed.
@@ -31,35 +31,40 @@ HyperdriveDeployBaseTask(
             [],
         );
 
+        // Take the `REGISTRY_SALT` string and convert it to bytes32
+        // compatible formatting.
+        let salt = stringToHex(process.env.REGISTRY_SALT! as `0x${string}`, {
+            size: 32,
+        });
+
+        // Assemble the creation code by packing the registry contract's
+        // bytecode with its constructor arguments.
+        let artifact = artifacts.readArtifactSync("HyperdriveRegistry");
+        let creationCode = encodePacked(
+            ["bytes", "bytes"],
+            [
+                artifact.bytecode,
+                encodeAbiParameters(
+                    [{ name: "_name", type: "string" }],
+                    [name],
+                ),
+            ],
+        );
+
         // Retrieve the contract's bytecode from the artifact and pack it
         // with the constructor arguments to form the creation code.
-        let artifact = artifacts.readArtifactSync("HyperdriveRegistry");
-        let address = await create3Deployer.write.deploy([
-            stringToHex(process.env.REGISTRY_SALT! as `0x${string}`, {
-                size: 32,
-            }),
-            encodePacked(
-                ["bytes", "bytes"],
-                [
-                    artifact.bytecode,
-                    encodeAbiParameters(
-                        [{ name: "_name", type: "string" }],
-                        [name],
-                    ),
-                ],
-            ),
-        ]);
-        console.log(` - saving ${name}... ${address}`);
+        let tx = await create3Deployer.write.deploy([salt, creationCode]);
+        let pc = await viem.getPublicClient();
+        await pc.waitForTransactionReceipt({ hash: tx });
+        console.log(` - saving ${name}...`);
+
+        // Use the deployer address to back-compute the deployed contract address
+        // and store the deployment configuration in deployments.json.
         let deployer = (await getNamedAccounts())["deployer"];
         hyperdriveDeploy.deployments.add(
             name,
             "HyperdriveRegistry",
-            await create3Deployer.read.getDeployed([
-                deployer as Address,
-                stringToHex(process.env.REGISTRY_SALT! as `0x${string}`, {
-                    size: 32,
-                }),
-            ]),
+            await create3Deployer.read.getDeployed([deployer as Address, salt]),
         );
     },
 );
