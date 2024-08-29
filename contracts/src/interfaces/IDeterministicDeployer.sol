be9110477f41d0def9af49c199d9738a4d1473d6
John McClure (pickleback) <mcclurejt@gmail.com>
2024-08-28 19:23:25 -0500

    use existing create2 deployer so that we don't have to worry about deployer nonces

---
+++ contracts/src/interfaces/IDeterministicDeployer.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.20;
+
+import { ICreateX } from "createx/ICreateX.sol";
+
+/// @notice This is included so that it appears in artifacts for tooling.
+interface IDeterministicDeployer is ICreateX {}
