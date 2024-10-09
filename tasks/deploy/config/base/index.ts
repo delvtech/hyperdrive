2c96bb106342e1849b88ec134f4712bb50e52a08
Alex Towle <jalextowle@gmail.com>
2024-10-09 06:33:19 -1000

    Added the Moonwell ETH vault (#1186)
    * Added the Moonwell ETH vault
    
    * Added the deployment script for the Moonwell ETH vault

--- tasks/deploy/config/base/index.ts
+++ tasks/deploy/config/base/index.ts
@@ -1,4 +1,6 @@
 export * from "./cbeth-182day";
 export * from "./chainlink-coordinator";
+export * from "./erc4626-coordinator";
 export * from "./factory";
+export * from "./moonwell-eth-182day";
 export * from "./morpho-blue-coordinator";
