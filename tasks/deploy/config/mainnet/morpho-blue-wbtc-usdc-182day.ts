45ee59f790c6e391d77a9828fe7b4f0ddb8a1cc7
MazyGio <mazygio@element.fi>
2024-11-15 11:45:54 -0500

    finalize deployment params and add link to morpho market

--- tasks/deploy/config/mainnet/morpho-blue-wbtc-usdc-182day.ts
+++ tasks/deploy/config/mainnet/morpho-blue-wbtc-usdc-182day.ts
@@ -79,7 +79,9 @@ export const MAINNET_MORPHO_BLUE_WBTC_USDC_182DAY: HyperdriveInstanceConfig<"Mor
         salt: toBytes32("0x42080085"),
         extraData: morphoBlueParameters,
         contribution: CONTRIBUTION,
-        fixedAPR: parseEther("0.04"),
+        // NOTE: The latest variable rate on the Morpho Blue market is 2.93% APY:
+        // https://app.morpho.org/market?id=0x3a85e619751152991742810df6ec69ce473daef99e28a64ab2340d7b7ccfee49&network=mainnet&morphoPrice=0.75
+        fixedAPR: parseEther("0.0575"),
         timestretchAPR: parseEther("0.075"),
         options: async (hre) => ({
             extraData: "0x",
