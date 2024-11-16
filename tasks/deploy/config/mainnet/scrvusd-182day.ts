3cad9073ca702a3affd9c1199447107fca82b333
MazyGio <mazygio@element.fi>
2024-11-16 11:21:31 -0500

    update fixedAPR to a valid amount

--- tasks/deploy/config/mainnet/scrvusd-182day.ts
+++ tasks/deploy/config/mainnet/scrvusd-182day.ts
@@ -20,71 +20,74 @@ export const MAINNET_SCRVUSD_182DAY_NAME = "ElementDAO 182 Day scrvUSD Hyperdriv
 // The initial contribution of the pool.
 const CONTRIBUTION = parseEther("100");
 
-export const MAINNET_SCRVUSD_182DAY: HyperdriveInstanceConfig<"ERC4626"> = {
-    name: MAINNET_SCRVUSD_182DAY_NAME,
-    prefix: "ERC4626",
-    coordinatorAddress: async (hre) =>
-        hre.hyperdriveDeploy.deployments.byName(
-            MAINNET_ERC4626_COORDINATOR_NAME,
-        ).address,
-    deploymentId: keccak256(toBytes(MAINNET_SCRVUSD_182DAY_NAME)),
-    salt: toBytes32("0x69420"),
-    extraData: "0x",
-    contribution: CONTRIBUTION,
-    // NOTE: Link to underlying scrvUSD market
-    // https://crvusd.curve.fi/#/ethereum/scrvUSD
-    fixedAPR: parseEther("0.15"),
-    timestretchAPR: parseEther("0.15"),
-    options: async (hre) => ({
-        extraData: "0x",
-        asBase: true,
-        destination: (await hre.getNamedAccounts())["deployer"] as Address,
-    }),
-    // Prepare to deploy the contract by setting approvals.
-    prepare: async (hre) => {
-        let baseToken = await hre.viem.getContractAt(
-            "contracts/src/interfaces/IERC20.sol:IERC20",
-            CRVUSD_ADDRESS_MAINNET,
-        );
-        let tx = await baseToken.write.approve([
+export const MAINNET_SCRVUSD_182DAY: HyperdriveInstanceConfig<"ERC4626"> = 
+    {
+        name: MAINNET_SCRVUSD_182DAY_NAME,
+        prefix: "ERC4626",
+        coordinatorAddress: async (hre) =>
             hre.hyperdriveDeploy.deployments.byName(
                 MAINNET_ERC4626_COORDINATOR_NAME,
             ).address,
-            CONTRIBUTION,
-        ]);
-        let pc = await hre.viem.getPublicClient();
-        await pc.waitForTransactionReceipt({ hash: tx });
-    },
-    poolDeployConfig: async (hre) => {
-        let factoryContract = await hre.viem.getContractAt(
-            "HyperdriveFactory",
-            hre.hyperdriveDeploy.deployments.byName(MAINNET_FACTORY_NAME)
-                .address,
-        );
-        return {
-            baseToken: CRVUSD_ADDRESS_MAINNET,
-            vaultSharesToken: SCRVUSD_ADDRESS_MAINNET,
-            circuitBreakerDelta: parseEther("0.05"),
-            minimumShareReserves: parseEther("0.001"),
-            minimumTransactionAmount: parseEther("0.001"),
-            positionDuration: parseDuration(SIX_MONTHS),
-            checkpointDuration: parseDuration("1 day"),
-            timeStretch: 0n,
-            governance: await factoryContract.read.governance(),
-            feeCollector: await factoryContract.read.feeCollector(),
-            sweepCollector: await factoryContract.read.sweepCollector(),
-            checkpointRewarder: await factoryContract.read.checkpointRewarder(),
-            ...(await getLinkerDetails(
-                hre,
+        deploymentId: keccak256(toBytes(MAINNET_SCRVUSD_182DAY_NAME)),
+        salt: toBytes32("0x69420"),
+        extraData: "0x",
+        contribution: CONTRIBUTION,
+        // NOTE: Link to underlying scrvUSD market
+        // https://crvusd.curve.fi/#/ethereum/scrvUSD
+        fixedAPR: parseEther("0.10"),
+        timestretchAPR: parseEther("0.15"),
+        options: async (hre) => ({
+            extraData: "0x",
+            asBase: true,
+            destination: (await hre.getNamedAccounts())["deployer"] as Address,
+        }),
+        // Prepare to deploy the contract by setting approvals.
+        prepare: async (hre) => {
+            let baseToken = await hre.viem.getContractAt(
+                "contracts/src/interfaces/IERC20.sol:IERC20",
+                CRVUSD_ADDRESS_MAINNET,
+            );
+            let tx = await baseToken.write.approve([
+                hre.hyperdriveDeploy.deployments.byName(
+                    MAINNET_ERC4626_COORDINATOR_NAME,
+                ).address,
+                CONTRIBUTION,
+            ]);
+            let pc = await hre.viem.getPublicClient();
+            await pc.waitForTransactionReceipt({ hash: tx });
+        },
+        poolDeployConfig: async (hre) => {
+            let factoryContract = await hre.viem.getContractAt(
+                "HyperdriveFactory",
                 hre.hyperdriveDeploy.deployments.byName(MAINNET_FACTORY_NAME)
                     .address,
-            )),
-            fees: {
-                curve: parseEther("0.01"),
-                flat: normalizeFee(parseEther("0.0005"), SIX_MONTHS),
-                governanceLP: parseEther("0.15"),
-                governanceZombie: parseEther("0.03"),
-            },
-        };
-    },
-};
+            );
+            return {
+                baseToken: CRVUSD_ADDRESS_MAINNET,
+                vaultSharesToken: SCRVUSD_ADDRESS_MAINNET,
+                circuitBreakerDelta: parseEther("0.05"),
+                minimumShareReserves: parseEther("0.001"),
+                minimumTransactionAmount: parseEther("0.001"),
+                positionDuration: parseDuration(SIX_MONTHS),
+                checkpointDuration: parseDuration("1 day"),
+                timeStretch: 0n,
+                governance: await factoryContract.read.hyperdriveGovernance(),
+                feeCollector: await factoryContract.read.feeCollector(),
+                sweepCollector: await factoryContract.read.sweepCollector(),
+                checkpointRewarder: 
+                    await factoryContract.read.checkpointRewarder(),
+                ...(await getLinkerDetails(
+                    hre,
+                    hre.hyperdriveDeploy.deployments.byName(
+                        MAINNET_FACTORY_NAME
+                    ).address,
+                )),
+                fees: {
+                    curve: parseEther("0.01"),
+                    flat: normalizeFee(parseEther("0.0005"), SIX_MONTHS),
+                    governanceLP: parseEther("0.15"),
+                    governanceZombie: parseEther("0.03"),
+                },
+            };
+        },
+    };
