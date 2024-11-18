0fe35eceb82bbb385f319fe4b0d864e85a6af59c
Matthew Brown <mattfdbrown@gmail.com>
2024-11-18 15:54:58 -0600

    log to terminal in benchmark worflow

--- python/gas_benchmarks.py
+++ python/gas_benchmarks.py
@@ -1,6 +1,6 @@
+import json
 import subprocess
 import sys
-import json
 
 OUTPUT_PATH = sys.argv[1]
 
@@ -22,45 +22,59 @@ FUNCTION_NAMES = [
 #
 # Run the Solidity tests and write the test name and the gas used to a markdown table.
 try:
-    test_output = subprocess.check_output('FOUNDRY_FUZZ_RUNS=100 forge test --no-match-path \'test/instances/*\' --no-match-contract \'MultiToken__transferFrom\' --gas-report', shell=True).decode()
+    process = subprocess.Popen(
+        "FOUNDRY_FUZZ_RUNS=100 forge test --no-match-path 'test/instances/*' --no-match-contract 'MultiToken__transferFrom' --gas-report",
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        universal_newlines=True,
+    )
+
+    capture = []
+    found_contract = ""
+    found_report = False
+    for line in iter(process.stdout.readline, ""):
+        print(line, end="")  # Print the output to the terminal
+        if not found_contract and CONTRACT_NAME in line:
+            found_contract = True
+        if found_contract and not found_report and "Function Name" in line:
+            found_report = True
+        elif found_report and len(line) > 0:
+            cols = line.split("|")
+            function_name = cols[1].strip()
+            if function_name in FUNCTION_NAMES:
+                capture += [
+                    {
+                        "name": f"{cols[1].strip()}: min",
+                        "value": cols[2].strip(),
+                        "unit": "gas",
+                    }
+                ]
+                capture += [
+                    {
+                        "name": f"{cols[1].strip()}: avg",
+                        "value": cols[3].strip(),
+                        "unit": "gas",
+                    }
+                ]
+                capture += [
+                    {
+                        "name": f"{cols[1].strip()}: max",
+                        "value": cols[5].strip(),
+                        "unit": "gas",
+                    }
+                ]
+        elif found_report:
+            break
+
+    process.wait()
+    if process.returncode != 0:
+        print(process.stderr.read())
+        exit(1)
+
+    with open(OUTPUT_PATH, "w") as f:
+        json.dump(capture, f)
+
 except subprocess.CalledProcessError as e:
     print(e.output)
     exit(1)
-capture = []
-found_contract = ""
-found_report = False
-for line in test_output.split("\n"):
-    if not found_contract and CONTRACT_NAME in line:
-        found_contract = True
-    if found_contract and not found_report and "Function Name" in line:
-        found_report = True
-    elif found_report and len(line) > 0:
-        cols = line.split("|")
-        function_name = cols[1].strip()
-        if function_name in FUNCTION_NAMES:
-            capture += [
-                {
-                    "name": f"{cols[1].strip()}: min",
-                    "value": cols[2].strip(),
-                    "unit": "gas",
-                }
-            ]
-            capture += [
-                {
-                    "name": f"{cols[1].strip()}: avg",
-                    "value": cols[3].strip(),
-                    "unit": "gas",
-                }
-            ]
-            capture += [
-                {
-                    "name": f"{cols[1].strip()}: max",
-                    "value": cols[5].strip(),
-                    "unit": "gas",
-                }
-            ]
-    elif found_report:
-        break
-
-with open(OUTPUT_PATH, "w") as f:
-    json.dump(capture, f)
