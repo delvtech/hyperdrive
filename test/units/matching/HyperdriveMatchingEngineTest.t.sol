959b2e98d02c33cd3b8c1e449831524729ddc986
John McClure (pickleback) <mcclurejt@gmail.com>
2024-11-26 00:57:36 -0600

    update matching engine version

--- test/units/matching/HyperdriveMatchingEngineTest.t.sol
+++ test/units/matching/HyperdriveMatchingEngineTest.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity 0.8.22;
+pragma solidity 0.8.24;
 
 import { VmSafe } from "forge-std/Vm.sol";
 import { IMorpho } from "morpho-blue/src/interfaces/IMorpho.sol";
