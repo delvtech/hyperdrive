187946527083961b737fd91c76672053a3893995
Alex Towle <jalextowle@gmail.com>
2024-11-27 12:33:06 -0800

    Adds comprehensive unit tests for the mint function

--- test/units/hyperdrive/MintTest.t.sol
+++ test/units/hyperdrive/MintTest.t.sol
@@ -1,9 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity 0.8.22;
 
-// FIXME
-import { console2 as console } from "forge-std/console2.sol";
-
 import { VmSafe } from "forge-std/Vm.sol";
 import { IHyperdrive } from "../../../contracts/src/interfaces/IHyperdrive.sol";
 import { AssetId } from "../../../contracts/src/libraries/AssetId.sol";
@@ -12,23 +9,7 @@ import { HyperdriveMath } from "../../../contracts/src/libraries/HyperdriveMath.
 import { HyperdriveTest, HyperdriveUtils } from "../../utils/HyperdriveTest.sol";
 import { Lib } from "../../utils/Lib.sol";
 
-// FIXME: Add full Natspec.
-//
-// FIXME: Add all of the failure tests.
-//
-// FIXME: Add assertions that show that:
-//
-// - The governance fees were paid.
-// - The flat fee of the short was paid.
-// - Variable interest was pre-paid.
-// - The correct amount of bonds were minted.
-//
-// To make this test better, we can work backward from the bond amount to the
-// full calculation.
-//
-// This should all contribute to ensuring solvency.
-//
-// FIXME: Think about what other tests we need once we have this function.
+/// @dev A test suite for the mint function.
 contract MintTest is HyperdriveTest {
     using FixedPointMath for uint256;
     using HyperdriveUtils for IHyperdrive;
@@ -189,73 +170,214 @@ contract MintTest is HyperdriveTest {
         baseToken.approve(address(hyperdrive), baseAmount);
 
         // Get some data before minting.
+        MintTestCase memory testCase = _mintTestCase(
+            alice, // funder
+            bob, // long
+            celine, // short
+            baseAmount, // amount
+            true, // asBase
+            "" // extraData
+        );
+
+        // Verify the mint transaction.
+        _verifyMint(testCase);
+    }
+
+    /// @dev Ensures that minting performs correctly when it succeeds.
+    function test_mint_success_prepaid_interest() external {
+        // Mint some base tokens to Alice and approve Hyperdrive.
+        vm.stopPrank();
+        vm.startPrank(alice);
+        uint256 baseAmount = 100_000e18;
+        baseToken.mint(baseAmount);
+        baseToken.approve(address(hyperdrive), baseAmount);
+
+        // Mint a checkpoint and accrue interest. This sets us up to have
+        // prepaid interest to account for.
+        hyperdrive.checkpoint(hyperdrive.latestCheckpoint(), 0);
+        advanceTime(CHECKPOINT_DURATION.mulDown(0.5e18), 2.5e18);
+
+        // Get some data before minting.
+        MintTestCase memory testCase = _mintTestCase(
+            alice, // funder
+            bob, // long
+            celine, // short
+            baseAmount, // amount
+            true, // asBase
+            "" // extraData
+        );
+
+        // Verify the mint transaction.
+        _verifyMint(testCase);
+    }
+
+    struct MintTestCase {
+        // Trading metadata.
+        address funder;
+        address long;
+        address short;
+        uint256 maturityTime;
+        uint256 amount;
+        bool asBase;
+        bytes extraData;
+        // The balances before the mint.
+        uint256 funderBaseBalanceBefore;
+        uint256 hyperdriveBaseBalanceBefore;
+        uint256 longBalanceBefore;
+        uint256 shortBalanceBefore;
+        // The state variables before the mint.
+        uint256 longsOutstandingBefore;
+        uint256 shortsOutstandingBefore;
+        uint256 governanceFeesAccruedBefore;
+        // Idle, pool depth, and spot price before the mint.
+        uint256 idleBefore;
+        uint256 kBefore;
+        uint256 spotPriceBefore;
+        uint256 lpSharePriceBefore;
+    }
+
+    /// @dev Creates the test case for the mint transaction.
+    /// @param _funder The funder of the mint.
+    /// @param _long The long destination.
+    /// @param _short The short destination.
+    /// @param _amount The amount of base or vault shares to deposit.
+    /// @param _asBase A flag indicating whether or not the deposit is in base
+    ///        or vault shares.
+    /// @param _extraData The extra data for the transaction.
+    function _mintTestCase(
+        address _funder,
+        address _long,
+        address _short,
+        uint256 _amount,
+        bool _asBase,
+        bytes memory _extraData
+    ) internal view returns (MintTestCase memory) {
         uint256 maturityTime = hyperdrive.latestCheckpoint() +
             hyperdrive.getPoolConfig().positionDuration;
-        uint256 idleBefore = hyperdrive.idle();
-        uint256 kBefore = hyperdrive.k();
-        uint256 spotPriceBefore = hyperdrive.calculateSpotPrice();
-        uint256 aliceBaseBalanceBefore = baseToken.balanceOf(address(alice));
-        uint256 aliceLongBalanceBefore = hyperdrive.balanceOf(
-            AssetId.encodeAssetId(AssetId.AssetIdPrefix.Long, maturityTime),
-            alice
-        );
-        uint256 hyperdriveBaseBalanceBefore = baseToken.balanceOf(
-            address(hyperdrive)
-        );
-        uint256 governanceFeesAccruedBefore = hyperdrive
-            .getUncollectedGovernanceFees();
+        return
+            MintTestCase({
+                // Trading metadata.
+                funder: _funder,
+                long: _long,
+                short: _short,
+                maturityTime: maturityTime,
+                amount: _amount,
+                asBase: _asBase,
+                extraData: _extraData,
+                // The balances before the mint.
+                funderBaseBalanceBefore: baseToken.balanceOf(_funder),
+                hyperdriveBaseBalanceBefore: baseToken.balanceOf(
+                    address(hyperdrive)
+                ),
+                longBalanceBefore: hyperdrive.balanceOf(
+                    AssetId.encodeAssetId(
+                        AssetId.AssetIdPrefix.Long,
+                        maturityTime
+                    ),
+                    _long
+                ),
+                shortBalanceBefore: hyperdrive.balanceOf(
+                    AssetId.encodeAssetId(
+                        AssetId.AssetIdPrefix.Short,
+                        maturityTime
+                    ),
+                    _short
+                ),
+                // The state variables before the mint.
+                longsOutstandingBefore: hyperdrive
+                    .getPoolInfo()
+                    .longsOutstanding,
+                shortsOutstandingBefore: hyperdrive
+                    .getPoolInfo()
+                    .shortsOutstanding,
+                governanceFeesAccruedBefore: hyperdrive
+                    .getUncollectedGovernanceFees(),
+                // Idle, pool depth, and spot price before the mint.
+                idleBefore: hyperdrive.idle(),
+                kBefore: hyperdrive.k(),
+                spotPriceBefore: hyperdrive.calculateSpotPrice(),
+                lpSharePriceBefore: hyperdrive.getPoolInfo().lpSharePrice
+            });
+    }
 
+    /// @dev Process a mint transaction and verify that the state was updated
+    ///      correctly.
+    /// @param _testCase The test case for the mint test.
+    function _verifyMint(MintTestCase memory _testCase) internal {
         // Ensure that Alice can successfully mint.
         vm.stopPrank();
         vm.startPrank(alice);
-        (uint256 maturityTime_, uint256 bondAmount) = hyperdrive.mint(
-            baseAmount,
+        (uint256 maturityTime, uint256 bondAmount) = hyperdrive.mint(
+            _testCase.amount,
             0,
             IHyperdrive.PairOptions({
-                longDestination: alice,
-                shortDestination: alice,
-                asBase: true,
-                extraData: ""
+                longDestination: _testCase.long,
+                shortDestination: _testCase.short,
+                asBase: _testCase.asBase,
+                extraData: _testCase.extraData
             })
         );
-        assertEq(maturityTime_, maturityTime);
+        assertEq(maturityTime, _testCase.maturityTime);
 
         // Verify that the balances increased and decreased by the right amounts.
         assertEq(
-            baseToken.balanceOf(alice),
-            aliceBaseBalanceBefore - baseAmount
+            baseToken.balanceOf(_testCase.funder),
+            _testCase.funderBaseBalanceBefore - _testCase.amount
         );
         assertEq(
             hyperdrive.balanceOf(
-                AssetId.encodeAssetId(AssetId.AssetIdPrefix.Long, maturityTime),
-                alice
+                AssetId.encodeAssetId(
+                    AssetId.AssetIdPrefix.Long,
+                    _testCase.maturityTime
+                ),
+                _testCase.long
             ),
-            aliceLongBalanceBefore + bondAmount
+            _testCase.longBalanceBefore + bondAmount
+        );
+        assertEq(
+            hyperdrive.balanceOf(
+                AssetId.encodeAssetId(
+                    AssetId.AssetIdPrefix.Short,
+                    _testCase.maturityTime
+                ),
+                _testCase.short
+            ),
+            _testCase.shortBalanceBefore + bondAmount
         );
         assertEq(
             baseToken.balanceOf(address(hyperdrive)),
-            hyperdriveBaseBalanceBefore + baseAmount
+            _testCase.hyperdriveBaseBalanceBefore + _testCase.amount
         );
 
-        // Verify that idle, spot price, and pool depth are all
+        // Verify that idle, spot price, LP share price, and pool depth are all
         // unchanged.
-        assertEq(hyperdrive.idle(), idleBefore);
-        assertEq(hyperdrive.calculateSpotPrice(), spotPriceBefore);
-        assertEq(hyperdrive.k(), kBefore);
+        assertEq(hyperdrive.idle(), _testCase.idleBefore);
+        assertEq(hyperdrive.calculateSpotPrice(), _testCase.spotPriceBefore);
+        assertEq(hyperdrive.k(), _testCase.kBefore);
+        assertEq(
+            hyperdrive.getPoolInfo().lpSharePrice,
+            _testCase.lpSharePriceBefore
+        );
 
-        // Ensure that the governance fees accrued increased by the right amount.
+        // Ensure that the longs outstanding, shorts outstanding, and governance
+        // fees accrued increased by the right amount.
+        assertEq(
+            hyperdrive.getPoolInfo().longsOutstanding,
+            _testCase.longsOutstandingBefore + bondAmount
+        );
+        assertEq(
+            hyperdrive.getPoolInfo().shortsOutstanding,
+            _testCase.shortsOutstandingBefore + bondAmount
+        );
         assertEq(
             hyperdrive.getUncollectedGovernanceFees(),
-            governanceFeesAccruedBefore +
+            _testCase.governanceFeesAccruedBefore +
                 2 *
                 bondAmount
                     .mulDown(hyperdrive.getPoolConfig().fees.flat)
                     .mulDown(hyperdrive.getPoolConfig().fees.governanceLP)
         );
 
-        // FIXME: This is good, but we need test cases that verify that this
-        // interoperates with the rest of the Hyperdrive system.
-        //
         // Ensure that the base amount is the bond amount plus the prepaid
         // variable interest plus the governance fees plus the prepaid flat fee.
         uint256 openVaultSharePrice = hyperdrive
@@ -271,26 +393,59 @@ contract MintTest is HyperdriveTest {
             bondAmount.mulDown(hyperdrive.getPoolConfig().fees.flat).mulDown(
                 hyperdrive.getPoolConfig().fees.governanceLP
             );
-        assertGt(baseAmount, requiredBaseAmount);
-        assertApproxEqAbs(baseAmount, requiredBaseAmount, 2);
+        assertGt(_testCase.amount, requiredBaseAmount);
+        assertApproxEqAbs(_testCase.amount, requiredBaseAmount, 10);
 
-        // FIXME: Verify the event.
+        // Verify the `Mint` event.
+        _verifyMintEvent(_testCase, bondAmount);
     }
 
-    // FIXME: We can add more cases for minting success.
-
-    // FIXME
-    function _verifyMint() internal view {
-        // FIXME
+    /// @dev Verify the mint event.
+    /// @param _testCase The test case containing all of the metadata and data
+    ///        relating to the mint transaction.
+    /// @param _bondAmount The amount of bonds that were minted.
+    function _verifyMintEvent(
+        MintTestCase memory _testCase,
+        uint256 _bondAmount
+    ) internal {
+        VmSafe.Log[] memory logs = vm.getRecordedLogs().filterLogs(
+            Mint.selector
+        );
+        assertEq(logs.length, 1);
+        VmSafe.Log memory log = logs[0];
+        assertEq(address(uint160(uint256(log.topics[1]))), _testCase.long);
+        assertEq(address(uint160(uint256(log.topics[2]))), _testCase.short);
+        assertEq(uint256(log.topics[3]), _testCase.maturityTime);
+        (
+            uint256 longAssetId,
+            uint256 shortAssetId,
+            uint256 amount,
+            uint256 vaultSharePrice,
+            bool asBase,
+            uint256 bondAmount,
+            bytes memory extraData
+        ) = abi.decode(
+                log.data,
+                (uint256, uint256, uint256, uint256, bool, uint256, bytes)
+            );
+        assertEq(
+            longAssetId,
+            AssetId.encodeAssetId(
+                AssetId.AssetIdPrefix.Long,
+                _testCase.maturityTime
+            )
+        );
+        assertEq(
+            shortAssetId,
+            AssetId.encodeAssetId(
+                AssetId.AssetIdPrefix.Short,
+                _testCase.maturityTime
+            )
+        );
+        assertEq(amount, _testCase.amount);
+        assertEq(vaultSharePrice, hyperdrive.getPoolInfo().vaultSharePrice);
+        assertEq(asBase, _testCase.asBase);
+        assertEq(bondAmount, _bondAmount);
+        assertEq(extraData, _testCase.extraData);
     }
-
-    // FIXME
-    // function verifyMintEvent() internal {
-    //     VmSafe.Log[] memory logs = vm.getRecordedLogs().filterLogs(
-    //         Mint.selector
-    //     );
-    //     assertEq(logs.length, 1);
-    //     VmSafe.Log memory log = logs[0];
-    //     assertEq(address(uint160(uint256(log.topics[1]))), destination);
-    // }
 }
