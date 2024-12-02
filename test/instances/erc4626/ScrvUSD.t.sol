31c8b797ba4ebce82f34d03d926adc44c1651217
MazyGio <mazygio@element.fi>
2024-12-02 12:33:05 -0500

    update scrvusd test to comply with this PR's updates

--- test/instances/erc4626/ScrvUSD.t.sol
+++ test/instances/erc4626/ScrvUSD.t.sol
@@ -67,6 +67,8 @@ contract scrvUSDHyperdriveTest is ERC4626HyperdriveInstanceTest {
                 shouldAccrueInterest: true,
                 // The base test tolerances.
                 closeLongWithBaseTolerance: 20,
+                closeShortWithBaseUpperBoundTolerance: 10,
+                closeShortWithBaseTolerance: 100,
                 roundTripLpInstantaneousWithBaseTolerance: 1e5,
                 roundTripLpWithdrawalSharesWithBaseTolerance: 1e6,
                 roundTripLongInstantaneousWithBaseUpperBoundTolerance: 1e3,
