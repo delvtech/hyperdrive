77a52c93487e6bd159d567bb992165cd351ff994
MazyGio <95626798+MazyGio@users.noreply.github.com>
2024-12-02 10:52:09 -0500

    scrvUSD mainnet integration + CI fixes (#1215)
    * add scrvUSD instance test
    
    * linting fixes and remove console import
    
    * remove console logs
    
    * log to terminal in benchmark worflow
    
    * set unbuffered so python logs to terminal
    
    * add print
    
    * update gas_benchmark
    
    * add more threads
    
    * wait for the process to finish
    
    * skip tests again
    
    * fix regex
    
    * update solc requirement for scrvusd test
    
    ---------
    
    Co-authored-by: Matthew Brown <mattfdbrown@gmail.com>

--- python/gas_benchmarks.py
+++ python/gas_benchmarks.py
@@ -1,6 +1,6 @@
+import json
 import subprocess
 import sys
-import json
 
 OUTPUT_PATH = sys.argv[1]
 
@@ -17,50 +17,111 @@ FUNCTION_NAMES = [
     "checkpoint",
 ]
 
-# HACK: We have to ignore the Chainlink instance test and the combinatorial test
-# since these tests fail during gas benchmarking.
+print("Starting gas benchmarks...")
+# Run the Solidity tests and write the test name and the gas used to a json file.  The gas-report
+# option outputs a gas summary for every contract that looks like this:
 #
-# Run the Solidity tests and write the test name and the gas used to a markdown table.
+# | contracts/test/MockHyperdrive.sol:MockHyperdrive contract |                 |        |        |        |         |
+# |-----------------------------------------------------------|-----------------|--------|--------|--------|---------|
+# | Deployment Cost                                           | Deployment Size |        |        |        |         |
+# | 19563789                                                  | 97621           |        |        |        |         |
+# | Function Name                                             | min             | avg    | median | max    | # calls |
+# | accrue                                                    | 43765           | 43792  | 43789  | 43801  | 108     |
+# | balanceOf                                                 | 2640            | 2973   | 2640   | 4640   | 6       |
+# | getCheckpointExposure                                     | 7121            | 7121   | 7121   | 7121   | 216     |
+# | getPoolConfig                                             | 9814            | 16819  | 20314  | 22814  | 1548    |
+# | getPoolInfo                                               | 15073           | 26402  | 31392  | 31412  | 671     |
+# | initialize                                                | 355979          | 356037 | 356035 | 356107 | 228     |
+# | openLong                                                  | 33370           | 170709 | 126607 | 286702 | 444     |
+# | pause                                                     | 42450           | 42456  | 42456  | 42462  | 2       |
+# | setLongExposure                                           | 43914           | 43914  | 43914  | 43914  | 1       |
+# | setPauser                                                 | 25306           | 25306  | 25306  | 25306  | 12      |
+#
+# We are only interested in the MockHyperdrive contract and functions listed in FUNCTION_NAMES.
+# We parse the output with the following steps:
+# 1. Check for the contract name.
+# 2. Check for the beginning of the gas report summary by looking for "Function Name".
+# 3. Go line by line and capture the gas report for functions that we are interested in.
+# 4. When we reach the end of the report, break out of the for-loop.
 try:
-    test_output = subprocess.check_output('FOUNDRY_FUZZ_RUNS=100 forge test --no-match-path \'test/instances/*\' --no-match-contract \'MultiToken__transferFrom\' --gas-report', shell=True).decode()
+    # HACK: We have to ignore certains tests that fail during gas benchmarking.
+    SKIP_TESTS = [
+        # ZombieInterestTest tests with high fuzz amounts
+        "test_zombie_interest_short_lp",
+        "test_zombie_interest_long_lp",
+        "test_zombie_long",
+        "test_zombie_short",
+        "test_zombie_long_short",
+        # IntraCheckpointNettingTest tests with high fuzz amounts
+        "test_netting_fuzz",
+        # ExtremeInputs tests with high fuzz amounts
+        "test__updateLiquidity__extremeValues__fuzz",
+        "test_short_below_minimum_share_reserves",
+    ]
+    SKIP_CONTRACTS = ["MultiToken__transferFrom", "ExtremeInputs", "ZombieInterestTest", "IntraCheckpointNettingTest"]
+    process = subprocess.Popen(
+        f"FOUNDRY_FUZZ_RUNS=100 forge test --no-match-path 'test/instances/*' --no-match-test '{'|'.join(SKIP_TESTS)}' --no-match-contract '{'|'.join(SKIP_CONTRACTS)}' --gas-report",
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        universal_newlines=True,
+    )
+    capture = []
+    found_contract: bool = False
+    found_report: bool = False
+    if process.stdout:
+        # Read the output line by line
+        for line in iter(process.stdout.readline, ""):
+            print(line)
+            # Split the line into columns which we can use to check if we are in the gas report summary.
+            cols = [col.strip() for col in line.split("|") if col.strip() != ""]
+
+            # Once we have found the contract look for the gas report summary.
+            if not found_contract and CONTRACT_NAME in line and len(cols) > 0:
+                found_contract = True
+
+            # Once we have found the gas report summary, start capturing the gas report.
+            if found_contract and not found_report and "Function Name" in line:
+                found_report = True
+
+            # Now, go line by line and capture the gas report for functions that we are interested in.
+            elif found_report and len(line) > 0 and len(cols) == 6:
+                function_name = cols[0]
+                if function_name in FUNCTION_NAMES:
+                    capture += [
+                        {
+                            "name": f"{cols[0]}: min",
+                            "value": cols[1],
+                            "unit": "gas",
+                        }
+                    ]
+                    capture += [
+                        {
+                            "name": f"{cols[0]}: avg",
+                            "value": cols[2],
+                            "unit": "gas",
+                        }
+                    ]
+                    capture += [
+                        {
+                            "name": f"{cols[0]}: max",
+                            "value": cols[3],
+                            "unit": "gas",
+                        }
+                    ]
+            # When we reach the end of the report, break out of the for-loop.
+            elif found_report:
+                break
+
+    # Wait for the process to finish.
+    process.wait()
+    if process.returncode != 0 and process.stderr:
+        print(process.stderr.read())
+        exit(1)
+
+    with open(OUTPUT_PATH, "w", encoding="utf-8") as f:
+        json.dump(capture, f)
+
 except subprocess.CalledProcessError as e:
     print(e.output)
     exit(1)
-capture = []
-found_contract = ""
-found_report = False
-for line in test_output.split("\n"):
-    if not found_contract and CONTRACT_NAME in line:
-        found_contract = True
-    if found_contract and not found_report and "Function Name" in line:
-        found_report = True
-    elif found_report and len(line) > 0:
-        cols = line.split("|")
-        function_name = cols[1].strip()
-        if function_name in FUNCTION_NAMES:
-            capture += [
-                {
-                    "name": f"{cols[1].strip()}: min",
-                    "value": cols[2].strip(),
-                    "unit": "gas",
-                }
-            ]
-            capture += [
-                {
-                    "name": f"{cols[1].strip()}: avg",
-                    "value": cols[3].strip(),
-                    "unit": "gas",
-                }
-            ]
-            capture += [
-                {
-                    "name": f"{cols[1].strip()}: max",
-                    "value": cols[5].strip(),
-                    "unit": "gas",
-                }
-            ]
-    elif found_report:
-        break
-
-with open(OUTPUT_PATH, "w") as f:
-    json.dump(capture, f)
